33bedf6b161daab33849561f0657f55956edd768
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-02 15:26:03 +0100

    new SubProcessPythonCalculator.  This is just like SocketIOCalculator but uses pickle.  Useful for multiprocessing.  This should be generalized to work with MPI.  Added get_properties() method on Calculator to request multiple properties at once.  Currently just has a loop with get_property() but we can generalize it later so it doesn't do the caching check more than once.

--- ase/calculators/subprocesscalculator.py
+++ ase/calculators/subprocesscalculator.py
@@ -1,37 +1,101 @@
 import sys
-from subprocess import Popen
-from ase.calculators.calculator import Calculator
+from subprocess import Popen, PIPE
+from ase.calculators.calculator import Calculator, all_properties
+from ase.calculators.singlepoint import SinglePointDFTCalculator
 import pickle
 
-class SubProcessCalculator(Calculator):
-    def __init__(self, calc):
-        self.calc = calc
-        self.proc = None
 
-    @property
-    def is_open(self):
-        return self.proc is None
+class CalculatorInput:
+    def __init__(self, name, kwargs):
+        self.name = name
+        self.kwargs = kwargs
+
+    def __repr__(self):
+        return 'CalculatorInput({}, {})'.format(self.name, self.kwargs)
+
+
+def wrap_subprocess(calc):
+    calc_input = CalculatorInput(calc.name, calc.parameters)
+    return SubProcessPythonCalculator(calc_input)
+
+# TODO: Generalize outputs somehow?
+#class CalculatorOutput:
+#    def __init__(self, results):
+#        self.results = results
+
+class SubProcessPythonCalculator(Calculator):
+    implemented_properties = all_properties
+    def __init__(self, calc_input):
+        super().__init__()
+        proc = Popen([sys.executable, '-m', __name__],
+                     stdout=PIPE, stdin=PIPE)  #, stderr=PIPE)
+        self.proc = proc
+        self.calc_input = calc_input
+
+        #self._write(calc_input)
+        self._send(calc_input)
+
+    def set(self, **kwargs):
+        if hasattr(self, 'proc'):
+            raise RuntimeError('No setting things for now, thanks')
+
+    def _send(self, obj):
+        pickle.dump(obj, self.proc.stdin)
+        self.proc.stdin.flush()
+
+    def _recv(self):
+        return pickle.load(self.proc.stdout)
+
+    def __repr__(self):
+        return '{}({})'.format(type(self).__name__,
+                               self.calc_input)
 
     def __enter__(self):
-        self.proc = Popen([sys.executable, '-m', __name__],
-                          stdout=PIPE, stdin=PIPE, stderr=PIPE)
-        ...
+        return self
 
     def __exit__(self, *args):
+        self._send('stop')
         self.proc.wait()
         self.proc = None
 
+    def _run_calculation(self, atoms, properties, system_changes):
+        self._send('calculate')
+        self._send((atoms, properties, system_changes))
+
     def calculate(self, atoms, properties=None, system_changes=None):
-        pickle.dump(self.calc, atoms, properties, system_changes)
-        exitcode = proc.wait()
-        if exitcode:
-            raise OSError()
+        atoms = atoms.copy()  # Discards calculator
+        Calculator.calculate(self, atoms, properties, system_changes)
+        self._run_calculation(atoms, properties, system_changes)
+        results = self._recv()
+        self.results.update(results)
 
 
 def main():
+    from ase.calculators.calculator import get_calculator_class
+
+    def recv():
+        return pickle.load(sys.stdin.buffer)
+
+    def send(obj):
+        pickle.dump(obj, sys.stdout.buffer)
+        sys.stdout.flush()
+
+
+    calc_input = recv()
+    cls = get_calculator_class(calc_input.name)
+    calc = cls(**calc_input.kwargs)
     while True:
-        calc, atoms, properties, system_changes = pickle.load(sys.stdin)
-        calc.calculate....
+        instruction = recv()
+        if instruction == 'stop':
+            break
+        elif instruction != 'calculate':
+            raise ValueError('Bad instruction: {}'.format(instruction))
+
+        atoms, properties, system_changes = recv()
+
+        calc.calculate(atoms, properties, system_changes)
+        results = calc.results
+        send(results)
 
 if __name__ == '__main__':
     main()
