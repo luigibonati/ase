be73952c02a63cf16a551ef2d5f5d2e3e2553161
pmla <pete.mahler.larsen@gmail.com>
2019-11-06 17:16:44 +0100

    fixed permute_axes.pbc and ReducedCrystal components

--- ase/test/crystal_reduction.py
+++ ase/test/crystal_reduction.py
@@ -1,17 +1,19 @@
 import numpy as np
 from ase.build import bulk, mx2, nanotube
+from ase.geometry import permute_axes
 from ase.geometry.rmsd import find_crystal_reductions
 
 
 TOL = 1E-10
 
 
-def permute_atoms(atoms, perm):
-    atoms = atoms.copy()
-    atoms.set_positions(atoms.get_positions()[:, perm])
-    atoms.set_cell(atoms.cell[perm][:, perm], scale_atoms=False)
-    atoms.set_pbc(atoms.pbc[perm])
-    return atoms
+def check_components(atoms, result):
+    for reduced in result:
+        assert (np.bincount(reduced.components) == reduced.factor).all()
+        x = atoms.numbers[np.argsort(reduced.components)]
+        x = x.reshape((len(atoms) // reduced.factor, reduced.factor))
+        assert (x - x[:, 0][:, None] == 0).all()
+
 
 # 3-dimensional: NaCl
 size = 3
@@ -21,28 +23,31 @@ assert len(result) == size
 assert all([reduced.rmsd < TOL for reduced in result])
 factors = [reduced.factor for reduced in result]
 assert tuple(factors) == (3, 9, 27)
+check_components(atoms, result)
 
 # 2-dimensional: MoS2
 for i in range(3):
     size = 4
     atoms = mx2(formula='MoS2', size=(size, size, 1))
     permutation = np.roll(np.arange(3), i)
-    atoms = permute_atoms(atoms, permutation)
+    atoms = permute_axes(atoms, permutation)
 
     result = find_crystal_reductions(atoms)
     assert len(result) == size
     assert all([reduced.rmsd < TOL for reduced in result])
     factors = [reduced.factor for reduced in result]
     assert tuple(factors) == (2, 4, 8, 16)
+    check_components(atoms, result)
 
 # 1-dimensional: carbon nanotube
 for i in range(3):
     size = 4
     atoms = nanotube(3, 3, length=size)
     permutation = np.roll(np.arange(3), i)
-    atoms = permute_atoms(atoms, permutation)
+    atoms = permute_axes(atoms, permutation)
 
     result = find_crystal_reductions(atoms)
     factors = [reduced.factor for reduced in result[:2]]
     assert tuple(factors) == (2, 4)
     assert all([reduced.rmsd < TOL for reduced in result[:2]])
+    check_components(atoms, result)
