8fb925e8b42bae97e6d38c77ffa1423c899bdf6c
pmla <pete.mahler.larsen@gmail.com>
2019-11-07 10:02:41 +0100

    added inversion symmetry finding

--- ase/geometry/rmsd/__init__.py
+++ ase/geometry/rmsd/__init__.py
@@ -1,3 +1,4 @@
 from ase.geometry.rmsd.crystal_reduction import find_crystal_reductions
+from ase.geometry.rmsd.inversion_symmetry import find_inversion_symmetry
 
-__all__ = ['find_crystal_reductions']
+__all__ = ['find_crystal_reductions', 'find_inversion_symmetry']
