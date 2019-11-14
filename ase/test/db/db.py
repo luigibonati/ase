9eb1e0391070d6bd360caccc98e470c8ce1fc1ef
Kirsten Winther <winther@stanford.edu>
2019-11-13 17:21:09 -0800

    test both ways to connect to db

--- ase/test/db/db.py
+++ ase/test/db/db.py
@@ -46,29 +46,28 @@ for name in ['testase.json', 'testase.db', 'postgresql', 'mysql', 'mariadb']:
         if name is None:
             continue
 
-    con = connect(name)
     t1 = time.time()
-    if 'postgres' in name or 'mysql' in name or 'mariadb' in name:
-        con.delete([row.id for row in con.select()])
+    with connect(name) as con:
+        if 'postgres' in name or 'mysql' in name or 'mariadb' in name:
+            con.delete([row.id for row in con.select()])
 
-    cli(cmd.replace('testase.json', name))
-    assert con.get_atoms(H=1)[0].magmom == 1
-    count(5)
-    count(3, 'hydro')
-    count(0, 'foo')
-    count(3, abc=42)
-    count(3, 'abc')
-    count(0, 'abc,foo')
-    count(3, 'abc,hydro')
-    count(0, foo='bar')
-    count(1, formula='H2')
-    count(1, formula='H2O')
-    count(3, 'fmax<0.1')
-    count(1, '0.5<mass<1.5')
-    count(5, 'energy')
-
-    id = con.reserve(abc=7)
-    assert con[id].abc == 7
+        cli(cmd.replace('testase.json', name))
+        assert con.get_atoms(H=1)[0].magmom == 1
+        count(5)
+        count(3, 'hydro')
+        count(0, 'foo')
+        count(3, abc=42)
+        count(3, 'abc')
+        count(0, 'abc,foo')
+        count(3, 'abc,hydro')
+        count(0, foo='bar')
+        count(1, formula='H2')
+        count(1, formula='H2O')
+        count(3, 'fmax<0.1')
+        count(1, '0.5<mass<1.5')
+        count(5, 'energy')
+        id = con.reserve(abc=7)
+        assert con[id].abc == 7
 
     for key in ['calculator', 'energy', 'abc', 'name', 'fmax']:
         count(6, sort=key)
@@ -76,6 +75,7 @@ for name in ['testase.json', 'testase.db', 'postgresql', 'mysql', 'mariadb']:
 
     cli('ase -T gui --terminal {}@3'.format(name))
 
+    con = connect(name)
     con.delete([id])
 
     if name != 'testase.json':  # transfer between db formats
