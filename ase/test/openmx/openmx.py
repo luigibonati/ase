a18eeaa88a30ec7b487ebf73f6fc4715ddaa3e6b
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-24 23:23:31 +0100

    add openmx test equal to the datadriven one.  We don't want to have this kind of pairs of tests in general, but we probably need this for now

---
+++ ase/test/openmx/openmx.py
@@ -0,0 +1,23 @@
+import numpy as np
+from ase.calculators.openmx import OpenMX
+from ase.build import molecule
+from ase.utils import workdir
+
+atoms = molecule('H2O')
+atoms.center(vacuum=2.5)
+calc = OpenMX()
+atoms.calc = calc
+with workdir('omx-old', mkdir=True):
+    e = atoms.get_potential_energy()
+    f = atoms.get_forces()
+print(e)
+print(f)
+eref = -474.2232383806571
+fref = np.array([[-1.25984064e-08,  0.00000000e+00,  1.54738047e-01],
+                 [ 6.37633632e-09,  9.07735945e-02, -4.88082632e-02],
+                 [ 6.37633632e-09, -9.07735945e-02, -4.88082632e-02]])
+
+eerr = abs(e - eref)
+ferr = np.abs(f - fref).max()
+assert eerr  < 1e-6
+assert ferr < 1e-6
