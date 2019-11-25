6968779ea7eed1e1da96e794f631c1b594dd7d53
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-25 14:40:33 +0100

    add tests of datadriven calculator

--- ase/test/datadriven/emt.py
+++ ase/test/datadriven/emt.py
@@ -1,4 +1,9 @@
 from ase.test.datadriven.main import test_singlepoint
 from ase.calculators.datadriven import new_emt
+from ase.build import bulk
 
-test_singlepoint('emt', new_emt())
+atoms = bulk('Au') * (2, 2, 2)
+atoms.rattle(stdev=0.2)
+atoms.calc = new_emt()
+
+test_singlepoint('emt', atoms)
