27268fdfba99b5df5653048bba040685ed17dbf8
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-25 14:40:33 +0100

    add tests of datadriven calculator

--- ase/test/datadriven/main.py
+++ ase/test/datadriven/main.py
@@ -2,11 +2,7 @@ from ase.calculators.datadriven import new_emt
 from ase.build import bulk
 from ase.utils import workdir
 
-def test_singlepoint(name, calc):
-    atoms = bulk('Au') * (2, 2, 2)
-    atoms.rattle(stdev=0.2)
-
-    atoms.calc = calc
+def test_singlepoint(name, atoms):
     with workdir('files-{}'.format(name), mkdir=True):
         e = atoms.get_potential_energy()
         f = atoms.get_forces()
