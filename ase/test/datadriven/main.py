bdd9f0507699f8be97d85f0cad82cc9b80834dce
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-25 15:46:19 +0100

    make sure the datadriven calculator test instantiates the real calculator object type which it is testing.  This will make sure that the test suite correctly skips those calculators

--- ase/test/datadriven/main.py
+++ ase/test/datadriven/main.py
@@ -1,8 +1,17 @@
 from ase.calculators.datadriven import new_emt
+from ase.calculators.calculator import get_calculator_class
 from ase.build import bulk
 from ase.utils import workdir
 
+
+def skip_if_not_enabled(name):
+    cls = get_calculator_class(name)
+    cls()  # the test suite monkeypatches these classes so this raises SkipTest
+
+
 def test_singlepoint(name, atoms):
+    skip_if_not_enabled(name)
+
     with workdir('files-{}'.format(name), mkdir=True):
         e = atoms.get_potential_energy()
         f = atoms.get_forces()
