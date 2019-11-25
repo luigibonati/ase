db5624295dcdd4844138c047642e67ee93fb2a0b
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-25 17:19:07 +0100

    use single kpoint

--- ase/test/datadriven/openmx.py
+++ ase/test/datadriven/openmx.py
@@ -8,9 +8,10 @@ atoms = molecule('H2O')
 atoms.center(vacuum=2.5)
 
 calc = new_openmx(
-    scf_energycutoff=150,
+    scf_energycutoff=50,
+    kpts=1,
     scf_mixing_type='Rmm-Diis',
-    scf_criterion=1e-6)
+    scf_criterion=1e-3)
 
 atoms.calc = calc
 test_singlepoint('openmx', atoms)
@@ -19,13 +20,13 @@ test_singlepoint('openmx', atoms)
 e = atoms.get_potential_energy()
 f = atoms.get_forces()
 
-# Use FD check?
-eref = -474.2232383806571
-fref = np.array([[-1.25984064e-08,  0.00000000e+00,  1.54738047e-01],
-                 [ 6.37633632e-09,  9.07735945e-02, -4.88082632e-02],
-                 [ 6.37633632e-09, -9.07735945e-02, -4.88082632e-02]])
 
-eerr = abs(e - eref)
-ferr = np.abs(f - fref).max()
+energy = -474.11500268758516
+force = np.array([[-3.08532403e-10, -1.95403855e-09, -1.01401451e-01],
+       [-5.09078464e-09,  7.42704323e-02, -2.33793323e-02],
+       [ 5.60500531e-09, -7.42704305e-02, -2.33793365e-02]])
+
+eerr = abs(e - energy)
+ferr = np.abs(f - force).max()
 assert eerr  < 1e-6
 assert ferr < 1e-6
