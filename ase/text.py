25476e119e39b35d3e7b7b9ef42bd5d56bb9a71c
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-11-27 16:30:07 +0100

    add ansi colours for all atoms based on CPK.  For testing, add periodic system which we can print in colours

--- ase/text.py
+++ ase/text.py
@@ -47,11 +47,92 @@ class ANSIColors:
     __getattr__ = get
 
 
-atom_colors = {1: 'light gray', 8: 'light red', 6: 'dark gray'}
+atom_colors = [None] * 119
 
 ansi_nocolor = '\x1b[0m'
 ansi = ANSIColors()
 
+def set_colors(symbols, color):
+    from ase.symbols import Symbols
+    for Z in Symbols.fromsymbols(symbols).numbers:
+        atom_colors[Z] = color
+
+
+table = """\
+
+  H                                                 He
+ Li Be                                B  C  N  O  F Ne
+ Na Mg                               Al Si  P  S Cl Ar
+  K Ca Sc Ti  V Cr Mn Fe Co Ni Cu Zn Ga Ge As Se Br Kr
+ Rb Sr  Y Zr Nb Mo Tc Ru Rh Pd Ag Cd In Sn Sb Te  I Xe
+ Cs Ba  * Hf Ta  W Re Os Ir Pt Au Hg Tl Pb Bi Po At Rn
+ Fr Ra ** Rf Db Sg Bh Hs Mt Ds Rg Cn Nh Fl Mc Lv Ts Og
+
+      * La Ce Pr Nd Pm Sm Eu Gd Tb Dy Ho Er Tm Yb Lu
+     ** Ac Th Pa  U Np Pu Am Cm Bk Cf Es Fm Md No Lr
+"""
+
+col = set_colors
+
+# We choose colours based more or less on CPK.
+
+col('X', 'red')
+col('H', 'light gray')
+col('LiNaKRbCsFr', 'purple') ,  # Group 1
+col('BeMgCaSr', 'light green')  #  Group 2
+col('BaRa', 'green')  # More group 2
+col('HeNeAr', 'light cyan')  # Nobles part 1
+col('KrXeRn', 'cyan')  # Nobles part 2
+
+# Transition metals 3d
+col('ScTiV', 'light gray')
+col('Cr', 'cyan')
+col('Mn', 'purple')
+col('Fe', 'light red')
+col('Co', 'light purple')
+col('Ni', 'light green')
+col('Cu', 'brown')
+col('Zn', 'light blue')
+
+# 4d
+col('YZrNb', 'light cyan')
+col('MoTcRuRhPd', 'cyan')
+col('Ag', 'white')
+col('Cd', 'light purple')
+
+# 5d
+col('LaHfTaW', 'light blue')
+col('ReOsIr', 'blue')
+col('PtHg', 'light gray')
+col('Au', 'yellow')
+
+# The main-group elements are a bit of a colour hodge-podge
+col('BSi', 'light purple')
+col('CPb', 'dark gray')
+col('Al', 'light gray')
+col('GaInTlPSeTePoAt', 'brown')
+col('GeSn', 'cyan')
+col('N', 'light blue')
+col('O', 'light red')
+col('FCl', 'light green')
+col('Br', 'red')
+col('S', 'yellow')
+col('AsSbBiI', 'purple')
+
+# Lanthanides
+col('Ce', 'white')
+col('PrNdPmSmEuGdTbDy', 'light cyan')
+col('HoErTm', 'light green')
+col('YbLu', 'green')
+
+# Actinides and Ac
+col('AcThPaU', 'light blue')
+col('NpPuAm', 'blue')
+col('CmBkCfEsFmMdNoLr', 'purple')
+
+# Post-actinides
+col('RfDbSgBhHsMt', 'red')
+col('DsRgCnNhFlMcLvTsOg', 'white')
 
 
 def plot(atoms):
@@ -66,9 +147,8 @@ def plot(atoms):
     cell_cv = atoms.get_cell()
     if not atoms.cell or (cell_cv - np.diag(cell_cv.diagonal())).any():
         atoms = atoms.copy()
-        atoms.cell = [1, 1, 1]
         atoms.center(vacuum=0.0)
-        cell_cv = atoms.get_cell()
+        cell_cv = atoms.get_cell().complete()
         plot_box = False
     else:
         plot_box = True
@@ -148,27 +228,44 @@ class Grid:
             self.color_key[i, j] = color_key
 
 
-def main(stdscr):
+def main():
+    # main(stdscr)
     import sys
     import time
-    from ase.io import read
-    fname = sys.argv[1]
-    atoms0 = read(fname)
-    #txt = plot(atoms)
-    #print(txt)
+    from ase.io import iread
 
-    for i in range(200):
-        stdscr.clear()
-        atoms = atoms0.copy()
-        atoms.rotate(i, 'z', center='com')
-        txt = plot(atoms)
-        stdscr.addstr(0, 0, 'hello {}'.format(ansi.red(str(i))))
-        curses.doupdate()
-        #print(ansi.red('hello world'))
-        time.sleep(0.02)
-        stdscr.noutrefresh()
-        #stdscr.refresh()
+    if len(sys.argv) == 1:
+        from ase.data import atomic_numbers
+        import re
+        def substitute(match):
+            sym = match.group()
+            Z = atomic_numbers[sym]
+            return ansi[atom_colors[Z]](sym)
+        print(re.sub(r'[A-Z][a-z]?', substitute, table))
+        return
+
+    for fname in sys.argv[1:]:
+        for i, atoms in enumerate(iread(fname)):
+            formula = str(atoms.symbols)
+            print('{}@{}: {}'.format(fname, i, atoms))
+            txt = plot(atoms)
+            print(txt)
+            print()
+
+    if 0:
+        for i in range(200):
+            stdscr.clear()
+            atoms = atoms0.copy()
+            atoms.rotate(i, 'z', center='com')
+            txt = plot(atoms)
+            stdscr.addstr(0, 0, 'hello {}'.format(ansi.red(str(i))))
+            curses.doupdate()
+            #print(ansi.red('hello world'))
+            time.sleep(0.02)
+            stdscr.noutrefresh()
+            #stdscr.refresh()
 
 if __name__ == '__main__':
-    import curses
-    curses.wrapper(main)
+    main()
+    #import curses
+    #curses.wrapper(main)
