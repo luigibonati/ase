782d908e2f84f3219319a40c05efd0e5b0dbabff
david <davidollodart@gmail.com>
2019-12-08 19:08:26 -0800

    ENH: added table printing for calculator outputs
    Borrowing the (undocumented) ase.gui.images Images object to print out
    results similarly to how the gui displays a plot. The table becomes
    very wide if both displacements and forces are in the same one and so
    different tables are printed. But both of these quantities should generally
    be compared together. A briefer table of only magnitude differences
    in positions and forces may be added.

--- ase/cli/diff.py
+++ ase/cli/diff.py
@@ -1,5 +1,5 @@
 import numpy as np
-from ase.io import read
+from ase.io import read, iread
 
 # there are several packages for pretty printing text tables in python
 template = "{title}\n{toprule}\n{header}\n{midrule}\n{body}\n{bottomrule}\n{summary}"
@@ -49,9 +49,47 @@ def table_gen(elements, positions1, positions2, natural_order=False):
 
     return template.format(**fd).expandtabs(tabwidth)
 
+# there are several packages for pretty printing text tables in python
+fd2 = {}
+fd2['title'] = 'Forces and Energies'
+fd2['toprule'] = fd2['bottomrule'] = '=' * cwidth
+fd2['midrule'] = '-' * cwidth
+fd2['header'] = '\t'.join(
+    ['index', '   Fx   ', '   Fy   ', '   Fz   ', '   F   ', 'F rank', 'element'])
+row2 = '{0}\t{1:+.1E}\t{2:+.1E}\t{3:+.1E}\t{4:.1E}\t{5}\t{6}'
+
+def table_gen_2(elements, forces, energy, natural_order=False):
+    force_ranks = sorted(range(len(elements)),key=lambda x: -np.linalg.norm(forces[x]))
+    body = []
+    if natural_order:
+        for c, force in enumerate(forces):
+            body.append(
+                row.format(
+                    c,
+                    *force,
+                    np.linalg.norm(force),
+                    force_ranks[c],
+                    elements[c]
+                    ))
+    else:  # rank order
+        for c, pointer in enumerate(pointers):
+            body.append(
+                row.format(
+                    pointer,
+                    *forces[pointer],
+                    np.linalg.norm(forces[pointer]),
+                    c,
+                    elements[pointer]
+                    ))
+
+    fd2['body'] = '\n'.join(body)
+    fd2['summary'] = 'Energy = {0:.2f} eV'.format(energy)
+
+    return template.format(**fd2).expandtabs(tabwidth)
+
 
 class CLICommand:
-    """Print/plot differences between atoms/calculations.
+    """Print differences between atoms/calculations.
 
     Specify more things here in a second block.
 
@@ -61,34 +99,107 @@ class CLICommand:
     @staticmethod
     def add_arguments(parser):
         add = parser.add_argument
-        add('f1', metavar='initial file', help='initial file')
-        add('f2', metavar='final file', help='final file')
-        add('-e', '--energies', action="store_true", help="Plot energies")
-        add('-d', '--displacements', action="store_true", help="Plot displacements")
-        add('-f', '--forces', action="store_true", help="Plot forces")
-        add('-r', '--rank-order', action="store_true", help="Order atoms by rank")
+        add('files', metavar='file(s)', 
+                help='either one trajectory file or two non-trajectory files',
+                nargs = '+')
+        add('-r', '--rank-order', action="store_true", 
+                help="Order atoms by rank")
+        add('-c', '--calculator-outputs', action="store_true", help="display calculator outputs")
 
     @staticmethod
     def run(args, parser):
         from ase.io.formats import UnknownFileTypeError
-        try:
-            atoms1 = read(args.f1)
-            elements = atoms1.symbols
-            positions1 = atoms1.positions
-        except UnknownFileTypeError:
-            raise UnknownFileTypeError
-        try:
-            atoms2 = read(args.f2)
-            positions2 = atoms2.positions
-        except UnknownFileTypeError:
-            raise UnknownFileTypeError
+        if len(args.files) == 1 and not args.calculator_outputs: 
+            try:
+                traj = iread(args.files[0])
+            except UnknownFileTypeError:
+                raise UnknownFileTypeError
 
-        # same number of atoms and order
-        assert((atoms2.symbols == elements).all())
+            counter = 0
+            for atoms in traj:
+                elements = atoms.symbols
+                positions = atoms.positions
+                if counter > 0:
+                    # same number of atoms and order
+                    assert((elements == elements0).all())
+                    print('\n'*2)
+                    print('Iteration/Image Numbers {}-{}'.format(counter+1,counter))
+                    print(
+                            table_gen(
+                                elements,
+                                positions0,
+                                positions,
+                                natural_order = not args.rank_order
+                                )
+                            )
+                positions0 = positions
+                elements0 = elements
+                counter += 1
 
-        print(
-            table_gen(
-                elements,
-                positions1,
-                positions2,
-                natural_order=not args.rank_order))
+        elif len(args.files) == 1 and args.calculator_outputs:
+            #relies on the (undocumented) gui Images object
+            try:
+                from ase.gui.images import Images
+                images = Images()
+                images.read([args.files[0]])
+            except UnknownFileTypeError:
+                raise UnknownFileTypeError
+
+            counter = 0
+            for atoms in images:
+                elements = atoms.symbols
+                positions = atoms.positions
+                if counter > 0:
+                    # same number of atoms and order
+                    assert((elements == elements0).all())
+                    print('\n'*2)
+                    print('Iteration/Image Numbers {}-{}'.format(counter+1,counter))
+                    print(
+                            table_gen(
+                                elements,
+                                positions0,
+                                positions,
+                                natural_order = not args.rank_order
+                                )
+                            )
+                positions0 = positions
+                elements0 = elements
+                forces = images.get_forces(atoms)
+                energy = images.get_energy(atoms)
+                print('\n')
+                print(
+                        table_gen_2(
+                            elements,
+                            forces,
+                            energy,
+                            natural_order = not args.rank_order
+                            )
+                        )
+                counter += 1
+
+        elif len(args.files) == 2:
+            f1 = args.files[0]
+            f2 = args.files[1]
+            try:
+                atoms1 = read(f1)
+                elements = atoms1.symbols
+                positions1 = atoms1.positions
+            except UnknownFileTypeError:
+                raise UnknownFileTypeError
+            try:
+                atoms2 = read(f2)
+                positions2 = atoms2.positions
+            except UnknownFileTypeError:
+                raise UnknownFileTypeError
+
+            # same number of atoms and order
+            assert((atoms2.symbols == elements).all())
+
+            print(
+                table_gen(
+                    elements,
+                    positions1,
+                    positions2,
+                    natural_order=not args.rank_order))
+        else:
+            raise Exception('Pass either 1 or 2 arguments')
