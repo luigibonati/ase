d6bed61df9589b9e3d6f84fac19c746146a53461
Estefania Garijo del Rio <egarijo@fysik.dtu.dk>
2019-12-10 15:16:27 +0100

    Make gp always return a variance variable, even if it does not compute it

--- ase/optimize/activelearning/gp/gp.py
+++ ase/optimize/activelearning/gp/gp.py
@@ -97,7 +97,7 @@ class GaussianProcess():
             covariance = np.tensordot(v, v, axes=(0, 0))
             V = variance - covariance
             return f, V
-        return f
+        return f, None
 
     def neg_log_likelihood(self, params, *args):
         """Negative logarithm of the marginal likelihood and its derivative.
