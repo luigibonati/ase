a4be974bb3ddb97e67b94d51f176066ace669559
egarijo <egarijo@curie.fysik.dtu.dk>
2019-12-13 15:13:49 +0100

    Remove unnecesary call to calculate

--- ase/optimize/activelearning/gp/calculator.py
+++ ase/optimize/activelearning/gp/calculator.py
@@ -175,7 +175,7 @@ class GPCalculator(Calculator, GaussianProcess):
         for i in train_images:
             if i not in self.train_images:
                 self.train_images.append(i)
-        self.calculate(atoms=self.train_images[0])  # Test one to attach.
+        #self.calculate(atoms=self.train_images[0])  # Test one to attach.
 
     def train_model(self):
         """ Train a model with the previously fed observations."""
