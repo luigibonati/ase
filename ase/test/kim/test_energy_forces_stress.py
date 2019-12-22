e2853bae2683a04a44469b22eb2f48ee61a2edea
Ask Hjorth Larsen <asklarsen@gmail.com>
2019-12-23 00:14:35 +0100

    port the kim tests to pytest using quick hacks

--- ase/test/kim/test_energy_forces_stress.py
+++ ase/test/kim/test_energy_forces_stress.py
@@ -1,45 +1,49 @@
-"""
-To test that the calculator can produce correct energy and forces.  This
-is done by comparing the energy for an FCC argon lattice with an example
-model to the known value; the forces/stress returned by the model are
-compared to numerical estimates via finite difference.
-"""
-import numpy as np
-from ase.calculators.kim import KIM
-from ase.lattice.cubic import FaceCenteredCubic
+def test():
+    """
+    To test that the calculator can produce correct energy and forces.  This
+    is done by comparing the energy for an FCC argon lattice with an example
+    model to the known value; the forces/stress returned by the model are
+    compared to numerical estimates via finite difference.
+    """
+    import pytest
+    pytest.importorskip('kimpy')
 
-# Create an FCC atoms crystal
-atoms = FaceCenteredCubic(
-    directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
-    size=(1, 1, 1),
-    symbol="Ar",
-    pbc=(1, 0, 0),
-    latticeconstant=3.0,
-)
+    import numpy as np
+    from ase.calculators.kim import KIM
+    from ase.lattice.cubic import FaceCenteredCubic
 
-# Perturb the x coordinate of the first atom by less than the cutoff distance
-atoms.positions[0, 0] += 0.01
+    # Create an FCC atoms crystal
+    atoms = FaceCenteredCubic(
+        directions=[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+        size=(1, 1, 1),
+        symbol="Ar",
+        pbc=(1, 0, 0),
+        latticeconstant=3.0,
+    )
 
-calc = KIM("ex_model_Ar_P_Morse_07C")
-atoms.set_calculator(calc)
+    # Perturb the x coordinate of the first atom by less than the cutoff distance
+    atoms.positions[0, 0] += 0.01
 
-# Get energy and analytical forces/stress from KIM model
-energy = atoms.get_potential_energy()
-forces = atoms.get_forces()
-stress = atoms.get_stress()
+    calc = KIM("ex_model_Ar_P_Morse_07C")
+    atoms.set_calculator(calc)
 
-# Previously computed energy for this configuration for this model
-energy_ref = 19.7196709065  # eV
+    # Get energy and analytical forces/stress from KIM model
+    energy = atoms.get_potential_energy()
+    forces = atoms.get_forces()
+    stress = atoms.get_stress()
 
-# Compute forces and virial stress numerically
-forces_numer = calc.calculate_numerical_forces(atoms, d=0.0001)
-stress_numer = calc.calculate_numerical_stress(atoms, d=0.0001, voigt=True)
+    # Previously computed energy for this configuration for this model
+    energy_ref = 19.7196709065  # eV
 
-tol = 1e-6
-assert np.isclose(energy, energy_ref, tol)
-assert np.allclose(forces, forces_numer, tol)
-assert np.allclose(stress, stress_numer, tol)
+    # Compute forces and virial stress numerically
+    forces_numer = calc.calculate_numerical_forces(atoms, d=0.0001)
+    stress_numer = calc.calculate_numerical_stress(atoms, d=0.0001, voigt=True)
 
-# This has been known to segfault
-atoms.set_pbc(True)
-atoms.get_potential_energy()
+    tol = 1e-6
+    assert np.isclose(energy, energy_ref, tol)
+    assert np.allclose(forces, forces_numer, tol)
+    assert np.allclose(stress, stress_numer, tol)
+
+    # This has been known to segfault
+    atoms.set_pbc(True)
+    atoms.get_potential_energy()
--- ase/test/kim/test_relax.py
+++ ase/test/kim/test_relax.py
@@ -1,22 +1,26 @@
-"""
-Test that a static relaxation that requires multiple neighbor list
-rebuilds can be carried out successfully.  This is verified by relaxing
-an icosahedral cluster of atoms and checking that the relaxed energy
-matches a known precomputed value for an example model.
-"""
-import numpy as np
-from ase.cluster import Icosahedron
-from ase.calculators.kim import KIM
-from ase.optimize import BFGS
+def test():
+    """
+    Test that a static relaxation that requires multiple neighbor list
+    rebuilds can be carried out successfully.  This is verified by relaxing
+    an icosahedral cluster of atoms and checking that the relaxed energy
+    matches a known precomputed value for an example model.
+    """
+    import pytest
+    pytest.importorskip('kimpy')
 
-energy_ref = -0.5420939378624228  # eV
+    import numpy as np
+    from ase.cluster import Icosahedron
+    from ase.calculators.kim import KIM
+    from ase.optimize import BFGS
 
-# Create structure and calculator
-atoms = Icosahedron("Ar", latticeconstant=3.0, noshells=2)
-calc = KIM("ex_model_Ar_P_Morse_07C")
-atoms.set_calculator(calc)
+    energy_ref = -0.5420939378624228  # eV
 
-opt = BFGS(atoms, logfile=None)
-opt.run(fmax=0.05)
+    # Create structure and calculator
+    atoms = Icosahedron("Ar", latticeconstant=3.0, noshells=2)
+    calc = KIM("ex_model_Ar_P_Morse_07C")
+    atoms.set_calculator(calc)
 
-assert np.isclose(atoms.get_potential_energy(), energy_ref)
+    opt = BFGS(atoms, logfile=None)
+    opt.run(fmax=0.05)
+
+    assert np.isclose(atoms.get_potential_energy(), energy_ref)
