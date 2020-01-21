9ef181d8fca391889837770e603b8b97fd0f24ab
Estefania Garijo del Rio <egarijo@fysik.dtu.dk>
2020-01-21 11:11:10 +0100

    Remove calls to previously deleted variable

--- ase/optimize/activelearning/tests/bondedkernel.py
+++ ase/optimize/activelearning/tests/bondedkernel.py
@@ -5,16 +5,16 @@ def test_implementation():
     D = 6
     params = {"weight": 1.0, "scale": 0.4}
     X = np.asarray([np.random.rand(6) for i in range(3)])
-    
+
     bk = BondExponential(D)
     sq_k = SquaredExponential(D)
-    
+
     bk.set_params(params)
     sq_k.set_params(params)
 
     bk.G = np.eye(D)
     bk.F = np.eye(D)
-    
+
     assert np.allclose(bk.kernel_matrix(X), sq_k.kernel_matrix(X))
     assert np.allclose(bk.gradient(X),      sq_k.gradient(X))
 
@@ -26,13 +26,13 @@ def test_graph_metric(debug=False):
     kernel.set_params(params)
 
     r = [1.2]*Natoms
-    kernel.init_metric(r, eps = 1e-14)
+    kernel.init_metric(r)
 
     if debug:
         print(kernel.g)
 
     assert np.allclose(kernel.G, np.dot(kernel.F.T, kernel.F))
-    
+
     if debug:
         X = np.asarray([np.random.rand(3*Natoms) for i in range(3)])
         print(kernel.kernel_matrix(X))
@@ -44,7 +44,7 @@ def one_point_sample(Natoms=2):
     kernel.set_params(params)
 
     r = [1.2]*Natoms
-    kernel.init_metric(r, eps = 1e-14)
+    kernel.init_metric(r)
 
     X = np.asarray([np.random.rand(3*Natoms)])
     K = kernel.kernel_matrix(X)
@@ -56,6 +56,13 @@ def one_point_sample(Natoms=2):
     assert np.allclose(K,true_K)
 
 
+def test_g_eigh(Natoms=2):
+    kernel = BondExponential(3*Natoms)
+    params = {"weight": 1.0, "scale": 0.4}
+    kernel.set_params(params)
+    r = [1.2]*Natoms
+    kernel.init_metric(r)
+
 def test_is_symmetric(Natoms=2, Nsamples=1, gradient = False):
 
     kernel = BondExponential(3*Natoms)
@@ -63,7 +70,7 @@ def test_is_symmetric(Natoms=2, Nsamples=1, gradient = False):
     kernel.set_params(params)
 
     r = [1.2]*Natoms
-    kernel.init_metric(r, eps = 1e-14)
+    kernel.init_metric(r)
 
     X = np.asarray([np.random.rand(3*Natoms) for i in range(Nsamples)])
     K = kernel.kernel_matrix(X)
@@ -108,7 +115,7 @@ def first_step(Nattempts=1, l=0.1):
     calcparams = {'noise': 0.001,
                   'kernel': kernel,
                   'kernel_params' : params,
-                  'update_prior_strategy': 'maximum', 
+                  'update_prior_strategy': 'maximum',
                   'train_images': [],
                   'calculate_uncertainty': False,
                   'mask_constraints': False}
@@ -117,19 +124,19 @@ def first_step(Nattempts=1, l=0.1):
 
     # Optimizer
     opt_params = {'model_calculator': ml_calculator,
-                  'optimizer': SP, 
+                  'optimizer': SP,
                   'use_previous_observations': False,
                   'surrogate_starting_point': 'min',
                   'trainingset': [],
-                  'print_format': 'ASE', 
-                  'fit_to': 'constraints', 
+                  'print_format': 'ASE',
+                  'fit_to': 'constraints',
                   'optimizer_kwargs': {'fmax': 'scipy default',
                                        'method': 'L-BFGS-B'}}
 
     atoms0 = atoms.copy()
     optimizer = AIDMin(atoms, logfile = None, **opt_params)
     optimizer.run(fmax=0.01, steps=1)
-                  
+
     x0 = atoms0.get_positions().flatten()
     x1 = atoms.get_positions().flatten()
 
