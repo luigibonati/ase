5ef724f848cf487e2d7d2de539895ce611a9d4ce
Michael Walter <mcoywalter@gmail.com>
2020-04-20 14:53:21 +0200

    WIP Lippincott

--- ase/calculators/bond_polarizability.py
+++ ase/calculators/bond_polarizability.py
@@ -1,13 +1,10 @@
+import re
 import numpy as np
 
 from ase.data import covalent_radii
 from ase.neighborlist import NeighborList
 
-
-class BondPolarizability:
-    """Evaluate polarizability from bonds after
-    Marinov and Zotov Phys. Rev. B 55 (1997) 2938-2944
-    """
+class LippincottStuttman:
     # atomic polarizability values from
     # Lippincott and Stutman J. Phys. Chem. 68 (1964) 2926-2940
     # DOI:10.1021/j100792a033
@@ -18,6 +15,38 @@ class BondPolarizability:
         'O': 0.592,
         'Si': 2.988
     }
+
+    def __init__(self):
+        self._data = {}
+    def __getitem__(self, key):
+        if key not in self.data:
+            el1, el2 = re.findall('[A-Z][^A-Z]*', key)
+            self._data[key] = self.calculate(el1, el2)
+        return self._data[key]
+
+
+class LippincottStuttmanPerp(LippincottStuttman):
+    def calculate(self, el1, el2):
+        return 0
+
+
+class LippincottStuttmanPara(LippincottStuttman):
+    def calculate(self, el1, el2):
+        return 0
+
+    
+
+alphaperp = {
+    'CC': 0.71
+}
+alphapara = {
+    'CC': 1.69
+}
+
+class BondPolarizability:
+    """Evaluate polarizability from bonds after
+    Marinov and Zotov Phys. Rev. B 55 (1997) 2938-2944
+    """
 
     def __call__(self, atoms):
         return self.calculate(atoms)
