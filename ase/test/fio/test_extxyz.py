beaf63794362ee7883558c3518891f61cbfc7a42
James Kermode <james.kermode@gmail.com>
2020-04-30 11:48:45 +0100

    some flake fixes

--- ase/test/fio/test_extxyz.py
+++ ase/test/fio/test_extxyz.py
@@ -308,7 +308,7 @@ def test_stress():
     a_stress = a.get_stress()
     a.write('tmp.xyz')
     b = ase.io.read('tmp.xyz')
-    assert abs(b.get_stress() - sigma).max() < 1e-6
+    assert abs(b.get_stress() - a_stress).max() < 1e-6
     assert abs(b.arrays['stress'] - np.arange(6, dtype=float)).max() < 1e-6
     b_stress = b.info['stress']
     assert abs(full_3x3_to_voigt_6_stress(b_stress) - a_stress).max() < 1e-6
