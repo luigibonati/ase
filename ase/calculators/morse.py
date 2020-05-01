da32751649b0857e7b6b7ab072e21ac5e6217f9c
James Kermode <james.kermode@gmail.com>
2020-05-01 11:56:19 +0100

    apply a smooth cutoff function to morse potential

--- ase/calculators/morse.py
+++ ase/calculators/morse.py
@@ -5,6 +5,28 @@ from math import exp, sqrt
 from ase.calculators.calculator import Calculator
 from ase.neighborlist import neighbor_list
 
+def fcut(r, r0, r1):
+    """
+    Piecewise quintic C^{2,1} regular polynomial for use as a smooth cutoff.
+    Ported from JuLIP.jl, https://github.com/JuliaMolSim/JuLIP.jl 
+    
+    Parameters
+    ----------
+    r0 - inner cutoff radius
+    r1 - outder cutoff radius
+    """""
+    s = 1.0 - (r-r0) / (r1-r0)
+    return (s >= 1.0) + (((0.0 < s) & (s < 1.0)) *
+                         (6.0 * s**5 - 15.0 * s**4 + 10.0 * s**3))
+
+def fcut_d(r, r0, r1):
+    """
+    Derivative of fcut() function defined above
+    """
+    s = 1-(r-r0) / (r1-r0)
+    return -(((0.0 < s) & (s < 1.0)) *
+             ((30*s**4 - 60 * s**3 + 30 * s**2) / (r1-r0)))
+
 class MorsePotential(Calculator):
     """Morse potential.
 
@@ -15,7 +37,8 @@ class MorsePotential(Calculator):
     default_parameters = {'epsilon': 1.0,
                           'rho0': 6.0,
                           'r0': 1.0,
-                          'rc': None}
+                          'rcut1': 1.9,
+                          'rcut2': 2.7}
     nolabel = True
 
     def __init__(self, **kwargs):
@@ -39,18 +62,23 @@ class MorsePotential(Calculator):
         epsilon = self.parameters.epsilon
         rho0 = self.parameters.rho0
         r0 = self.parameters.r0
-        rc = self.parameters.rc
-        if rc is None:
-            rc = 3 * r0
+        rcut1 = self.parameters.rcut1 * r0
+        rcut2 = self.parameters.rcut2 * r0
 
         forces = np.zeros((len(self.atoms), 3))
         preF = - 2 * epsilon * rho0 / r0
 
-        i, j, d, D = neighbor_list('ijdD', atoms, rc)
-        expf = np.exp(rho0 * (1.0 - d/r0))
-        energy = 0.5*(epsilon * expf * (expf - 2)).sum()
+        i, j, d, D = neighbor_list('ijdD', atoms, rcut2)
+        dhat = (D / d[:, None]).T
 
-        F = preF * (expf * (expf - 1) * (D / d[:, None]).T).T
+        expf = np.exp(rho0 * (1.0 - d/r0))
+        fc = fcut(d, rcut1, rcut2)
+
+        E = epsilon * expf * (expf - 2)
+        dE = preF * expf * (expf - 1) * dhat
+        energy = 0.5 * (E * fc).sum()
+
+        F = (dE * fc + E * fcut_d(d, rcut1, rcut2) * dhat).T
         for dim in range(3):
             forces[:, dim] = np.bincount(i, weights=F[:, dim],
                                          minlength=len(atoms))
