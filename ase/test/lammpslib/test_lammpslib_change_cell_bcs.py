97047eaef4e20a6e6f2a0d940bcdbb5b46ec6a58
dskarls <dskpersistent@gmail.com>
2020-05-03 17:14:02 -0700

    TST: Use symlinks to potential files for lammpslib tests
    Currently, these tests generally fail unless you've made sure that
    lammps can find the necessary potential file.  This can be done by
    placing the file in the current directory or elsewhere in the python
    search path, or by setting the LAMMPS_POTENTIALS environment variable
    (see https://lammps.sandia.gov/doc/pair_python.html).  Because the new
    ase-dockers lammps image installs the bundled potential parameter files,
    we can place a symlink to the relevant file in the current directory.

--- ase/test/lammpslib/test_lammpslib_change_cell_bcs.py
+++ ase/test/lammpslib/test_lammpslib_change_cell_bcs.py
@@ -1,9 +1,21 @@
 def test_lammpslib_change_cell_bcs():
     # test that a change in unit cell boundary conditions is
     # handled correctly by lammpslib
+    import os
     import numpy as np
     from ase.calculators.lammpslib import LAMMPSlib
     from ase.lattice.cubic import FaceCenteredCubic
+
+    # If the potential file NiAlH_jea.eam.alloy isn't present in the current
+    # directory, create a symlink for it.  This is done using the installation
+    # path for the potential files in the ase-dockers lammps image,
+    # /usr/local/share/lammps/potentials
+    POTENTIAL_FILE = "NiAlH_jea.eam.alloy"
+    if not os.path.isfile(POTENTIAL_FILE):
+        LAMMPS_POTENTIALS_DIR = os.path.join(os.sep, "usr", "local", "share",
+                                             "lammps", "potentials")
+        os.symlink(os.path.join(LAMMPS_POTENTIALS_DIR, POTENTIAL_FILE),
+                   os.path.join(os.getcwd(), POTENTIAL_FILE))
 
     cmds = ["pair_style eam/alloy",
             "pair_coeff * * NiAlH_jea.eam.alloy Ni H"]
