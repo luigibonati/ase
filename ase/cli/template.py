a27d2e4351ee3a3d1e02005c2cd286bd808104f7
david <davidollodart@gmail.com>
2020-05-07 02:26:18 -0700

    DEV: combine get data functions
    The separate functions have been combined. The list of atoms properties,
    and the branch of atoms properties or calculator properties, is kept.
    This is because it is best to have as many classifications of the properties
    as possible to make a decision tree with the first two nodes being atoms data
     or calculator data. However, the first check does an exact match and
    then redundant matches and partial matches. Given the conventions, e.g.,
    to prepend 'a' for average, a decision tree based on position in string
    isn't possible.

--- ase/cli/template.py
+++ ase/cli/template.py
@@ -87,46 +87,6 @@ def sort2rank(sort):
 num2sym = dict(zip(np.argsort(chemical_symbols), chemical_symbols))
 sym2num = {v: k for k, v in num2sym.items()}
 
-
-def get_field_data_atoms(atoms1, atoms2, field):
-    if field[0] == 'r':
-        field = field[1:]
-        rank_order = True
-    else:
-        rank_order = False
-
-    if field == 't':
-        data = atoms1.get_tags()
-    elif field == 'an':
-        data = atoms1.numbers
-    elif field == 'el':
-        data = np.array([sym2num[sym] for sym in atoms1.symbols])
-    elif field == 'i':
-        data = np.arange(len(atoms1))
-    else:
-        if field.startswith('d'):
-            y = atoms2.positions - atoms1.positions
-        elif field.startswith('p'):
-            if field[1] == '1':
-                y = atoms1.positions
-            else:
-                y = atoms2.positions
-
-        if field.endswith('x'):
-            data = y[:, 0]
-        elif field.endswith('y'):
-            data = y[:, 1]
-        elif field.endswith('z'):
-            data = y[:, 2]
-        else:
-            data = np.linalg.norm(y, axis=1)
-
-    if rank_order:
-        return sort2rank(np.argsort(-data))
-
-    return data
-
-
 atoms_props = [
     'dx',
     'dy',
@@ -147,34 +107,57 @@ atoms_props = [
 
 
 def get_field_data(atoms1, atoms2, field):
-
-    if field.lstrip('r') in atoms_props:
-        return get_field_data_atoms(atoms1, atoms2, field)
-
     if field[0] == 'r':
         field = field[1:]
         rank_order = True
     else:
         rank_order = False
 
-    if field[0] == 'd':
-        y = atoms2.get_forces() - atoms1.get_forces()
-    elif field[0] == 'a':
-        y = (atoms2.get_forces() + atoms1.get_forces()) / 2
-    else:
-        if field[1] == '1':
-            y = atoms1.get_forces()
+    if field in atoms_props:
+        if field == 't':
+            data = atoms1.get_tags()
+        elif field == 'an':
+            data = atoms1.numbers
+        elif field == 'el':
+            data = np.array([sym2num[sym] for sym in atoms1.symbols])
+        elif field == 'i':
+            data = np.arange(len(atoms1))
         else:
-            y = atoms2.get_forces()
+            if field.startswith('d'):
+                y = atoms2.positions - atoms1.positions
+            elif field.startswith('p'):
+                if field[1] == '1':
+                    y = atoms1.positions
+                else:
+                    y = atoms2.positions
 
-    if field.endswith('x'):
-        data = y[:, 0]
-    elif field.endswith('y'):
-        data = y[:, 1]
-    elif field.endswith('z'):
-        data = y[:, 2]
+            if field.endswith('x'):
+                data = y[:, 0]
+            elif field.endswith('y'):
+                data = y[:, 1]
+            elif field.endswith('z'):
+                data = y[:, 2]
+            else:
+                data = np.linalg.norm(y, axis=1)
     else:
-        data = np.linalg.norm(y, axis=1)
+        if field[0] == 'd':
+            y = atoms2.get_forces() - atoms1.get_forces()
+        elif field[0] == 'a':
+            y = (atoms2.get_forces() + atoms1.get_forces()) / 2
+        else:
+            if field[1] == '1':
+                y = atoms1.get_forces()
+            else:
+                y = atoms2.get_forces()
+
+        if field.endswith('x'):
+            data = y[:, 0]
+        elif field.endswith('y'):
+            data = y[:, 1]
+        elif field.endswith('z'):
+            data = y[:, 2]
+        else:
+            data = np.linalg.norm(y, axis=1)
 
     if rank_order:
         return sort2rank(np.argsort(-data))
