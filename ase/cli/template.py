cd6ad30740029389176a4b79185c10cd9a3944c4
david <davidollodart@gmail.com>
2020-05-07 03:43:40 -0700

    DEV: change precision round to base 10

--- ase/cli/template.py
+++ ase/cli/template.py
@@ -60,9 +60,9 @@ def prec_round(a, prec=2):
         return a
     else:
         s = 1 if a > 0 else -1
-        m = np.log(s * a) // 1
-        c = np.log(s * a) % 1
-    return s * np.round(np.exp(c), prec) * np.exp(m)
+        m = np.log10(s * a) // 1
+        c = np.log10(s * a) % 1
+    return s * np.round(10**c, prec) * 10**m
 
 
 prec_round = np.vectorize(prec_round)
