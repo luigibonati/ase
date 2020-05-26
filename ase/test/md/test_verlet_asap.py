c0a8dafcd4dda67b27a550e3b66264d4eb0d620d
Jakob Schiotz <schiotz@fysik.dtu.dk>
2020-05-26 15:49:36 +0200

    PEP8 compliance with autopep8 (and manual intervention).

--- ase/test/md/test_verlet_asap.py
+++ ase/test/md/test_verlet_asap.py
@@ -5,14 +5,15 @@ from ase.io import Trajectory, read
 from ase.utils import seterr
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution, Stationary
 
+
 def test_verlet_asap(asap3):
     with seterr(all='raise'):
-        a = bulk('Au').repeat((2,2,2))
+        a = bulk('Au').repeat((2, 2, 2))
         a[5].symbol = 'Ag'
         a.pbc = (True, True, False)
         print(a)
         a.calc = asap3.EMT()
-        MaxwellBoltzmannDistribution(a, 300*kB, force_temp=True)
+        MaxwellBoltzmannDistribution(a, 300 * kB, force_temp=True)
         Stationary(a)
         assert abs(a.get_temperature() - 300) < 0.0001
         print(a.get_forces())
