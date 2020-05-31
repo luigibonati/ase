ac5a3f5c9a44fdfb3e5ef0bc9c34ae877f73fe93
Tristan Maxson <tgmaxson@gmail.com>
2020-05-31 00:22:36 +0000

    Rework root_surface and test_root_surf

--- ase/build/root.py
+++ ase/build/root.py
@@ -1,8 +1,6 @@
-from math import atan2, ceil, cos, sin, log10
-
-import numpy as np
-
+from math import log10, atan2, cos, sin
 from ase.build import hcp0001, fcc111, bcc111
+import numpy as np
 
 
 def hcp0001_root(symbol, root, size, a=None, c=None,
@@ -55,136 +53,26 @@ def bcc111_root(symbol, root, size, a=None,
     return atoms
 
 
-def root_surface(primitive_slab, root, swap_alpha=False, eps=1e-8):
-    """Creates a cell from a primitive cell that repeats along the x and y
-    axis in a way consistent with the primitive cell, that has been cut
-    to have a side length of *root*.
+def point_in_cell_2d(point, cell, eps=1e-8):
+    def tri_area(t1, t2, t3):
+        t1x, t1y = t1[0:2]
+        t2x, t2y = t2[0:2]
+        t3x, t3y = t3[0:2]
+        return abs(t1x*(t2y-t3y) + t2x*(t3y-t1y) + t3x*(t1y-t2y))/2
 
-    *primitive cell* should be a primitive 2d cell of your slab, repeated
-    as needed in the z direction.
+    c0 = (0, 0)
+    c1 = (cell[0][0:2])
+    c2 = (cell[1][0:2])
+    c3 = c1 + c2
 
-    *root* should be determined using an analysis tool such as the
-    root_surface_analysis function, or prior knowledge. It should always
-    be a whole number as it represents the number of repetitions.
+    cA = tri_area(c0, c1, c2) + tri_area(c1, c2, c3)
 
-    *swap_alpha* swaps the alpha angle of the cell."""
+    pA = tri_area(point, c0, c1) + tri_area(point, c1, c2) + tri_area(point, c2, c3) + tri_area(point, c3, c0)
 
-    logeps = int(-log10(eps))
-    atoms = primitive_slab.copy()
-
-    xscale = np.linalg.norm(atoms.cell[0][0:2])
-    xx, xy = atoms.cell[0][0:2] / xscale
-    yx, yy = atoms.cell[1][0:2] / xscale
-    cell_vectors = [[xx, xy], [yx, yy]]
-
-    # Make (0, 0) corner's angle flip from acute to obtuse or
-    # obtuse to acute with a small trick
-    if swap_alpha:
-        cell_vectors[1][0] *= -1
-
-    # Manipulate the cell vectors to find the best search zone and
-    # cast to numpy array.
-    cell_vectors = np.array(cell_vectors)
-    cell_vectors_mag = [np.linalg.norm(x) for x in cell_vectors]
-    cell_search = [int(ceil(float(root * 1.2) / float(x)))
-                   for x in cell_vectors_mag]
-
-    # Make these variables in function scope
-    # x,  y  = Raw grid point
-    # tx, ty = Transformed grid point
-    x, y, tx, ty = 0, 0, 0, 0
-
-    # Calculate square distances and break when appropriate
-    for x in range(cell_search[0]):
-        for y in range(cell_search[1]):
-            if x == y == 0:
-                continue
-            vect = (cell_vectors[0] * x) + (cell_vectors[1] * y)
-            dist = round((vect ** 2).sum(), logeps)
-            if dist == root:
-                tx, ty = vect
-                break
-        else:
-            continue
-        break
-    else:
-        # A root cell could not be found for this combination
-        raise RuntimeError("Can't find a root cell of {0} in [{1}, {2}]".
-                           format(root, cell_vectors[0], cell_vectors[1]))
-
-    tmag = np.linalg.norm((tx, ty))
-    root_angle = -atan2(ty, tx)
-    cell_scale = tmag / cell_vectors_mag[0]
-
-    root_rotation = [[cos(root_angle), -sin(root_angle)],
-                     [sin(root_angle), cos(root_angle)]]
-    cell = [np.dot(x, root_rotation) * cell_scale for x in cell_vectors]
-
-    def pretrim(atoms):
-        cell = atoms.cell
-        pos = atoms.positions
-
-        vertices = np.array([[0, 0],
-                             cell[0][0:2],
-                             cell[1][0:2],
-                             cell[0][0:2] + cell[1][0:2]])
-
-        mins = vertices.min(axis=0)
-        maxs = vertices.max(axis=0)
-
-        out = np.where(np.logical_not((pos[:, 0] >= mins[0] - eps * 10) &
-                                      (pos[:, 0] <= maxs[0] + eps * 10) &
-                                      (pos[:, 1] >= mins[1] - eps * 10) &
-                                      (pos[:, 1] <= maxs[1] + eps * 10)))
-
-        del atoms[out]
-
-    def remove_doubles(atoms, shift=True):
-        shift_vector = np.array([eps * 100, eps * 200, eps * 300])
-        if shift:
-            atoms.translate(shift_vector)
-        atoms.wrap()
-        valid = [0]
-        for x in range(len(atoms)):
-            for ypos, y in enumerate(valid):
-                xa = atoms[x].position
-                ya = atoms[y].position
-                if np.linalg.norm(xa - ya) < eps:
-                    break
-            else:
-                valid.append(x)
-        del atoms[[i for i in range(len(atoms)) if i not in valid]]
-        if shift:
-            atoms.translate(shift_vector * -1)
-
-    atoms_cell_mag = [np.linalg.norm(x)
-                      for x in np.array(atoms.cell[0:2, 0:2])]
-    cell_vect_mag = [np.linalg.norm(x) for x in np.array(cell_vectors)]
-    cell_scale = np.divide(atoms_cell_mag, cell_vect_mag)
-    atoms *= (cell_search[0], cell_search[1], 1)
-    atoms.cell[0:2, 0:2] = cell * cell_scale
-    atoms.center()
-    pretrim(atoms)
-    remove_doubles(atoms, shift=False)
-    remove_doubles(atoms, shift=True)
-
-    def rot(vector, angle):
-        return [(vector[0] * cos(angle)) - (vector[1] * sin(angle)),
-                (vector[0] * sin(angle)) + (vector[1] * cos(angle))]
-    angle = -atan2(atoms.cell[0][1], atoms.cell[0][0])
-    atoms.cell[0][0:2] = rot(atoms.cell[0][0:2], angle)
-    atoms.cell[1][0:2] = rot(atoms.cell[1][0:2], angle)
-    for atom in atoms:
-        atom.position[0:2] = rot(atom.position[0:2], angle)
-    atoms.center()
-
-    atoms.positions = np.around(atoms.positions, decimals=logeps)
-    ind = np.lexsort(
-        (atoms.positions[:, 0], atoms.positions[:, 1], atoms.positions[:, 2],))
-    return atoms[ind]
+    return pA <= cA + eps
 
 
-def root_surface_analysis(primitive_slab, root, allow_above=False, eps=1e-8):
+def root_surface_analysis(primitive_slab, root, root_point=False, eps=1e-8):
     """A tool to analyze a slab and look for valid roots that exist, up to
        the given root. This is useful for generating all possible cells
        without prior knowledge.
@@ -193,37 +81,91 @@ def root_surface_analysis(primitive_slab, root, allow_above=False, eps=1e-8):
 
        *root* is the desired root to find, and all below.
 
-       *allow_above* allows you to also include cells above
-       the given *root* if found in the process.  Otherwise these
-       are trimmed off."""
+       *root_point* returns the cell_points and the index of the root
 
+       """
+
+    # Setup parameters for cell searching
     logeps = int(-log10(eps))
-    atoms = primitive_slab
-    # Normalize the x axis to a distance of 1, and use the cell
-    # We ignore the z axis because this code cannot handle it
-    xscale = np.linalg.norm(atoms.cell[0][0:2])
-    xx, xy = atoms.cell[0][0:2] / xscale
-    yx, yy = atoms.cell[1][0:2] / xscale
-    cell_vectors = [[xx, xy], [yx, yy]]
 
-    # Manipulate the cell vectors to find the best search zone and
-    # cast to numpy array.
-    cell_vectors = np.array(cell_vectors)
-    cell_vectors_mag = [np.linalg.norm(x) for x in cell_vectors]
-    cell_search = [int(ceil(float(root * 1.2) / float(x)))
-                   for x in cell_vectors_mag]
+    xscale = np.linalg.norm(primitive_slab.cell[0][0:2])
+    xx, xy = primitive_slab.cell[0][0:2] / xscale
+    yx, yy = primitive_slab.cell[1][0:2] / xscale
+    cell_vectors = np.array([[xx, xy], [yx, yy]])
 
-    # Returns valid roots that are found in the given search
-    # space.  To find more, use a higher root.
-    valid = set()
-    for x in range(cell_search[0]):
-        for y in range(cell_search[1]):
-            if x == y == 0:
-                continue
-            vect = (cell_vectors[0] * x) + (cell_vectors[1] * y)
-            dist = round((vect ** 2).sum(), logeps)
-            # Only integer roots make sense logically
-            if dist.is_integer():
-                if dist <= root or allow_above:
-                    valid.add(int(dist))
-    return sorted(list(valid))
+    # Allocate grid for cell search search
+    points = np.indices((root+1, root+1)).T.reshape(-1, 2)
+
+    # Find points corresponding to full cells
+    cell_points = [cell_vectors[0] * x + cell_vectors[1] * y for x, y in points]
+
+    # Find point close to the desired cell (floating point error possible)
+    roots = np.around(np.linalg.norm(cell_points, axis=1)**2, logeps)
+
+    if root_point:
+        valid_roots = np.nonzero(roots == root)[0]
+        if len(valid_roots) == 0:
+            raise ValueError("Invalid root {} for cell {}".format(root, cell_vectors))
+        return cell_points, cell_points[np.nonzero(roots == root)[0][0]]
+    else:
+        roots = np.array([int(this_root) for this_root in roots
+                          if this_root.is_integer() and this_root <= root])
+        return set(roots[1:])
+
+
+def root_surface(primitive_slab, root, eps=1e-8):
+    """Creates a cell from a primitive cell that repeats along the x and y
+    axis in a way consisent with the primitive cell, that has been cut
+    to have a side length of *root*.
+
+    *primitive cell* should be a primitive 2d cell of your slab, repeated
+    as needed in the z direction.
+
+    *root* should be determined using an analysis tool such as the
+    root_surface_analysis function, or prior knowledge. It should always
+    be a whole number as it represents the number of repetitions."""
+
+    atoms = primitive_slab.copy()
+
+    xscale = np.linalg.norm(primitive_slab.cell[0][0:2])
+    xx, xy = primitive_slab.cell[0][0:2] / xscale
+    yx, yy = primitive_slab.cell[1][0:2] / xscale
+    cell_vectors = np.array([[xx, xy], [yx, yy]])
+
+    # Do root surface analysis
+    cell_points, root_point = root_surface_analysis(primitive_slab, root, root_point=True, eps=eps)
+
+    # Find new cell
+    root_angle = -atan2(root_point[1], root_point[0])
+    root_rotation = [[cos(root_angle), -sin(root_angle)],
+                     [sin(root_angle), cos(root_angle)]]
+    root_scale = np.linalg.norm(root_point)
+
+    cell = np.array([np.dot(x, root_rotation) * root_scale for x in cell_vectors])
+
+    # Find all cell centers within the cell
+    shift = cell_vectors.sum(axis=0)/2
+    cell_points = [point for point in cell_points if point_in_cell_2d(point+shift, cell, eps=eps)]
+
+    # Setup new cell
+    atoms.rotate(root_angle, v="z")
+    atoms *= (root, root, 1)
+    atoms.cell[0:2, 0:2] = cell * xscale
+    atoms.center()
+
+    # Remove all extra atoms
+    del atoms[[atom.index for atom in atoms if not point_in_cell_2d(atom.position, atoms.cell, eps=eps)]]
+
+    # Rotate cell back to original orientation
+    standard_rotation = [[cos(-root_angle), -sin(-root_angle), 0],
+                         [sin(-root_angle), cos(-root_angle),  0],
+                         [0,                0,                 1]]
+
+    new_cell = np.array([np.dot(x, standard_rotation) for x in atoms.cell])
+    new_positions = np.array([np.dot(x, standard_rotation) for x in atoms.positions])
+
+    atoms.cell = new_cell
+    atoms.positions = new_positions
+
+    return atoms
+                                                                            
