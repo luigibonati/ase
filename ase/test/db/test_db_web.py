408fcb478b46bbf99658fb349342ffabdaafc188
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-06-03 11:20:20 +0200

    Write a proper test

--- ase/test/db/test_db_web.py
+++ ase/test/db/test_db_web.py
@@ -1,12 +1,10 @@
-def test_db_web():
+import pytest
+
+
+@pytest.fixture(scope='module')
+def database():
     from ase import Atoms
     from ase.db import connect
-    from pytest import importorskip
-
-    importorskip('flask')
-    import ase.db.app as app
-
-
 
     db = connect('test.db', append=False)
     x = [0, 1, 2]
@@ -23,14 +21,52 @@ def test_db_web():
              data={'x': x,
                    't1': t1,
                    't2': t2})
-    app.add_project(db)
+    db.write(atoms)
+
+    return db
+
+
+@pytest.fixture(scope='module')
+def client(database):
+    pytest.importorskip('flask')
+    import ase.db.app as app
+
+    app.add_project(database)
     app.app.testing = True
-    c = app.app.test_client()
+    return app.app.test_client()
+
+
+def test_add_columns(database):
+    from ase.db.web import Session
+    from ase.db.app import handle_query
+    session = Session('name')
+    project = {'default_columns': ['bar'],
+               'handle_query_function': handle_query}
+    session.update('query', '', {'query': 'id=2'}, project)
+    table = session.create_table(database, 'id', ['foo'])
+    assert table.columns == []
+    assert 'foo' in table.addcolumns
+
+
+def test_db_web(client):
+    import io
+    from ase.db.web import Session
+    from ase.io import read
+    c = client
     page = c.get('/').data.decode()
+    sid = Session.next_id - 1
     assert 'foo' in page
-    p1 = c.get('/default/row/1').data.decode()
-    print(p1)
-    c.get('/default/json/1').data
-    c.get('/default/sqlite/1').data
-    c.get('/default/sqlite?x=1').data
-    c.get('/default/json?x=1').data
+    for url in [f'/update/{sid}/query/bla/?query=id=1',
+                '/default/row/1']:
+        resp = c.get(url)
+        assert resp.status_code == 200
+
+    for type in ['json', 'xyz', 'cif']:
+        url = f'atoms/default/1/{type}'
+        resp = c.get(url)
+        assert resp.status_code == 200
+        atoms = read(io.StringIO(resp.data.decode()), format=type)
+        print(atoms.numbers)
+        if type != 'cif':
+            assert (atoms.numbers == [1, 1, 8]).all()
+        
