cadc7fd63e7fcee56b43f53354fd1674b60e44f5
Patrick Melix <patrick.melix@uni-leipzig.de>
2020-07-19 14:56:34 +0200

    split test into its logic parts

--- ase/test/neb/test_neb_cell.py
+++ ase/test/neb/test_neb_cell.py
@@ -1,10 +1,11 @@
-def test_neb_cell():
-    from ase import Atoms
-    from ase.neb import NEB
-    import numpy as np
+from ase import Atoms
+from ase.neb import NEB
+import numpy as np
 
-    initial = Atoms('H', positions=[(1,0.1,0.1)],cell=[[1,0,0],[0,1,0],[0,0,1]],pbc=True)
-    final = Atoms('H', positions=[(2,0.2,0.1)],cell=[[2,0,0],[0,2,0],[0,0,2]],pbc=True)
+initial = Atoms('H', positions=[(1,0.1,0.1)],cell=[[1,0,0],[0,1,0],[0,0,1]],pbc=True)
+final = Atoms('H', positions=[(2,0.2,0.1)],cell=[[2,0,0],[0,2,0],[0,0,2]],pbc=True)
+
+def test_neb_cell_default():
     images = [initial.copy()]
     images += [initial.copy()]
     images += [final.copy()]
@@ -12,6 +13,9 @@ def test_neb_cell():
     neb.interpolate()
     assert np.allclose(images[1].positions, [1.5,0.15,0.1])
     assert np.allclose(images[1].cell, initial.cell)
+
+
+def test_neb_cell_scaled_coord():
     images = [initial.copy()]
     images += [initial.copy()]
     images += [final.copy()]
@@ -19,6 +23,9 @@ def test_neb_cell():
     neb.interpolate(use_scaled_coord=True)
     assert np.allclose(images[1].positions, [1.0,0.1,0.075])
     assert np.allclose(images[1].cell, initial.cell)
+
+
+def test_neb_cell_interpolate_cell():
     images = [initial.copy()]
     images += [initial.copy()]
     images += [final.copy()]
@@ -26,6 +33,9 @@ def test_neb_cell():
     neb.interpolate(interpolate_cell=True)
     assert np.allclose(images[1].positions, [1.5,0.15,0.1])
     assert np.allclose(images[1].cell, initial.cell*1.5)
+
+
+def test_neb_cell_default_interpolate_cell_scaled_coord():
     images = [initial.copy()]
     images += [initial.copy()]
     images += [final.copy()]
