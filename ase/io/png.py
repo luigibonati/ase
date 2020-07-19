4f55e94c71d757cc15e279b72dd15704354045be
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-07-19 23:37:28 +0200

    remove even more outdated code

--- ase/io/png.py
+++ ase/io/png.py
@@ -19,21 +19,14 @@ class PNG(EPS):
             renderer._renderer.write_png(self.filename)
         else:
             from matplotlib import _png
-            # buffer_rgba does not accept arguments from version 1.2.0
-            # https://github.com/matplotlib/matplotlib/commit/f4fee350f9f
             import matplotlib
-            if LooseVersion(matplotlib.__version__) < '1.2.0':
-                _png.write_png(renderer.buffer_rgba(0, 0),
-                               renderer.width, renderer.height,
-                               self.filename, 72)
-            else:
-                x = renderer.buffer_rgba()
-                try:
-                    _png.write_png(x, self.w, self.h, self.filename, 72)
-                except (TypeError, ValueError):
-                    x = np.frombuffer(x, np.uint8).reshape(
-                        (int(self.h), int(self.w), 4))
-                    _png.write_png(x, self.filename, 72)
+            x = renderer.buffer_rgba()
+            try:
+                _png.write_png(x, self.w, self.h, self.filename, 72)
+            except (TypeError, ValueError):
+                x = np.frombuffer(x, np.uint8).reshape(
+                    (int(self.h), int(self.w), 4))
+                _png.write_png(x, self.filename, 72)
 
 
 def write_png(filename, atoms, **parameters):
