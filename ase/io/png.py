ba22bc5598e1b2119632cf359f06d3b56a695d1c
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-07-20 14:03:37 +0200

    grrr

--- ase/io/png.py
+++ ase/io/png.py
@@ -9,10 +9,16 @@ class PNG(EPS):
         self.renderer = RendererAgg(self.w, self.h, dpi)
 
     def write_trailer(self):
+        # The array conversion magic is necessary to make things work with
+        # matplotlib 2.0.0, 3.2.x, and 3.3.0 at the same time.
         import matplotlib.image
-        rgba_buffer = np.asarray(self.renderer.buffer_rgba())
+        buf = self.renderer.buffer_rgba()
+        # Buf is of type bytes (matplotlib < 3.3.0) or memoryview.
+        # That might be an implementation detail.
+        array = np.frombuffer(buf, dtype=np.uint8).reshape(
+            int(self.h), int(self.w), 4)
         matplotlib.image.imsave(
-            self.filename, rgba_buffer, format="png")
+            self.filename, array, format="png")
 
 
 def write_png(filename, atoms, **parameters):
