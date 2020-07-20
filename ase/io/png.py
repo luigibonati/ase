5e8fac165d782f1b2ae9cf8d76dec7fcc9417cab
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-07-20 04:35:32 +0200

    use matplotlib.image.imsave on the rgba buffer directly, deleting other old/unnecessary code

--- ase/io/png.py
+++ ase/io/png.py
@@ -1,27 +1,18 @@
-from distutils.version import LooseVersion
-
-import numpy as np
-
 from ase.io.eps import EPS
 
 
 class PNG(EPS):
     def write_header(self):
         from matplotlib.backends.backend_agg import RendererAgg
-
         dpi = 72
         self.renderer = RendererAgg(self.w, self.h, dpi)
 
     def write_trailer(self):
-        from matplotlib import _png
-        import matplotlib
-        x = self.renderer.buffer_rgba()
-        try:
-            _png.write_png(x, self.w, self.h, self.filename, 72)
-        except (TypeError, ValueError):
-            x = np.frombuffer(x, np.uint8).reshape(
-                (int(self.h), int(self.w), 4))
-            _png.write_png(x, self.filename, 72)
+        import matplotlib.image
+        rgba_buffer = self.renderer.buffer_rgba()
+
+        matplotlib.image.imsave(
+            self.filename, rgba_buffer, format="png")
 
 
 def write_png(filename, atoms, **parameters):
