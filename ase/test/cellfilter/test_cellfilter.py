bf46b1ef9483adf7c6cee14dc13ff16ae8abf649
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-07-27 15:31:58 +0200

    simplify test

--- ase/test/cellfilter/test_cellfilter.py
+++ ase/test/cellfilter/test_cellfilter.py
@@ -5,18 +5,15 @@ from ase.units import GPa
 from ase.build import bulk
 from ase.calculators.test import gradient_test
 from ase.constraints import UnitCellFilter, ExpCellFilter
-from ase.optimize import FIRE, LBFGSLineSearch
-
+from ase.optimize import FIRE, LBFGS, MDMin
+from ase.io import Trajectory
 
 @pytest.fixture
 def atoms(asap3):
     rng = np.random.RandomState(0)
     atoms = bulk('Cu', cubic=True)
-
-    # Perturb:
     atoms.positions[:, 0] *= 0.995
     atoms.cell += rng.uniform(-1e-2, 1e-2, size=9).reshape((3,3))
-
     atoms.calc = asap3.EMT()
     return atoms
 
@@ -29,7 +26,7 @@ def test_pressure(atoms, cellfilter):
     f, fn = gradient_test(xcellfilter)
     assert abs(f - fn).max() < 5e-6
 
-    opt = LBFGSLineSearch(xcellfilter)
+    opt = LBFGS(xcellfilter)
     opt.run(1e-3)
 
     # check pressure is within 0.1 GPa of target
@@ -38,44 +35,26 @@ def test_pressure(atoms, cellfilter):
     assert abs(pressure - 10.0) < 0.1
 
 
-def test_cellfilter(atoms):
-    ucf = UnitCellFilter(atoms)
-    f, fn = gradient_test(ucf)
+@pytest.mark.parametrize('cellfilter', [UnitCellFilter, ExpCellFilter])
+def test_cellfilter(atoms, cellfilter):
+    xcellfilter = cellfilter(atoms)
+    f, fn = gradient_test(xcellfilter)
     assert abs(f - fn).max() < 3e-6
-
-
-def test_expcellfilter(atoms):
-    ecf = ExpCellFilter(atoms)
-    # test all derivatives
-    f, fn = gradient_test(ecf)
-    assert abs(f - fn).max() < 3e-6
-
-
-from math import sqrt
-from ase import Atoms
-from ase.optimize import LBFGS
-from ase.constraints import UnitCellFilter
-from ase.io import Trajectory
-from ase.optimize.mdmin import MDMin
 
 
 # XXX This test should have some assertions!  --askhl
 def test_unitcellfilter(asap3):
-    a = 3.6
-    b = a / 2
-    cu = Atoms('Cu',
-               cell=[(0, b, b), (b, 0, b), (b, b, 0)],
-               pbc=1) * (6, 6, 6)
+    cu = bulk('Cu') * (6, 6, 6)
     cu.calc = asap3.EMT()
     f = UnitCellFilter(cu, [1, 1, 1, 0, 0, 0])
     opt = LBFGS(f)
     t = Trajectory('Cu-fcc.traj', 'w', cu)
     opt.attach(t)
     opt.run(5.0)
+    # No assertions??
 
-    # HCP:
-    from ase.build import bulk
-    cu = bulk('Cu', 'hcp', a=a / sqrt(2))
+def test_unitcellfilter_hcp(asap3):
+    cu = bulk('Cu', 'hcp', a=3.6 / 2.0**0.5)
     cu.cell[1,0] -= 0.05
     cu *= (6, 6, 3)
     cu.calc = asap3.EMT()
@@ -86,3 +65,4 @@ def test_unitcellfilter(asap3):
     t = Trajectory('Cu-hcp.traj', 'w', cu)
     opt.attach(t)
     opt.run(0.2)
+    # No assertions??
