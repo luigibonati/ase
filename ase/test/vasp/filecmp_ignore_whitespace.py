bc44d2634e3fca137138cc2ae13deba32ec86d7d
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2020-07-30 11:48:53 -0400

    Cleaner implementation of guts of filecmp_ignore_whitespace

--- ase/test/vasp/filecmp_ignore_whitespace.py
+++ ase/test/vasp/filecmp_ignore_whitespace.py
@@ -1,31 +1,18 @@
 import re
 import itertools
 
+def normalize_file_whitespace(lines):
+    """remove initial and final whitespace on each line, replace any interal
+    whitespace with one space, and remove trailing blank lines"""
+
+    lines_out = []
+    for l in lines.strip().splitlines():
+        lines_out.append(re.sub('\s+', ' ', l.strip()))
+    return '\n'.join(lines_out)
+
 def filecmp_ignore_whitespace(f1, f2):
     """Compare two files ignoring all leading and trailing whitespace, amount of 
     whitespace within lines, and any trailing whitespace-only lines."""
 
-    with open(f1) as ff1, open(f2) as ff2:
-        # check for mismatching lines
-        for l1, l2 in itertools.zip_longest(ff1, ff2):
-            if l1 is None or l2 is None:
-                # reached end of one file or the other
-                break
-            if re.sub(r'\s+', ' ', l1.strip()) != re.sub(r'\s+', ' ', l2.strip()):
-                return False
-        # check for additional non-blank lines after f1 finished
-        if l1 is None:
-            if len(l2.strip()) > 0:
-                return False
-            for l2 in ff2:
-                if len(l2.strip()) > 0:
-                    return False
-        # check for additional non-blank lines after f2 finished
-        if l2 is None:
-            if len(l1.strip()) > 0:
-                return False
-            for l1 in ff1:
-                if len(l1.strip()) > 0:
-                    return False
-
-    return True
+    return (normalize_file_whitespace(open(f1).read()) ==
+            normalize_file_whitespace(open(f2).read()))
