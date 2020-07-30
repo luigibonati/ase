a8ac0609781b4229bb51374604a80f0e401bd8a2
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2020-07-30 14:43:08 -0400

    Fix arg name and closing of files in filecmp_ignore_whitespace.py

--- ase/test/vasp/filecmp_ignore_whitespace.py
+++ ase/test/vasp/filecmp_ignore_whitespace.py
@@ -1,12 +1,12 @@
 import re
 
 
-def normalize_file_whitespace(lines):
+def normalize_file_whitespace(text):
     """remove initial and final whitespace on each line, replace any interal
     whitespace with one space, and remove trailing blank lines"""
 
     lines_out = []
-    for l in lines.strip().splitlines():
+    for l in text.strip().splitlines():
         lines_out.append(re.sub(r'\s+', ' ', l.strip()))
     return '\n'.join(lines_out)
 
@@ -15,5 +15,7 @@ def filecmp_ignore_whitespace(f1, f2):
     """Compare two files ignoring all leading and trailing whitespace, amount of
     whitespace within lines, and any trailing whitespace-only lines."""
 
-    return (normalize_file_whitespace(open(f1).read()) ==
-            normalize_file_whitespace(open(f2).read()))
+    with open(f1) as f1_o, open(f2) as f2_o:
+        same = (normalize_file_whitespace(open(f1).read()) ==
+                normalize_file_whitespace(open(f2).read()))
+    return same
