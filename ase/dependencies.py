4ff12b9380df695dda2afc96af77fc1b84c4ff5e
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-08-02 17:31:47 +0200

    grrr fix warning

--- ase/dependencies.py
+++ ase/dependencies.py
@@ -9,15 +9,16 @@ def format_dependency(modname: str) -> Tuple[str, str]:
         module = importlib.import_module(modname)
     except ImportError:
         return modname, 'not installed'
-    else:
-        version = getattr(module, '__version__', '?')
-        name = f'{modname}-{version}'
-        if modname == 'ase':
-            githash = search_current_git_hash(module)
-            if githash:
-                name += '-{:.10}'.format(githash)
 
-        return name, str(module.__path__[0])
+    version = getattr(module, '__version__', '?')
+    name = f'{modname}-{version}'
+    if modname == 'ase':
+        githash = search_current_git_hash(module)
+        if githash:
+            name += '-{:.10}'.format(githash)
+
+    # (only packages have __path__, but we are importing packages.)
+    return name, str(module.__path__[0])  # type: ignore
 
 
 def all_dependencies() -> List[Tuple[str, str]]:
