1db14e25b9dba196245e61763ed0ff9f800ff934
Alexander Sougaard Tygesen <atygesen@hotmail.com>
2020-08-04 13:01:11 +0200

    Filter deprecation warning

--- ase/calculators/vasp/create_input.py
+++ ase/calculators/vasp/create_input.py
@@ -24,11 +24,12 @@ import warnings
 import shutil
 from os.path import join, isfile, islink
 from typing import List
+import copy
 
 import numpy as np
 
 from ase.calculators.calculator import kpts2ndarray
-from ase.calculators.vasp.setups import setups_defaults
+from ase.calculators.vasp.setups import setups_defaults as _setups_defaults
 
 # Parameters that can be set in INCAR. The values which are None
 # are not written and default parameters of VASP are used for them.
@@ -1001,6 +1002,10 @@ class GenerateVaspInput:
         # corresponding values are suffixes or the full name of the setup
         # folder, respectively.
 
+        # Avoid mutating the module dictionary, so we use a copy instead
+        # Note, it is a nested dict, so a regular copy is not enough
+        setups_defaults = copy.deepcopy(_setups_defaults)
+
         # Default to minimal basis
         if p['setups'] is None:
             p['setups'] = {'base': 'minimal'}
@@ -1464,9 +1469,9 @@ class GenerateVaspInput:
         column. It is used for restart purposes to get sorting right
         when reading in an old calculation to ASE."""
 
-        file = open(join(directory, 'ase-sort.dat'), 'w')
-        for n in range(len(self.sort)):
-            file.write('%5i %5i \n' % (self.sort[n], self.resort[n]))
+        with open(join(directory, 'ase-sort.dat'), 'w') as file:
+            for _sort, _resort in zip(self.sort, self.resort):
+                print(f'{_sort:d} {_resort:d}', file=file)
 
 # The below functions are used to restart a calculation and are under early
 # constructions
