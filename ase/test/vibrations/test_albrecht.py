e992f9c1173aab746510e330e3e690898453c12d
Michael Walter <mcoywalter@gmail.com>
2020-08-05 15:54:48 +0200

    Add multiples test

--- ase/test/vibrations/test_albrecht.py
+++ ase/test/vibrations/test_albrecht.py
@@ -7,40 +7,67 @@ from ase.calculators.h2morse import (H2Morse,
 from ase.vibrations.albrecht import Albrecht
 
 
-def test_overlap():
-    name = 'rrmorse'
-    atoms = H2Morse()
-    om = 1
-    gam = 0.1
+@pytest.fixture
+def atoms():
+    return H2Morse()
+
+
+@pytest.fixture
+def rrname(tmp_path, atoms):
+    """Prepare the Resonant Raman calculation"""
+    name = str(tmp_path / 'rrmorse')
 
     rmc = ResonantRamanCalculator(atoms, H2MorseExcitedStatesCalculator,
                                   overlap=lambda x, y: x.overlap(y),
                                   name=name, txt='-')
     rmc.run()
 
+    return name
+
+
+def test_one_state(rrname, atoms):
     """One state only"""
+    om = 1
+    gam = 0.1
 
     ao = Albrecht(atoms, H2MorseExcitedStates,
                   exkwargs={'nstates': 1},
-                  name=name, overlap=True,
+                  name=rrname, overlap=True,
                   approximation='Albrecht A', txt=None)
     aoi = ao.absolute_intensity(omega=om, gamma=gam)[-1]
 
     al = Albrecht(atoms, H2MorseExcitedStates,
                   exkwargs={'nstates': 1},
-                  name=name, approximation='Albrecht A', txt=None)
+                  name=rrname, approximation='Albrecht A', txt=None)
     ali = al.absolute_intensity(omega=om, gamma=gam)[-1]
     assert ali == pytest.approx(aoi, 1e-9)
 
+
+def test_all_states(rrname, atoms):
     """Include degenerate states"""
+    om = 1
+    gam = 0.1
 
     ao = Albrecht(atoms, H2MorseExcitedStates,
-                  name=name, overlap=True,
+                  name=rrname, overlap=True,
                   approximation='Albrecht A', txt=None)
 
     aoi = ao.absolute_intensity(omega=om, gamma=gam)[-1]
 
     al = Albrecht(atoms, H2MorseExcitedStates,
-                  name=name, approximation='Albrecht A', txt=None)
+                  name=rrname, approximation='Albrecht A', txt=None)
     ali = al.absolute_intensity(omega=om, gamma=gam)[-1]
     assert ali == pytest.approx(aoi, 1e-5)
+
+
+def test_multiples(rrname, atoms):
+    """Run multiple vibrational excitations"""
+    om = 1
+    gam = 0.1
+    
+    ao = Albrecht(atoms, H2MorseExcitedStates,
+                  name=rrname, overlap=True, combinations=2,
+                  approximation='Albrecht A', txt=None)
+
+    aoi = ao.intensity(omega=om, gamma=gam)
+    assert len(aoi) == 27
