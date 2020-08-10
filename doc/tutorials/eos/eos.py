2d9b25e475b42bc2c9f207a0a48bf8ad7cfee913
Anubhab Haldar <jinxhaldar@gmail.com>
2020-08-10 05:42:47 +0000

    More cleaning of commented out code.

--- doc/tutorials/eos/eos.py
+++ doc/tutorials/eos/eos.py
@@ -2,5 +2,5 @@
 import runpy
 
 
-runpy.run_path('eos1.py')# .read(), 'eos1.py', 'exec'))
-runpy.run_path('eos2.py')# .read(), 'eos2.py', 'exec'))
+runpy.run_path('eos1.py')
+runpy.run_path('eos2.py')
