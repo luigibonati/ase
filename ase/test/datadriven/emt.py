2be51fb29257b0f9b8c442caec0fb9ae4b12b37c
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-09-02 20:48:41 +0200

    forget about the tests, we can do better now

--- ase/test/datadriven/emt.py
+++
@@ -1,9 +0,0 @@
-from ase.test.datadriven.main import test_singlepoint
-from ase.calculators.datadriven import new_emt
-from ase.build import bulk
-
-atoms = bulk('Au') * (2, 2, 2)
-atoms.rattle(stdev=0.2)
-atoms.calc = new_emt()
-
-test_singlepoint('emt', atoms)
