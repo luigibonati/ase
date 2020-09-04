fc1889004415aa337a7eb7a0e4c8f94cbe320b50
yuzie007 <yuji.ikeda.ac.jp@gmail.com>
2020-09-04 14:18:15 +0200

    ENH: Add `wrap` to `ase.io.vasp:write_vasp`

--- ase/io/vasp.py
+++ ase/io/vasp.py
@@ -961,7 +961,8 @@ def write_vasp(filename,
                symbol_count=None,
                long_format=True,
                vasp5=False,
-               ignore_constraints=False):
+               ignore_constraints=False,
+               wrap=None):
     """Method to write VASP position (POSCAR/CONTCAR) files.
 
     Writes label, scalefactor, unitcell, # of various kinds of atoms,
@@ -989,9 +990,15 @@ def write_vasp(filename,
 
     # Write atom positions in scaled or cartesian coordinates
     if direct:
-        coord = atoms.get_scaled_positions()
+        if wrap is None:
+            coord = atoms.get_scaled_positions()
+        else:
+            coord = atoms.get_scaled_positions(wrap=wrap)
     else:
-        coord = atoms.get_positions()
+        if wrap is None:
+            coord = atoms.get_positions()
+        else:
+            coord = atoms.get_positions(wrap=wrap)
 
     constraints = atoms.constraints and not ignore_constraints
 
