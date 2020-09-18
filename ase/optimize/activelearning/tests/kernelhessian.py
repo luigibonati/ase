6446621ae359818bfa7ecd031f5a03f1c15e67c4
Sami Juhani Kaappa <sjuka@einstein.fysik.dtu.dk>
2020-09-18 09:44:20 +0200

    Improve fingerprint tests

--- ase/optimize/activelearning/tests/kernelhessian.py
+++ ase/optimize/activelearning/tests/kernelhessian.py
@@ -1,82 +1,107 @@
-from ase.build import fcc100
+from ase.build import fcc111
 from ase.optimize.activelearning.gpfp.fingerprint import RadialAngularFP
 import numpy as np
 import time
 
-'''
-Test that the Hessian of the squared exponential kernel,
-using the fingerprint, matches with the numerical one.
-'''
+if __name__ == '__main__':
+    '''
+    Test that the Hessian of the squared exponential kernel,
+    using the fingerprint, matches with the numerical one.
+    '''
 
+    fp_class = RadialAngularFP
+    params = dict(limit=6.0, Rlimit=4.0, delta=0.4)
 
-def new_fp():
-    return RadialAngularFP(limit=10.0,
-                           Rlimit=4.0,
-                           delta=0.5,
-                           ascale=0.2)
+    def new_fp():
+        return fp_class(**params)
 
+    def print_matrix(matrix):
+        print()
+        for row in matrix:
+            for val in row:
+                print('{:12.06f}'.format(val), end='')
+            print()
+        print()
 
-# Create slab:
-slab = fcc100('Ag', size=(2, 2, 2))
-slab[-4].symbol = 'Au'
-slab[-2].symbol = 'Au'
-slab.center(axis=2, vacuum=4.0)
-slab.rattle(0.05)
+    # Create slab:
+    slab = fcc111('Ag', size=(2, 2, 2))
+    slab[-4].symbol = 'Au'
+    slab[-2].symbol = 'Au'
+    slab.center(axis=2, vacuum=4.0)
+    slab.rattle(0.05)
 
-fp = new_fp()
-fp.set_atoms(slab)
+    fp = new_fp()
+    fp.set_atoms(slab)
 
-index1 = 1
-index2 = 6
+    index1 = 1
+    index2 = 6
 
-dx = 0.0001
+    dx = 0.0001
 
-# Numerical:
-t0 = time.time()
-hessian = np.zeros([3, 3])
-for cc1 in range(3):
-    for cc2 in range(3):
+    # Numerical:
+    t0 = time.time()
+    hessian = np.zeros([3, 3])
+    for cc1 in range(3):
+        for cc2 in range(3):
 
-        change1, change2 = np.zeros(3), np.zeros(3)
-        change1[cc1] = dx
-        change2[cc2] = dx
+            change1, change2 = np.zeros(3), np.zeros(3)
+            change1[cc1] = dx
+            change2[cc2] = dx
 
-        atoms1 = slab.copy()
-        atoms1.rattle(0.01, seed=1)
-        fp1 = new_fp()
-        fp1.set_atoms(atoms1)
+            atoms1 = slab.copy()
+            atoms1.rattle(0.01, seed=1)
+            fp1 = new_fp()
+            fp1.set_atoms(atoms1)
 
-        atoms2 = atoms1.copy()
-        atoms2[index1].position += change1
-        fp2 = new_fp()
-        fp2.set_atoms(atoms2)
+            atoms2 = atoms1.copy()
+            atoms2[index1].position += change1
+            fp2 = new_fp()
+            fp2.set_atoms(atoms2)
 
-        atoms3 = atoms1.copy()
-        atoms3.rattle(0.02, seed=2)
-        fp3 = new_fp()
-        fp3.set_atoms(atoms3)
+            atoms3 = atoms1.copy()
+            atoms3.rattle(0.02, seed=2)
+            fp3 = new_fp()
+            fp3.set_atoms(atoms3)
 
-        atoms4 = atoms3.copy()
-        atoms4[index2].position += change2
-        fp4 = new_fp()
-        fp4.set_atoms(atoms4)
+            atoms4 = atoms3.copy()
+            atoms4[index2].position += change2
+            fp4 = new_fp()
+            fp4.set_atoms(atoms4)
 
-        hessian[cc1, cc2] = (((fp1.kernel(fp2, fp4) - fp1.kernel(fp2, fp3)) -
-                              (fp1.kernel(fp1, fp4) - fp1.kernel(fp1, fp3)))
-                             / dx**2)
-print('\nNumerical:\n', hessian, '\nTime consumed:',
-      (time.time() - t0), 'seconds\n')
+            hessian[cc1, cc2] = (((fp1.kernel(fp2, fp4) -
+                                   fp1.kernel(fp2, fp3)) -
+                                  (fp1.kernel(fp1, fp4) -
+                                   fp1.kernel(fp1, fp3)))
+                                 / dx**2)
+    print('Numerical:')
+    print_matrix(hessian)
+    print('Time consumed:', (time.time() - t0), 'seconds\n')
 
-# Analytical:
-t0 = time.time()
-atoms1 = slab.copy()
-atoms1.rattle(0.01, seed=1)
-fp1 = new_fp()
-fp1.set_atoms(atoms1)
-atoms3 = atoms1.copy()
-atoms3.rattle(0.02, seed=2)
-fp3 = new_fp()
-fp3.set_atoms(atoms3)
-analytical = fp1.kernel_hessian(fp3)[index1, index2]
-print('\nAnalytical:\n', analytical, '\nTime consumed:',
-      (time.time() - t0), 'seconds\n')
+    # Analytical:
+    t0 = time.time()
+    atoms1 = slab.copy()
+    atoms1.rattle(0.01, seed=1)
+    fp1 = new_fp()
+    fp1.set_atoms(atoms1)
+    atoms3 = atoms1.copy()
+    atoms3.rattle(0.02, seed=2)
+    fp3 = new_fp()
+    fp3.set_atoms(atoms3)
+    analytical = fp1.kernel_hessian(fp3)[index1, index2]
+
+    print('Analytical:')
+    print_matrix(analytical)
+    print('Time consumed:', (time.time() - t0), 'seconds\n')
+
+    try:
+        rtol = 1e-3
+        atol = 1e-8
+        assert(np.allclose(hessian, analytical,
+                           rtol=rtol, atol=atol))
+        txt = 'Kernel Hessians match with relative '
+        txt += 'tolerance {}. OK!'.format(rtol)
+        print(txt)
+    except AssertionError:
+        txt = 'ERROR! Kernel Hessians don\'t match '
+        txt += 'with relative tolerance {}.'.format(rtol)
+        print(txt)
---
+++ ase/optimize/activelearning/tests/test_fp_periodic.py
@@ -0,0 +1,55 @@
+from ase.optimize.activelearning.gpfp.fingerprint import RadialAngularFP
+from ase.build import fcc111
+import numpy as np
+
+if __name__ == '__main__':
+
+    """
+    Test that fingerprint behaves as wanted with
+    periodic boundary conditions, i.e. that re-centering
+    atoms does not affect the fingerprint nor its gradients.
+    """
+
+    import time
+    t0 = time.time()
+
+    # Create slab:
+    slab = fcc111('Ag', size=(2, 2, 2))
+    slab[-4].symbol = 'Au'
+    slab[-2].symbol = 'Au'
+    slab.center(axis=2, vacuum=4.0)
+    slab.rattle(0.05)
+    slab.pbc = (True, True, False)
+
+    fp = RadialAngularFP
+    params = dict(limit=8.0, Rlimit=4.0, delta=0.5, ascale=0.2)
+
+    fp0 = fp(**params)
+    fp0.set_atoms(slab)
+    vec0 = fp0.vector
+
+    slab.positions += np.diag(slab.get_cell()) / 1.56789
+    fp1 = fp(**params)
+    fp1.set_atoms(slab)
+    vec1 = fp1.vector
+
+    d = fp0.distance(fp0, fp1)
+
+    # from ase.visualize import view
+    # view(slab)
+
+    # from matplotlib import pyplot as plt
+    # plt.plot(vec0)
+    # plt.plot(vec1)
+    # plt.show()
+
+    assert(d < 1e-8)
+
+    assert(np.allclose(vec0, vec1, atol=1e-8))
+
+    assert(np.allclose(fp0.gradients, fp1.gradients, atol=1e-8))
+
+    assert(np.allclose(fp0.anglegradients, fp1.anglegradients, atol=1e-8))
+
+    t1 = time.time()
+    print("time: {:.06f} sec".format(t1 - t0))
