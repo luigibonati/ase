5a040c61e9cbf292eaea1c47893f12fc381c44a7
Jakob Schiotz <schiotz@fysik.dtu.dk>
2020-09-22 14:14:01 +0200

    Add test for Melchionna's NPT dynamics with reasonable statistics (asap3 based)

--- ase/test/md/test_npt_asap.py
+++ ase/test/md/test_npt_asap.py
@@ -1,43 +1,37 @@
 from ase.units import fs, kB, GPa
 from ase.build import bulk
-from ase.md.nvtberendsen import NVTBerendsen
+from ase.md.npt import NPT
 from ase.md.nptberendsen import NPTBerendsen
 from ase.md import MDLogger
 from ase.utils import seterr
 from ase.md.velocitydistribution import MaxwellBoltzmannDistribution, Stationary
 import numpy as np
 
-def test_nvt_berendsen_asap(asap3):
-    _berendsen_asap(asap3, None)
 
-def test_npt_berendsen_asap(asap3):
-    _berendsen_asap(asap3, 1.0 * GPa)
-
-
-def _berendsen_asap(asap3, pressure):
-    "Test NVT or NPT Berendsen dynamics.  The pressure should be in atomic units."
+def test_npt_asap(asap3):
     with seterr(all='raise'):
-        rng = np.random.RandomState(None)
+        pressure = 1.0 * GPa
+        rng = np.random.RandomState(0)
         a = bulk('Cu', orthorhombic=True).repeat((6, 6, 6))
         print(a)
         a.calc = asap3.EMT()
-        # Set temperature to 10 K
-        MaxwellBoltzmannDistribution(a, 10 * kB, force_temp=True, rng=rng)
-        Stationary(a)
-        assert abs(a.get_temperature() - 10) < 0.0001
-        # Berendsen dynamics should raise this to 300 K
+        # NPT Dynamics is awful at setting correct pressure and
+        # temperature, but should be good at maintaining it.  Use
+        # NPTBerendsen to hit the right value.
         T = 300
-        if pressure is None:
-            md = NVTBerendsen(a, timestep=4 * fs, temperature=T, taut=2000*fs,
-                                  logfile='-', loginterval=500)
-        else:
-            md = NPTBerendsen(a, timestep=4 * fs, temperature=T, taut=2000*fs,
-                                  pressure=pressure/GPa*1e4, taup=2000*fs,
-                                  compressibility=1 / (140 * 1e4))
-            # We want logging with stress included
-            md.attach(MDLogger(md, a, '-', stress=True), interval=500)
-        md.run(steps=5000)
+        MaxwellBoltzmannDistribution(a, T * kB, force_temp=True, rng=rng)
+        Stationary(a)
+        berend = NPTBerendsen(a, timestep=4 * fs, temperature=T, taut=2000*fs,
+                              pressure=pressure/GPa*1e4, taup=2000*fs,
+                              compressibility=1 / (140 * 1e4),
+                              logfile='-', loginterval=500)
+        berend.run(steps=5000)
         # Now gather the temperature over 10000 timesteps, collecting it every 5 steps
+        ptime = 2000 * fs
+        md = NPT(a, timestep=4 * fs, temperature=T*kB, externalstress=pressure,
+                     ttime=2000 * fs, pfactor=ptime**2/(140 / GPa))
+        # We want logging with stress included
+        md.attach(MDLogger(md, a, '-', stress=True), interval=500)
         temp = []
         press = []
         for i in range(2000):
