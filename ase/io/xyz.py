46c826000fee941048074bb53901b1a445f8c697
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2020-09-23 11:47:24 +0300

    Restore the description of the module

--- ase/io/xyz.py
+++ ase/io/xyz.py
@@ -1,3 +1,9 @@
+"""Reference implementation of reader and writer for standard XYZ files.
+
+See https://en.wikipedia.org/wiki/XYZ_file_format
+
+Note that the .xyz files are handled by the extxyz module by default.
+"""
 from ase.atoms import Atoms
 
 
