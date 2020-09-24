0aa14836c873e2ca86d637d1549293bd9b333f3f
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-09-24 13:55:45 +0200

    totally rewrite external viewer code

--- ase/test/test_external_viewer.py
+++ ase/test/test_external_viewer.py
@@ -3,26 +3,33 @@ from pathlib import Path
 
 import pytest
 
-from ase.visualize import view
-from ase.visualize.external import open_external_viewer
+from ase.visualize import view, viewers, PyViewer
+#from ase.visualize.external import open_external_viewer
+from ase.build import bulk
 
 dummy_args = [sys.executable, '-c', 'print("hello")']
 
 
-def test_file_missing():
-    viewer = open_external_viewer(dummy_args, 'file_which_does_not_exist')
-    status = viewer.wait()
-    assert status != 0
+#def test_file_missing():
+    #viewer = open_external_viewer(dummy_args, 'file_which_does_not_exist')
+    #status = viewer.wait()
+    #assert status != 0
 
 
 def test_ok_including_cleanup():
-    temppath = Path('tmp.txt')
-    temppath.write_text('hello')
-    assert temppath.exists()
-    viewer = open_external_viewer(dummy_args, str(temppath))
+    atoms = bulk('Au')
+    viewer = view(atoms, 'ase_gui_cli')
+    print(viewer)
+    viewer.terminate()
     status = viewer.wait()
-    assert status == 0
-    assert not temppath.exists()
+    assert status != 0
+    #temppath = Path('tmp.txt')
+    #temppath.write_text('hello')
+    #assert temppath.exists()
+    #viewer = open_external_viewer(dummy_args, str(temppath))
+    #status = viewer.wait()
+    #assert status == 0
+    #assert not temppath.exists()
 
 
 def test_view_ase_gui():
@@ -34,3 +41,24 @@ def test_view_ase_gui():
     viewer.terminate()
     status = viewer.wait()
     assert status != 0
+
+
+@pytest.fixture
+def atoms():
+    return bulk('Au')
+
+
+def test_pyviewer_mock(atoms, monkeypatch):
+    def mock_view(self, atoms, repeat=None):
+        print(f'viewing {atoms} with mock "{self.name}"')
+        return (atoms, self.name)
+
+    monkeypatch.setattr(PyViewer, 'sage', mock_view, raising=False)
+
+    (atoms1, name1) = view(atoms, viewer='sage')
+    assert name1 == 'sage'
+    assert atoms1 == atoms
+
+    atoms2, name2 = view(atoms, viewer='sage', repeat=(2, 2, 2))
+    assert name2 == 'sage'
+    assert len(atoms2) == 8 * len(atoms)
--- ase/visualize/__init__.py
+++ ase/visualize/__init__.py
@@ -4,72 +4,136 @@ import subprocess
 import sys
 import tempfile
 
+from ase.io.formats import ioformats
 from ase.io import write
 import ase.parallel as parallel
 
 
-def _pipe_to_gui(atoms, repeat, block):
+def _pipe_to_ase_gui(atoms, repeat):
     buf = BytesIO()
     write(buf, atoms, format='traj')
 
     args = [sys.executable, '-m', 'ase', 'gui', '-']
     if repeat:
-        args.append(' --repeat={},{},{}'.format(*repeat))
+        args.append('--repeat={},{},{}'.format(*repeat))
 
     proc = subprocess.Popen(args, stdin=subprocess.PIPE)
     proc.stdin.write(buf.getvalue())
     proc.stdin.close()
-    if block:
-        proc.wait()
     return proc
 
 
-class ExternalCommandViewer:
-    def __init__(self, fmt, argv):
+class CLIViewer:
+    def __init__(self, name, fmt, argv):
+        self.name = name
         self.fmt = fmt
         self.argv = argv
 
+    @property
+    def ioformat(self):
+        return ioformats[self.fmt]
 
-viewers = dict(
-    vmd=ExternalCommandViewer('cube', ['vmd']),
-    rasmol=ExternalCommandViewer('proteindatabank', ['rasmol', '-pdb']),
-    xmakemol=ExternalCommandViewer('xyz', ['xmakemol', '-f']),
-    gopenmol=ExternalCommandViewer('xyz', ['runGOpenMol']),
-    avogadro=ExternalCommandViewer('cube', ['avogadro']),
-)
+    def mktemp(self):
+        suffix = '.' + self.ioformat.extensions[0]
+        return tempfile.NamedTemporaryFile(suffix=suffix)
 
+    def view_blocking(self, atoms, data=None):
+        with self.mktemp() as fd:
+            if data is None:
+                write(fd, atoms, format=self.fmt)
+            else:
+                write(fd, atoms, format=self.fmt, data=data)
+            self.execute_viewer(self.argv + [fd.name])
+
+    def _execute(self, argv):
+        subprocess.check_call(argv)
+
+    def view(self, atoms, data=None, repeat=None):
+        """Spawn a new process in which to open the viewer."""
+        if repeat is not None:
+            atoms = atoms.repeat(repeat)
+
+        proc = Popen([sys.executable, '-m', 'ase.visualize.external'],
+                     stdin=PIPE)
+
+        pickle.dump((self, atoms, data), proc.stdin)
+        proc.stdin.close()
+        return proc
+
+    @classmethod
+    def viewers(cls):
+        return [
+            cls('ase_gui_cli', 'traj', [sys.executable, '-m', 'ase.gui']),
+            cls('avogadro', 'cube', ['avogadro']),
+            cls('gopenmol', 'xyz', ['runGOpenMol']),
+            cls('rasmol', 'proteindatabank', ['rasmol', '-pdb']),
+            cls('vmd', 'cube', ['vmd']),
+            cls('xmakemol', 'xyz', ['xmakemol', '-f']),
+        ]
+
+
+class PyViewer:
+    def __init__(self, name, supports_repeat=False):
+        self.name = name
+        self.supports_repeat = supports_repeat
+
+    def view(self, atoms, data=None, repeat=None):
+        # Delegate to any of the below methods
+        func = getattr(self, self.name)
+        if self.supports_repeat:
+            return func(atoms, repeat)
+        else:
+            if repeat is not None:
+                atoms = atoms.repeat(repeat)
+            return func(atoms)
+
+    def sage(self, atoms):
+        from ase.visualize.sage import view_sage_jmol
+        return view_sage_jmol(atoms)
+
+    def ngl(self, atoms):
+        from ase.visualize.ngl import view_ngl
+        return view_ngl(atoms)
+
+    def x3d(self, atoms):
+        from ase.visualize.x3d import view_x3d
+        return view_x3d(atoms)
+
+    def ase(self, atoms, repeat):
+        return _pipe_to_ase_gui(atoms, repeat)
+
+    @classmethod
+    def viewers(cls):
+        return [
+            cls('ase', supports_repeat=True),
+            cls('ngl'),
+            cls('sage'),
+            cls('x3d'),
+        ]
+
+
+
+viewers = {viewer.name: viewer
+           for viewer in CLIViewer.viewers() + PyViewer.viewers()}
+viewers['nglview'] = viewers['ngl']
 
 def view(atoms, data=None, viewer='ase', repeat=None, block=False):
     # Ignore for parallel calculations:
     if parallel.world.size != 1:
         return
 
-    vwr = viewer.lower()
+    vwr = viewers[viewer.lower()]
+    handle = vwr.view(atoms, data=data, repeat=repeat)
 
-    if vwr == 'ase':
-        return _pipe_to_gui(atoms, repeat, block)
+    if block and hasattr(handle, 'wait'):
+        status = handle.wait()
+        if status != 0:
+            raise RuntimeError(f'Viewer "{vwr.name}" failed with status '
+                               '{status}')
 
-    if repeat is not None:
-        atoms = atoms.repeat()
+    return handle
 
-    if vwr == 'sage':
-        from ase.visualize.sage import view_sage_jmol
-        view_sage_jmol(atoms)
-        return
-
-    if vwr in ('ngl', 'nglview'):
-        from ase.visualize.ngl import view_ngl
-        return view_ngl(atoms)
-
-    if vwr == 'x3d':
-        from ase.visualize.x3d import view_x3d
-        return view_x3d(atoms)
-
-    if vwr in viewers:
-        viewer = viewers[vwr]
-        format = viewer.fmt
-        command = viewer.command
-    elif vwr == 'paraview':
+def paraview_stuff():
         # macro for showing atoms in paraview
         macro = """\
 from paraview.simple import *
@@ -100,26 +164,15 @@ Render()
             fd.write(macro)
         format = 'vtu'
         command = 'paraview --script=' + script_name
-    else:
-        raise RuntimeError('Unknown viewer: ' + viewer)
 
-    fd, filename = tempfile.mkstemp('.' + format, 'ase-')
+    #viewer_obj = viewers[vwr]
+    #process = viewer_obj.view(atoms, data=data)
 
-    if data is None:
-        write(filename, atoms, format=format)
-    else:
-        write(filename, atoms, format=format, data=data)
 
-    viewer_args = command.split() + [filename]
+def main():
+    cli_viewer, atoms, data = pickle.load(sys.stdin.buffer)
+    cli_viewer.view_blocking(atoms, data)
 
-    from ase.visualize.external import open_external_viewer
-    viewer = open_external_viewer(viewer_args, filename)
 
-    if block:
-        status = viewer.wait()
-        if status != 0:
-            raise RuntimeError(
-                'Viewer failed on file "{}" with status {} and args: {}'
-                .format(filename, status, viewer_args))
-
-    return viewer
+if __name__ == '__main__':
+    main()
