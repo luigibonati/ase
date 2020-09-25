b7755ff2c508078a29d52e1f11f29d8c38825c06
Petr Grigorev <grigorievpit@gmail.com>
2020-09-25 18:56:16 +0100

    added test for the number of qm_buffer atoms

--- ase/test/forcefields/test_forceqmmm.py
+++ ase/test/forcefields/test_forceqmmm.py
@@ -7,24 +7,60 @@ from ase.calculators.qmmm import ForceQMMM, RescaledCalculator
 from ase.eos import EquationOfState
 from ase.optimize import FIRE
 from ase.neighborlist import neighbor_list
-
+from ase.geometry import get_distances
 
 @pytest.mark.slow
 def test_forceqmmm():
+
     # parameters
     N_cell = 2
     R_QMs = np.array([3, 7])
 
     # setup bulk and MM region
     bulk_at = bulk("Cu", cubic=True)
-    sigma = (bulk_at*2).get_distance(0, 1)*(2.**(-1./6))
+    sigma = (bulk_at * 2).get_distance(0, 1) * (2. ** (-1. / 6))
     mm = LennardJones(sigma=sigma, epsilon=0.05)
     qm = EMT()
+
+    # test number of atoms in qm_buffer_mask for spherical region in a fully periodic cell
+    bulk_at = bulk("Cu", cubic=True)
+    alat = bulk_at.cell[0, 0]
+    N_cell_geom = 10
+    at0 = bulk_at * N_cell_geom
+    r = at0.get_distances(0, np.arange(len(at0)), mic=True)
+    print("N_cell", N_cell_geom, 'N_MM', len(at0), "Size", N_cell_geom * alat)
+    qm_rc = 5.37  # cutoff for EMC()
+
+    for R_QM in [1.0e-3,  # one atom in the center
+                 alat / np.sqrt(2.0) + 1.0e-3,  # should give 12 nearest neighbours + atom in the center
+                 alat + 1.0e-3]:  # should give 18 neighbours + atom in the center:
+
+        at = at0.copy()
+        qm_mask = r < R_QM
+        qm_buffer_mask_ref = r < 2 * qm_rc + R_QM
+        # exclude atoms that are too far (in case of non spherical region)
+        # this is the old way to do it
+        _, r_qm_buffer = get_distances(at.positions[qm_buffer_mask_ref], at.positions[qm_mask], at.cell, at.pbc)
+        updated_qm_buffer_mask = np.ones_like(at[qm_buffer_mask_ref])
+        for i, r_qm in enumerate(r_qm_buffer):
+            if r_qm.min() > 2 * qm_rc:
+                updated_qm_buffer_mask[i] = False
+
+        qm_buffer_mask_ref[qm_buffer_mask_ref] = updated_qm_buffer_mask
+
+        print(f'R_QM             {R_QM}   N_QM        {qm_mask.sum()}')
+        print(f'R_QM + buffer: {2 * qm_rc + R_QM:.2f} N_QM_buffer {qm_buffer_mask_ref.sum()}')
+        print(f'                     N_total:    {len(at)}')
+        qmmm = ForceQMMM(at, qm_mask, qm, mm, buffer_width=2 * qm_rc)
+        # build qm_buffer_mask and test it
+        qmmm.initialize_qm_buffer_mask(at)
+        print(f'      Calculator N_QM_buffer:    {qmmm.qm_buffer_mask.sum().sum()}')
+        assert qmmm.qm_buffer_mask.sum() == qm_buffer_mask_ref.sum()
 
     # compute MM and QM equations of state
     def strain(at, e, calc):
         at = at.copy()
-        at.set_cell((1.0 + e)*at.cell, scale_atoms=True)
+        at.set_cell((1.0 + e) * at.cell, scale_atoms=True)
         at.calc = calc
         v = at.get_volume()
         e = at.get_potential_energy()
@@ -36,49 +72,32 @@ def test_forceqmmm():
 
     eos_qm = EquationOfState(v_qm, E_qm)
     v0_qm, E0_qm, B_qm = eos_qm.fit()
-    a0_qm = v0_qm**(1.0/3.0)
+    a0_qm = v0_qm ** (1.0 / 3.0)
 
     eos_mm = EquationOfState(v_mm, E_mm)
     v0_mm, E0_mm, B_mm = eos_mm.fit()
-    a0_mm = v0_mm**(1.0/3.0)
+    a0_mm = v0_mm ** (1.0 / 3.0)
 
     mm_r = RescaledCalculator(mm, a0_qm, B_qm, a0_mm, B_mm)
     v_mm_r, E_mm_r = zip(*[strain(bulk_at, e, mm_r) for e in eps])
 
     eos_mm_r = EquationOfState(v_mm_r, E_mm_r)
     v0_mm_r, E0_mm_r, B_mm_r = eos_mm_r.fit()
-    a0_mm_r = v0_mm_r**(1.0/3)
+    a0_mm_r = v0_mm_r ** (1.0 / 3)
 
     # check match of a0 and B after rescaling is adequete
-    assert abs((a0_mm_r - a0_qm)/a0_qm) < 1e-3 # 0.1% error in lattice constant
-    assert abs((B_mm_r - B_qm)/B_qm) < 0.05 # 5% error in bulk modulus
+    assert abs((a0_mm_r - a0_qm) / a0_qm) < 1e-3  # 0.1% error in lattice constant
+    assert abs((B_mm_r - B_qm) / B_qm) < 0.05  # 5% error in bulk modulus
 
     # plt.plot(v_mm, E_mm - np.min(E_mm), 'o-', label='MM')
     # plt.plot(v_qm, E_qm - np.min(E_qm), 'o-', label='QM')
     # plt.plot(v_mm_r, E_mm_r - np.min(E_mm_r), 'o-', label='MM rescaled')
     # plt.legend()
 
-    # test the geometry of qm_buffer_mask for spherical region in a fully periodic cell
-    at0 = bulk_at * 10
-    r = at0.get_distances(0, np.arange(len(at0)), mic=True)
-    print("N_cell", N_cell, 'N_MM', len(at0), "Size", N_cell * bulk_at.cell[0, 0])
-    for R_QM in R_QMs:
-        at = at0.copy()
-        qm_mask = r < R_QM
-        qm_buffer_mask_ref = r < 2 * qm.rc + R_QM
-        print(f'R_QM             {R_QM}   N_QM        {qm_mask.sum()}')
-        print(f'R_QM + buffer: {2 * qm.rc + R_QM:.2f} N_QM_buffer {qm_buffer_mask_ref.sum()}')
-        print(f'                     N_total:    {len(at)}')
-        qmmm = ForceQMMM(at, qm_mask, qm, mm, buffer_width=2 * qm.rc)
-        # build qm_buffer_mask and test it
-        qmmm.initialize_qm_buffer_mask(at)
-        print(f'      Calculator N_QM_buffer:    {qmmm.qm_buffer_mask.sum().sum()}')
-        assert qmmm.qm_buffer_mask.sum() == qm_buffer_mask_ref.sum()
-
     at0 = bulk_at * N_cell
     r = at0.get_distances(0, np.arange(1, len(at0)), mic=True)
     print(len(r))
-    del at0[0] # introduce a vacancy
+    del at0[0]  # introduce a vacancy
     print("N_cell", N_cell, 'N_MM', len(at0), "Size", N_cell * bulk_at.cell[0, 0])
 
     ref_at = at0.copy()
@@ -95,6 +114,8 @@ def test_forceqmmm():
         print(f'R_QM             {R_QM}   N_QM        {qm_mask.sum()}')
         print(f'R_QM + buffer: {2 * qm.rc + R_QM:.2f} N_QM_buffer {qm_buffer_mask_ref.sum()}')
         print(f'                     N_total:    {len(at)}')
+        # Warning: Small size of the cell and large size of the buffer
+        # lead to the qm calculation performed on the whole cell.
         qmmm = ForceQMMM(at, qm_mask, qm, mm, buffer_width=2 * qm.rc)
         qmmm.initialize_qm_buffer_mask(at)
         at.calc = qmmm
@@ -105,13 +126,13 @@ def test_forceqmmm():
     # compute error in energy norm |\nabla u - \nabla u_ref|
     def strain_error(at0, u_ref, u, cutoff, mask):
         I, J = neighbor_list('ij', at0, cutoff)
-        I, J = np.array([(i,j) for i, j in zip(I, J) if mask[i]]).T
+        I, J = np.array([(i, j) for i, j in zip(I, J) if mask[i]]).T
         v = u_ref - u
         dv = np.linalg.norm(v[I, :] - v[J, :], axis=1)
         return np.linalg.norm(dv)
 
-    du_global = [strain_error(at0, u_ref, u, 1.5*sigma, np.ones(len(r))) for u in us]
-    du_local = [strain_error(at0, u_ref, u, 1.5*sigma, r < 3.0) for u in us]
+    du_global = [strain_error(at0, u_ref, u, 1.5 * sigma, np.ones(len(r))) for u in us]
+    du_local = [strain_error(at0, u_ref, u, 1.5 * sigma, r < 3.0) for u in us]
 
     print('du_local', du_local)
     print('du_global', du_global)
