631f9c4843f0af2a17ac5ae252371cfd5a46da98
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-09-26 18:49:57 +0200

    add test of Octahedron.  More correct neighbour distance tolerance in cluster test

--- ase/test/test_cluster.py
+++ ase/test/test_cluster.py
@@ -9,10 +9,11 @@ a0 = 4.05
 ico_cubocta_sizes = [1, 13, 55, 147, 309, 563, 923, 1415]
 ico_corner_coordination = 6
 ico_corners = 12
+fcc_maxcoordination = 12
 
 
 def coordination_numbers(atoms):
-    return np.bincount(neighbor_list('i', atoms, 1.1 * a0))
+    return np.bincount(neighbor_list('i', atoms, 0.80 * a0))
 
 
 @pytest.mark.parametrize('shells', range(1, 6))
@@ -27,3 +28,20 @@ def test_icosa(shells):
     assert min(coordination) == ico_corner_coordination
     ncorners = sum(coordination == ico_corner_coordination)
     assert ncorners == ico_corners
+
+from ase.cluster.octahedron import Octahedron
+
+
+octa_sizes = [1, 6, 19, 44, 85, 146, 231, 344]
+@pytest.mark.parametrize('shells', range(2, 8))
+def test_regular_octahedron(shells):
+    octa = Octahedron(sym, length=shells + 1, cutoff=0)
+    coordination = coordination_numbers(octa)
+    assert len(octa) == octa_sizes[shells]
+
+    assert min(coordination) == 4  # corner atoms
+    assert sum(coordination == 4) == 6  # number of corners
+
+    # All internal atoms must have coordination as if in bulk crystal:
+    expected_internal_atoms = octa_sizes[shells - 2]
+    assert sum(coordination == fcc_maxcoordination) == expected_internal_atoms
