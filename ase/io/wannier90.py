683ce7b83862245439da876de2a47cbadbd68d33
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-09-27 22:41:43 +0200

    Beginning of test

--- ase/io/wannier90.py
+++ ase/io/wannier90.py
@@ -5,19 +5,22 @@ from ase import Atoms
 
 def read_wout_all(fileobj):
     lines = fileobj.readlines()
+
     for n, line in enumerate(lines):
         if line.strip().lower().startswith('lattice vectors (ang)'):
             break
     else:
         raise ValueError('Could not fine lattice vectors')
+
     cell = [[float(x) for x in line.split()[-3:]]
             for line in lines[n + 1:n + 4]]
 
     for n, line in enumerate(lines):
-        if line.strip().lower().endswith('cartesian coordinate (ang)'):
+        if 'cartesian coordinate (ang)' in line.lower():
             break
     else:
         raise ValueError('Could not find coordinates')
+
     positions = []
     symbols = []
     n += 2
@@ -25,15 +28,17 @@ def read_wout_all(fileobj):
         words = lines[n].split()
         if len(words) == 1:
             break
-        positions.append([float(x) for x in words[-3:]])
+        positions.append([float(x) for x in words[-4:-1]])
         symbols.append(words[1])
+        n += 1
 
     atoms = Atoms(symbols, positions, cell=cell)
 
     n = len(lines) - 1
-    while True:
+    while n > 0:
         if lines[n].strip().lower().startswith('final state'):
             break
+        n -= 1
     else:
         return atoms, np.zeros((0, 3)), np.zeros((0,))
 
@@ -47,6 +52,7 @@ def read_wout_all(fileobj):
                             for x in
                             line.split('(')[1].split(')')[0].split(',')])
             widths.append(float(line.split()[-1]))
+            n += 1
         else:
             break
 
