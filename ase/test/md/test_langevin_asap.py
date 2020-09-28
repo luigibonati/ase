17257432d6dd3bf23af26bb68e39e9108e96def8
Jakob Schiotz <schiotz@fysik.dtu.dk>
2020-09-28 10:50:24 +0200

    Remove or shorten slow MD tests.

--- ase/test/md/test_langevin_asap.py
+++ ase/test/md/test_langevin_asap.py
@@ -9,7 +9,7 @@ import numpy as np
 def test_langevin_asap(asap3):
     with seterr(all='raise'):
         rng = np.random.RandomState(0)
-        a = bulk('Au', cubic=True).repeat((5, 5, 5))
+        a = bulk('Au', cubic=True).repeat((4, 4, 4))
         a.pbc = (False, False, False)
         a.center(vacuum=2.0)
         print(a)
@@ -22,12 +22,12 @@ def test_langevin_asap(asap3):
         T = 300
         md = Langevin(a, timestep=4 * fs, temperature_K=T, friction=0.01,
                           logfile='-', loginterval=500, rng=rng)
-        md.run(steps=5000)
+        md.run(steps=3000)
         # Now gather the temperature over 10000 timesteps, collecting it
         # every 5 steps
         temp = []
         energy = []
-        for i in range(2000):
+        for i in range(1500):
             md.run(steps=5)
             temp.append(a.get_temperature())
             energy.append(a.get_potential_energy() + a.get_kinetic_energy())
@@ -37,24 +37,3 @@ def test_langevin_asap(asap3):
         avgenergy = np.mean(energy)
         print("Temperature is {:.2f} K +/- {:.2f} K".format(avgtemp, fluct))
         assert abs(avgtemp - T) < 10.0
-        # Calculate the heat capacity, should be close to 3*kB per atom
-        dT = 25
-        md.set_temperature(temperature_K=T + dT)
-        md.run(steps=2000)
-        temp = []
-        energy = []
-        for i in range(5000):
-            md.run(steps=5)
-            temp.append(a.get_temperature())
-            energy.append(a.get_potential_energy() + a.get_kinetic_energy())
-        temp = np.array(temp)
-        avgtemp = np.mean(temp)
-        fluct = np.std(temp)
-        avgenergy2 = np.mean(energy)
-        print("Temperature is {:.2f} K +/- {:.2f} K".format(avgtemp, fluct))
-        assert abs(avgtemp - (T + dT)) < 10.0
-        cv = (avgenergy2 - avgenergy) / (len(a) * kB * dT)
-        print("Heat capacity per atom: {:.3f} kB".format(cv))
-        # We need much longer simulations for good statistics, hence the large
-        # allowed error.
-        assert abs(cv - 3.0) < 1.2 
