9963891b6af69d622e2fbefe4f9ab7965ea301f7
Petr Grigorev <grigorievpit@gmail.com>
2020-09-29 19:29:46 +0100

    added atoms.pbc.copy() to avoid changing the original cell pbc

--- ase/calculators/qmmm.py
+++ ase/calculators/qmmm.py
@@ -689,7 +689,7 @@ class ForceQMMM(Calculator):
         # print('qm_radius', qm_radius)
         # print(f"qm_radius", np.linalg.norm(qm_radius))
 
-        self.qm_cluster_pbc = atoms.pbc
+        self.qm_cluster_pbc = atoms.pbc.copy()
         self.qm_cluster_cell = atoms.cell.copy()
 
         if atoms.cell.orthorhombic:
@@ -715,6 +715,25 @@ class ForceQMMM(Calculator):
                 # round the qm cell to the required tolerance
                 self.qm_cluster_cell[i, i] = np.round((self.qm_cluster_cell[i, i]) / self.qm_cell_rounding) \
                                              * self.qm_cell_rounding
+    def get_qm_cluster(self, atoms):
+
+        if self.qm_buffer_mask is None:
+            self.initialize_qm_buffer_mask(atoms)
+
+        qm_cluster = atoms[self.qm_buffer_mask]
+        del qm_cluster.constraints
+
+        qm_cluster.set_cell(self.qm_cluster_cell)
+
+        qm_shift = (0.5 * qm_cluster.cell.diagonal() -
+                    qm_cluster.positions.mean(axis=0))
+
+        if 'cell_origin' in qm_cluster.info:
+            del qm_cluster.info['cell_origin']
+
+        qm_cluster.positions += qm_shift
+
+        return qm_cluster
 
     def calculate(self, atoms, properties, system_changes):
         Calculator.calculate(self, atoms, properties, system_changes)
