960e07ec122ba634b0135b71efdfc33c8ae4478a
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-10-03 22:28:49 +0200

    add ElkFactory to test suite.  Elk calculator is broken of course.

--- ase/test/factories.py
+++ ase/test/factories.py
@@ -162,6 +162,28 @@ def read_stdout(args, createfile=None):
     return stdout
 
 
+@factory('elk')
+class ElkFactory:
+    def __init__(self, executable, species_dir):
+        self.executable = executable
+        self.species_dir = species_dir
+
+    def version(self):
+        output = read_stdout([self.executable])
+        match = re.search('Elk code version (\S+)', output, re.M)
+        return match.group(1)
+
+    def calc(self, **kwargs):
+        from ase.calculators.elk import Elk
+        command = f'{self.executable} > elk.out'
+        return Elk(command=command, species_dir=self.species_dir,
+                   **kwargs)
+
+    @classmethod
+    def fromconfig(cls, config):
+        return cls(config.executables['elk'], config.datafiles['elk'][0])
+
+
 @factory('espresso')
 class EspressoFactory:
     def __init__(self, executable, pseudo_dir):
