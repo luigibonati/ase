dca7a8eea9726e2980ae207feef65cc67c5bb28e
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-10-04 18:35:19 +0200

    working test of elk

--- ase/test/calculator/elk/test_elk.py
+++ ase/test/calculator/elk/test_elk.py
@@ -1,10 +1,13 @@
 import pytest
 
-from ase.build import bulk
+from ase.build import bulk, molecule
+from ase.units import Hartree
 
-@pytest.mark.calculator('elk')
-def test_elk(factory):
+
+@pytest.mark.calculator('elk', tasks=0, ngridk=(5, 5, 5))
+def test_elk_bulk(factory):
     atoms = bulk('Si')
-    calc = factory.calc(tasks=0)
+    calc = factory.calc()
     atoms.calc = calc
-    atoms.get_potential_energy()
+    energy = atoms.get_potential_energy()
+    assert energy == pytest.approx(-578.077659570 * Hartree, abs=0.1)
