e680fdebaf8a5448619b4f64c4508536a48131fd
James Kermode <james.kermode@gmail.com>
2020-10-06 19:30:37 +0100

    add spline and string methods to test_get_neb_method()

--- ase/test/neb/test_shared_calculator_neb.py
+++ ase/test/neb/test_shared_calculator_neb.py
@@ -20,6 +20,10 @@ def test_get_neb_method():
     assert isinstance(neb.get_neb_method(neb_dummy, "aseneb"), neb.ASENEBMethod)
     assert isinstance(neb.get_neb_method(neb_dummy, "improvedtangent"),
                       neb.ImprovedTangentMethod)
+    assert isinstance(neb.get_neb_method(neb_dummy, "spline"),
+                      neb.SplineMethod)
+    assert isinstance(neb.get_neb_method(neb_dummy, "string"),
+                      neb.StringMethod)
 
     with raises(ValueError, match=r".*some_random_string.*"):
         _ = neb.get_neb_method(neb_dummy, "some_random_string")
