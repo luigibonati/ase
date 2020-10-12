a30db179d36ff4369e788caf43cd9f72157ecee3
dskarls <dskpersistent@gmail.com>
2020-10-12 09:26:42 -0700

    TST: Move lammps data file to testdata

--- ase/test/calculator/lammpslib/test_lammpslib_simple.py
+++ ase/test/calculator/lammpslib/test_lammpslib_simple.py
@@ -8,14 +8,19 @@ from ase.md.verlet import VelocityVerlet
 
 
 @pytest.fixture
-def fcc_Ni_with_H_at_center():
+def Atoms_fcc_Ni_with_H_at_center():
     atoms = bulk("Ni", cubic=True)
     atoms += Atom("H", position=atoms.cell.diagonal() / 2)
     return atoms
 
 
 @pytest.fixture
-def calc_params_Fe():
+def lammps_data_file_Fe(datadir):
+    return datadir / "lammpslib_simple_input.data"
+
+
+@pytest.fixture
+def calc_params_Fe(lammps_data_file_Fe):
     calc_params = {}
     calc_params["lammps_header"] = [
         "units           real",
@@ -27,7 +32,7 @@ def calc_params_Fe():
         "angle_style     harmonic",
         "kspace_style    ewald 0.0001",
         "kspace_modify   gewald 0.01",
-        "read_data       lammps.data",
+        f"read_data      {lammps_data_file_Fe}",
     ]
     calc_params["lmpcmds"] = []
     calc_params["atom_types"] = {"Fe": 1}
@@ -40,91 +45,35 @@ def calc_params_Fe():
 
 
 @pytest.fixture
-def lammps_data_file():
-    lammps_data_file = """
-    8 atoms
-    1 atom types
-    6 bonds
-    1 bond types
-    4 angles
-    1 angle types
-
-    -5.1188800000000001e+01 5.1188800000000001e+01 xlo xhi
-    -5.1188800000000001e+01 5.1188800000000001e+01 ylo yhi
-    -5.1188800000000001e+01 5.1188800000000001e+01 zlo zhi
-    0.000000 0.000000 0.000000 xy xz yz
-
-    Masses
-
-    1 56
-
-    Bond Coeffs
-
-    1 646.680887 1.311940
-
-    Angle Coeffs
-
-    1 300.0 107.0
-
-    Pair Coeffs
-
-    1 0.105000 3.430851
-
-    Atoms
-
-    1 1 1 0.0 -7.0654012878945753e+00 -4.7737244253442213e-01 -5.1102452666801824e+01 2 -1 6
-    2 1 1 0.0 -8.1237844371679362e+00 -1.3340695922796841e+00 4.7658302278206179e+01 2 -1 5
-    3 1 1 0.0 -1.2090525219882498e+01 -3.2315354021627760e+00 4.7363437099502839e+01 2 -1 5
-    4 1 1 0.0 -8.3272244953257601e+00 -4.8413162043515321e+00 4.5609055410298623e+01 2 -1 5
-    5 2 1 0.0 -5.3879618209198750e+00 4.9524635221072280e+01 3.0054862714858366e+01 6 -7 -2
-    6 2 1 0.0 -8.4950075933508273e+00 -4.9363297129348325e+01 3.2588925816534982e+01 6 -6 -2
-    7 2 1 0.0 -9.7544282093133940e+00 4.9869755980935565e+01 3.6362287886934432e+01 6 -7 -2
-    8 2 1 0.0 -5.5712437770663756e+00 4.7660225526197003e+01 3.8847235874270240e+01 6 -7 -2
-
-    Velocities
-
-    1 -1.2812627962466232e-02 -1.8102422526771818e-03 8.8697845357364469e-03
-    2 7.7087896348612683e-03 -5.6149199730983867e-04 1.3646724560472424e-02
-    3 -3.5128553734623657e-03 1.2368758037550581e-03 9.7460093657088121e-03
-    4 1.1626059392751346e-02 -1.1942908859710665e-05 8.7505240354339674e-03
-    5 1.0953500823880464e-02 -1.6710422557096375e-02 2.2322216388444985e-03
-    6 3.7515599452757294e-03 1.4091708517087744e-02 7.2963916249300454e-03
-    7 5.3953961772651359e-03 -8.2013715102925017e-03 2.0159609509813853e-02
-    8 7.5074008407567160e-03 5.9398495239242483e-03 7.3144909044607909e-03
-
-    Bonds
-
-    1 1 1 2
-    2 1 2 3
-    3 1 3 4
-    4 1 5 6
-    5 1 6 7
-    6 1 7 8
-
-    Angles
-
-    1 1 1 2 3
-    2 1 2 3 4
-    3 1 5 6 7
-    4 1 6 7 8
-    """
-    return lammps_data_file
+def Atoms_Fe(lammps_data_file_Fe):
+    Atoms_Fe = ase.io.read(
+        lammps_data_file_Fe,
+        format="lammps-data",
+        Z_of_type={1: 26},
+        units="real",
+    )
+    return Atoms_Fe
 
 
 @pytest.mark.calculator("lammpslib")
 def test_lammpslib_simple(
-    factory, calc_params_NiH, fcc_Ni_with_H_at_center, calc_params_Fe, lammps_data_file
+    factory,
+    calc_params_NiH,
+    Atoms_fcc_Ni_with_H_at_center,
+    calc_params_Fe,
+    Atoms_Fe,
 ):
     """
     Get energy from a LAMMPS calculation of an uncharged system.
     This was written to run with the 30 Apr 2019 version of LAMMPS,
     for which uncharged systems require the use of 'kspace_modify gewald'.
     """
-    NiH = fcc_Ni_with_H_at_center
+    NiH = Atoms_fcc_Ni_with_H_at_center
 
     # Add a bit of distortion to the cell
     NiH.set_cell(
-        NiH.cell + [[0.1, 0.2, 0.4], [0.3, 0.2, 0.0], [0.1, 0.1, 0.1]], scale_atoms=True
+        NiH.cell + [[0.1, 0.2, 0.4], [0.3, 0.2, 0.0], [0.1, 0.1, 0.1]],
+        scale_atoms=True,
     )
 
     calc = factory.calc(**calc_params_NiH)
@@ -181,28 +130,19 @@ def test_lammpslib_simple(
 
     # the example from the docstring
 
-    NiH = fcc_Ni_with_H_at_center
+    NiH = Atoms_fcc_Ni_with_H_at_center
     calc = factory.calc(**calc_params_NiH)
     NiH.calc = calc
     print("Energy ", NiH.get_potential_energy())
 
     # a more complicated example, reading in a LAMMPS data file
-
-    # then we run the actual test
-    with open("lammps.data", "w") as fd:
-        fd.write(lammps_data_file)
-
-    at = ase.io.read(
-        "lammps.data", format="lammps-data", Z_of_type={1: 26}, units="real"
-    )
-
     calc = factory.calc(**calc_params_Fe)
-    at.calc = calc
-    dyn = VelocityVerlet(at, 1 * units.fs)
+    Atoms_Fe.calc = calc
+    dyn = VelocityVerlet(Atoms_Fe, 1 * units.fs)
 
-    energy = at.get_potential_energy()
+    energy = Atoms_Fe.get_potential_energy()
     assert energy == pytest.approx(2041.411982950972, rel=1e-4)
 
     dyn.run(10)
-    energy = at.get_potential_energy()
+    energy = Atoms_Fe.get_potential_energy()
     assert energy == pytest.approx(312.4315854721744, rel=1e-4)
