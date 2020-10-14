3412ee4ee58940ec2e3ddc4b1a9245cf04a25312
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-10-14 18:24:37 +0200

    initial work on MagneticMoments object

---
+++ ase/magmoms.py
@@ -0,0 +1,51 @@
+class MagneticMoments:
+    arrayname = 'initial_magmoms'
+    components2type = {1: 'unpolarized',
+                       2: 'collinear',
+                       3: 'noncollinear'}
+
+    def __init__(self, atoms):
+        self.atoms = atoms
+
+    @property
+    def _array(self):
+        return self.atoms.arrays.get(self.arrayname)
+
+    def __bool__(self):
+        """False if unpolarized, else True."""
+        return self._array is not None
+
+    @property
+    def spin_type(self):
+        """Type of spin configuration as a string.
+
+        One of 'unpolarized', 'collinear', or 'noncollinear'."""
+        return self.components2type[self.spincomponents]
+
+    @property
+    def spincomponents(self):
+        """Number of spin components.
+
+        1 for unpolarized, 2 for collinear, or 3 for noncollinear."""
+        array = self._array
+        if array is None:
+            return 1
+        ndim = array.ndim
+        if ndim == 1:
+            return 2
+        elif ndim == 2:
+            return 3
+        else:
+            raise ValueError(f'Bad shape of magmoms: {array.shape}')
+
+    @property
+    def polarized(self) -> bool:
+        """Whether """
+        return self._array is not None
+
+    @property
+    def collinear(self) -> bool:
+        return self.spincomponents < 3
+
+    def set(self, magmoms):
+        self.atoms.set_initial_magnetic_moments(values)
---
+++ ase/test/test_magmoms.py
@@ -0,0 +1,38 @@
+import pytest
+from ase.build import molecule
+
+
+@pytest.fixture
+def atoms():
+    return molecule('NH3')
+
+
+@pytest.fixture
+def magmoms(atoms):
+    return atoms.magmoms
+
+
+def test_unpol(magmoms):
+    assert not magmoms
+    assert magmoms.spincomponents == 1
+    assert magmoms.spin_type == 'unpolarized'
+    assert not magmoms.polarized
+    assert magmoms.collinear
+
+
+def test_pol(atoms, magmoms):
+    atoms.set_initial_magnetic_moments(np.arange(len(atoms)))
+    assert magmoms
+    assert magmoms.spincomponents == 2
+    assert magmoms.spin_type == 'collinear'
+    assert magmoms.polarized
+    assert magmoms.collinear
+
+
+def test_noncollinear(atoms, magmoms):
+    atoms.set_initial_magnetic_moments([[1, 2, 3]] * len(atoms))
+    assert magmoms
+    assert magmoms.spincomponents == 3
+    assert magmoms.spin_type == 'noncollinear'
+    assert magmoms.polarized
+    assert not magmoms.collinear
