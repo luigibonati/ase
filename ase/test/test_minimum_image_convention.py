faefb745091e37227e6d7c0dc39aa45a6bc09851
pmla <peter.mahler.larsen@gmail.com>
2020-10-19 21:50:19 +0200

    add test for 2D cells

--- ase/test/test_minimum_image_convention.py
+++ ase/test/test_minimum_image_convention.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from numpy.testing import assert_allclose
 from ase.lattice.cubic import FaceCenteredCubic
@@ -5,18 +6,31 @@ from ase.neighborlist import mic as NeighborListMic
 from ase.neighborlist import NeighborList, PrimitiveNeighborList
 
 
-def test_minimum_image_convention():
+@pytest.mark.parametrize("dim", [2, 3])
+def test_minimum_image_convention(dim):
     size = 2
+    if dim == 2:
+        pbc = [True, True, False]
+    else:
+        pbc = [True, True, True]
     atoms = FaceCenteredCubic(size=[size, size, size],
                               symbol='Cu',
                               latticeconstant=2,
-                              pbc=(1, 1, 1))
+                              pbc=pbc)
+    if dim == 2:
+        cell = atoms.cell.uncomplete(atoms.pbc)
+        atoms.set_cell(cell, scale_atoms=False)
 
     d0 = atoms.get_distances(0, np.arange(len(atoms)), mic=True)
 
-    U = np.array([[1, 2, 2],
-                  [0, 1, 2],
-                  [0, 0, 1]])
+    if dim == 2:
+        U = np.array([[1, 2, 0],
+                      [0, 1, 0],
+                      [0, 0, 1]])
+    else:
+        U = np.array([[1, 2, 2],
+                      [0, 1, 2],
+                      [0, 0, 1]])
     assert np.linalg.det(U) == 1
     atoms.set_cell(U.T @ atoms.cell, scale_atoms=False)
     atoms.wrap()
@@ -27,7 +41,6 @@ def test_minimum_image_convention():
     vnbrlist = NeighborListMic(atoms.get_positions(), atoms.cell, atoms.pbc)
     d2 = np.linalg.norm(vnbrlist, axis=1)
     assert_allclose(d0, d2)
-
 
     nl = NeighborList(np.ones(len(atoms)) * 2 * size * np.sqrt(3),
                       bothways=True,
