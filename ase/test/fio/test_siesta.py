72f01748d7950a36a1cccc055e3b386b26746729
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-10-29 16:08:58 +0100

    add unittest of fdf parser while we are at it

--- ase/test/fio/test_siesta.py
+++ ase/test/fio/test_siesta.py
@@ -1,7 +1,8 @@
 from io import StringIO
 import numpy as np
 import pytest
-from ase.io.siesta import read_struct_out
+from ase.io.siesta import read_struct_out, read_fdf
+
 
 sample_struct_out = """\
   3.0   0.0   0.0
@@ -12,6 +13,7 @@ sample_struct_out = """\
    1   46   0.3   0.4   0.5
 """
 
+
 def test_read_struct_out():
     atoms = read_struct_out(StringIO(sample_struct_out))
     assert all(atoms.numbers == [45, 46])
@@ -21,3 +23,20 @@ def test_read_struct_out():
                                                     [-1.5, 4.0, 0.0],
                                                     [0.0, 0.0, 5.0]]))
     assert all(atoms.pbc)
+
+
+sample_fdf = """\
+potatoes 5
+COFFEE 6.5
+%block spam
+   1 2.5 hello
+%endblock spam
+"""
+
+def test_read_fdf():
+    dct = read_fdf(StringIO(sample_fdf))
+    # This is a "raw" parser, no type conversion is done.
+    ref = dict(potatoes=['5'],
+               coffee=['6.5'],
+               spam=[['1', '2.5', 'hello']])
+    assert dct == ref
