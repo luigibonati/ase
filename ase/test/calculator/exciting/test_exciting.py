e49f4f561f5e0498ba51d6b20fddbffcd4812a14
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-11-05 18:51:18 +0100

    fix flakes and clean up

--- ase/test/calculator/exciting/test_exciting.py
+++ ase/test/calculator/exciting/test_exciting.py
@@ -1,9 +1,10 @@
+import pytest
+from ase import Atoms
+from ase.build import bulk
+from ase.io import read, write
+
+
 def test_exciting():
-    from ase import Atoms
-    from ase.io import read, write
-    from ase.calculators.exciting import Exciting
-
-
     a = Atoms('N3O',
               [(0, 0, 0), (1, 0, 0), (0, 0, 1), (0.5, 0.5, 0.5)],
               pbc=True)
@@ -16,16 +17,7 @@ def test_exciting():
     print(b)
     print(b.get_positions())
 
-    Exciting(dir='excitingtestfiles',
-             kpts=(4, 4, 3),
-             # bin='/fshome/chm/git/exciting/bin/excitingser',
-             maxscl=3)
-    # maybe do something???
 
-
-
-import pytest
-from ase.build import bulk
 @pytest.mark.calculator_lite
 @pytest.mark.calculator('exciting')
 def test_exciting_bulk(factory):
