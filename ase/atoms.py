de40d7eb55c9ec3467bda6b666b86c6cb6c7d6f9
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-11-12 02:24:00 +0100

    move stress-related stuff to ase.stress

--- ase/atoms.py
+++ ase/atoms.py
@@ -16,10 +16,10 @@ import numpy as np
 import ase.units as units
 from ase.atom import Atom
 from ase.cell import Cell
-from ase.constraints import (voigt_6_to_full_3x3_stress,
-                             full_3x3_to_voigt_6_stress)
+from ase.stress import voigt_6_to_full_3x3_stress, full_3x3_to_voigt_6_stress
 from ase.data import atomic_masses, atomic_masses_common
-from ase.geometry import wrap_positions, find_mic, get_angles, get_distances, get_dihedrals
+from ase.geometry import (wrap_positions, find_mic, get_angles, get_distances,
+                          get_dihedrals)
 from ase.symbols import Symbols, symbols2numbers
 from ase.utils import deprecated
 
