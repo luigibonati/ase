355142d41f85668b28e8d9bdcdc7a7ab9f76e8c2
david <davidollodart@gmail.com>
2020-11-12 15:07:50 -0800

    flake8 for gettingstarted

--- doc/gettingstarted/surface.py
+++ doc/gettingstarted/surface.py
@@ -6,6 +6,6 @@ from ase.io import read, write
 
 runpy.run_path('N2Cu.py')
 image = read('N2Cu.traj@-1')
-write('surface.pov', 
-        image, 
+write('surface.pov',
+        image,
         povray_settings=dict(transparent=False)).render()
