74076f2c12c0172673885e3b5331325ab9eaad16
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-11-16 13:06:39 +0100

    correct name is "KIM API"

--- ase/test/conftest.py
+++ ase/test/conftest.py
@@ -151,7 +151,7 @@ def KIM():
             return _KIM(*args, **kwargs)
         except KIMModelNotFound:
             pytest.skip('KIM tests require the example KIM models.  '
-                        'These models are available if the kimpy package is '
+                        'These models are available if the KIM API is '
                         'built from source.')
 
     return KIM
