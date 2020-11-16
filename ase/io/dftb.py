3a50d3861813e580430e0ca7ac90a7829c16e6a9
mvdb <maxime.cp.vandenbossche@gmail.com>
2020-11-16 21:35:27 +0100

    STY: less brackets and more whitespaces

--- ase/io/dftb.py
+++ ase/io/dftb.py
@@ -19,16 +19,18 @@ def read_dftb(fd):
     mycell = []
 
     for iline, line in enumerate(lines):
-        if (line.strip().startswith('#')):
+        if line.strip().startswith('#'):
             pass
-        elif ('genformat') in line.lower():
+        elif 'genformat' in line.lower():
             natoms = int(lines[iline + 1].split()[0])
             if lines[iline + 1].split()[1].lower() == 's':
                 my_pbc = True
             elif lines[iline + 1].split()[1].lower() == 'f':
                 my_pbc = True
                 fractional = True
+
             symbols = lines[iline + 2].split()
+
             for i in range(natoms):
                 index = iline + 3 + i
                 aindex = int(lines[index].split()[1]) - 1
@@ -36,20 +38,21 @@ def read_dftb(fd):
 
                 position = [float(p) for p in lines[index].split()[2:]]
                 atoms_pos.append(position)
+
             if my_pbc:
                 for i in range(3):
                     index = iline + 4 + natoms + i
                     cell = [float(c) for c in lines[index].split()]
                     mycell.append(cell)
         else:
-            if ('TypeNames' in line):
+            if 'TypeNames' in line:
                 col = line.split()
                 for i in range(3, len(col) - 1):
                     type_names.append(col[i].strip("\""))
-            elif ('Periodic' in line):
-                if ('Yes' in line):
+            elif 'Periodic' in line:
+                if 'Yes' in line:
                     my_pbc = True
-            elif ('LatticeVectors' in line):
+            elif 'LatticeVectors' in line:
                 for imycell in range(3):
                     extraline = lines[iline + imycell + 1]
                     cols = extraline.split()
@@ -64,16 +67,16 @@ def read_dftb(fd):
     start_reading_coords = False
     stop_reading_coords = False
     for line in lines:
-        if (line.strip().startswith('#')):
+        if line.strip().startswith('#'):
             pass
         else:
-            if ('TypesAndCoordinates' in line):
+            if 'TypesAndCoordinates' in line:
                 start_reading_coords = True
             if start_reading_coords:
-                if ('}' in line):
+                if '}' in line:
                     stop_reading_coords = True
-            if (start_reading_coords and not (stop_reading_coords)
-               and 'TypesAndCoordinates' not in line):
+            if (start_reading_coords and not stop_reading_coords
+                and 'TypesAndCoordinates' not in line):
                 typeindexstr, xxx, yyy, zzz = line.split()[:4]
                 typeindex = int(typeindexstr)
                 symbol = type_names[typeindex - 1]
