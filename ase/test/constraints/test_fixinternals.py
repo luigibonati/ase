851d80d109d3c29af18449423a63ce0756a33159
jonas.amsler <jonas.amsler@kit.edu>
2020-11-17 12:04:19 +0100

    DEV: add constrained linear combination of angles and dihedrals
    extend the FixInternals constraint by anglecombo and dihedralcombo
    these constraints are defined similarly to the bondcombo constraint
    
    TST: extend test_fixinternals.py by corresponding tests
    
    include tests for correct `index_shuffle`

--- ase/test/constraints/test_fixinternals.py
+++ ase/test/constraints/test_fixinternals.py
@@ -6,12 +6,23 @@ import copy
 import pytest
 
 
+# Convenience functions to compute linear combinations of internal coordinates
 def get_bondcombo(atoms, bondcombo_def):
-    return sum([defin[2] * atoms.get_distance(defin[0], defin[1]) for
+    return sum([defin[2] * atoms.get_distance(*defin[0:2]) for
                 defin in bondcombo_def])
 
 
-def setup_test():
+def get_anglecombo(atoms, anglecombo_def):
+    return sum([defin[3] * atoms.get_angle(*defin[0:3]) for
+                defin in anglecombo_def])
+
+
+def get_dihedralcombo(atoms, dihedralcombo_def):
+    return sum([defin[4] * atoms.get_dihedral(*defin[0:4]) for
+                defin in dihedralcombo_def])
+
+
+def setup_fixinternals():
     atoms = molecule('CH3CH2OH', vacuum=5.0)
     atoms.rattle(stdev=0.3)
 
@@ -32,28 +43,19 @@ def setup_test():
     dihedral_def = [6, 0, 1, 2]
     target_dihedral = atoms.get_dihedral(*dihedral_def)
 
-    # Fix linear combination of two bond lengths with atom indices 0-8 and
-    # 0-6 with weighting coefficients 1.0 and -1.0 to the current value.
-    # In other words, fulfil the following constraint:
-    # 1 * atoms.get_distance(0, 8) -1 * atoms.get_distance(0, 6) = const.
-    bondcombo_def = [[0, 8, 1.0], [0, 6, -1.0]]
-    target_bondcombo = get_bondcombo(atoms, bondcombo_def)
-
     # Initialize constraint
     constr = FixInternals(bonds=[(target_bond, bond_def)],
                               angles_deg=[(target_angle, angle_def)],
                               dihedrals_deg=[(target_dihedral, dihedral_def)],
-                              bondcombos=[(target_bondcombo, bondcombo_def)],
                               epsilon=1e-10)
     print(constr)
     return (atoms, constr, bond_def, target_bond, angle_def, target_angle,
-            dihedral_def, target_dihedral, bondcombo_def, target_bondcombo)
+            dihedral_def, target_dihedral)
 
 
 def test_fixinternals():
     (atoms, constr, bond_def, target_bond, angle_def, target_angle,
-     dihedral_def, target_dihedral, bondcombo_def,
-     target_bondcombo) = setup_test()
+     dihedral_def, target_dihedral) = setup_fixinternals()
 
     calc = EMT()
 
@@ -61,13 +63,11 @@ def test_fixinternals():
 
     previous_angle = atoms.get_angle(*angle_def)
     previous_dihedral = atoms.get_dihedral(*dihedral_def)
-    previous_bondcombo = get_bondcombo(atoms, bondcombo_def)
 
     print('angle before', previous_angle)
     print('dihedral before', previous_dihedral)
     print('bond length before', atoms.get_distance(*bond_def))
-    print('(target bondlength %s)', target_bond)
-    print('linear bondcombination before', previous_bondcombo)
+    print('target bondlength', target_bond)
 
     atoms.calc = calc
     atoms.set_constraint(constr)
@@ -77,33 +77,88 @@ def test_fixinternals():
     new_angle = atoms.get_angle(*angle_def)
     new_dihedral = atoms.get_dihedral(*dihedral_def)
     new_bondlength = atoms.get_distance(*bond_def)
-    new_bondcombo = get_bondcombo(atoms, bondcombo_def)
 
     print('angle after', new_angle)
     print('dihedral after', new_dihedral)
     print('bondlength after', new_bondlength)
-    print('linear bondcombination after', new_bondcombo)
 
     err1 = new_angle - previous_angle
     err2 = new_dihedral - previous_dihedral
     err3 = new_bondlength - target_bond
-    err4 = new_bondcombo - previous_bondcombo
 
     print('error in angle', repr(err1))
     print('error in dihedral', repr(err2))
     print('error in bondlength', repr(err3))
-    print('error in bondcombo', repr(err4))
 
     assert err1 < 1e-11
     assert err2 < 1e-12
     assert err3 < 1e-12
-    assert err4 < 1e-12
+
+
+def setup_combos():
+    atoms = molecule('CH3CH2OH', vacuum=5.0)
+    atoms.rattle(stdev=0.3)
+
+    # Fix linear combination of two bond lengths with atom indices 0-8 and
+    # 0-6 with weighting coefficients 1.0 and -1.0 to the current value.
+    # In other words, fulfil the following constraint:
+    # 1.0 * atoms.get_distance(2, 1) + -1.0 * atoms.get_distance(2, 3) = const.
+    bondcombo_def = [[2, 1, 1.0], [2, 3, -1.0]]
+    target_bondcombo = get_bondcombo(atoms, bondcombo_def)
+
+    # Fix linear combination of two angles
+    # 1. * atoms.get_angle(7, 0, 8) + 1. * atoms.get_angle(7, 0, 6) = const.
+    anglecombo_def = [[7, 0, 8, 1.], [7, 0, 6, 1]]
+    target_anglecombo = get_anglecombo(atoms, anglecombo_def)
+
+    # Fix linear combination of two dihedrals
+    dihedralcombo_def = [[3, 2, 1, 4, 1.0], [2, 1, 0, 7, 1.0]]
+    target_dihedralcombo = get_dihedralcombo(atoms, dihedralcombo_def)
+
+    # Initialize constraint
+    constr = FixInternals(bondcombos=[(target_bondcombo, bondcombo_def)],
+                          anglecombos=[(target_anglecombo, anglecombo_def)],
+                          dihedralcombos=[(target_dihedralcombo,
+                          dihedralcombo_def)], epsilon=1e-10)
+    print(constr)
+    return (atoms, constr, bondcombo_def, target_bondcombo, anglecombo_def,
+            target_anglecombo, dihedralcombo_def, target_dihedralcombo)
+
+
+def test_combos():
+    (atoms, constr, bondcombo_def, target_bondcombo, anglecombo_def,
+     target_anglecombo, dihedralcombo_def,
+     target_dihedralcombo) = setup_combos()
+
+    ref_bondcombo = get_bondcombo(atoms, bondcombo_def)
+    ref_anglecombo = get_anglecombo(atoms, anglecombo_def)
+    ref_dihedralcombo = get_dihedralcombo(atoms, dihedralcombo_def)
+
+    atoms.calc = EMT()
+    atoms.set_constraint(constr)
+
+    opt = BFGS(atoms, trajectory='opt.traj', logfile='opt.log')
+    opt.run(fmax=0.01)
+
+    new_bondcombo = get_bondcombo(atoms, bondcombo_def)
+    new_anglecombo = get_anglecombo(atoms, anglecombo_def)
+    new_dihedralcombo = get_dihedralcombo(atoms, dihedralcombo_def)
+
+    err_bondcombo = new_bondcombo - ref_bondcombo
+    err_anglecombo = new_anglecombo - ref_anglecombo
+    err_dihedralcombo = new_dihedralcombo - ref_dihedralcombo
+
+    print('error in bondcombo:', repr(err_bondcombo))
+    print('error in anglecombo:', repr(err_anglecombo))
+    print('error in dihedralcombo:', repr(err_dihedralcombo))
+
+    for err in [err_bondcombo, err_anglecombo, err_dihedralcombo]:
+        assert err < 1e-12
 
 
 def test_index_shuffle():
     (atoms, constr, bond_def, target_bond, angle_def, target_angle,
-     dihedral_def, target_dihedral, bondcombo_def,
-     target_bondcombo) = setup_test()
+     dihedral_def, target_dihedral) = setup_fixinternals()
 
     constr2 = copy.deepcopy(constr)
 
@@ -112,16 +167,29 @@ def test_index_shuffle():
     constr.index_shuffle(atoms, range(len(atoms)))
     assert all(a == b for a, b in zip(constr.get_indices(), (0, 1, 2, 6, 8)))
 
-    # test only bondcombo remains
-    constr.index_shuffle(atoms, [0, 6, 8])
-    assert constr.bondcombos[0][1] == [[0, 2, 1.0], [0, 1, -1.0]]
-    
     # test full constraint is not part of new slice
     with pytest.raises(IndexError):
         constr.index_shuffle(atoms, [0])
 
-    # test only bondcombo is not part of the new slice
+    # test correct shuffling
     constr2.index_shuffle(atoms, [1, 2, 0, 6])
     assert constr2.bonds[0][1] == [0, 1]
     assert constr2.angles[0][1] == [3, 2, 0]
     assert constr2.dihedrals[0][1] == [3, 2, 0, 1]
+
+
+def test_combo_index_shuffle():
+    (atoms, constr, bondcombo_def, target_bondcombo, anglecombo_def,
+     target_anglecombo, dihedralcombo_def,
+     target_dihedralcombo) = setup_combos()
+
+    # test no change, test constr.get_indices()
+    answer = (0, 1, 2, 3, 4, 6, 7, 8)
+    assert all(a == b for a, b in zip(constr.get_indices(), answer))
+    constr.index_shuffle(atoms, range(len(atoms)))
+    assert all(a == b for a, b in zip(constr.get_indices(), answer))
+
+    # test anglecombo not part of slice
+    constr.index_shuffle(atoms, [1, 2, 3, 4, 0, 7])
+    assert constr.bondcombos[0][1] == [[1, 0, 1.0], [1, 2, -1.0]]
+    assert constr.dihedralcombos[0][1] == [[2, 1, 0, 3, 1.0], [1, 0, 4, 5, 1.0]]
