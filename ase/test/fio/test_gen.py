59d423161ce277d8b26f3730417fb6be64ec10d0
mvdb <maxime.cp.vandenbossche@gmail.com>
2020-11-18 20:12:04 +0100

    TST: add test for ase.io.dftb.write_dftb()

--- ase/test/fio/test_gen.py
+++ ase/test/fio/test_gen.py
@@ -7,12 +7,13 @@ from ase.io import read, write
 
 @pytest.mark.parametrize('pbc', [False, [True, True, False], True])
 @pytest.mark.parametrize('cell', [None, [[2.5, 0, 0], [2, 4, 0], [1, 2, 3]]])
-def test_gen(pbc, cell):
+@pytest.mark.parametrize('write_format', ['gen', 'dftb'])
+def test_gen(pbc, cell, write_format):
     atoms = Atoms(symbols='OCO', pbc=pbc, cell=cell,
                   positions=[[-0.1, 1.2, 0.3],
                              [-0.1, 0.0, 0.2],
                              [0.4, -0.9, 0.0]])
-    write('test.gen', atoms)
+    write('test.gen', atoms, format=write_format)
 
     atoms_new = read('test.gen')
     assert np.all(atoms_new.numbers == atoms.numbers)
