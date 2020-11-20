6d93420af1328996f85543a893fe4a6ee07001b2
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-11-20 16:29:27 +0100

    use factory in psi4 test

--- ase/test/calculator/psi4/test_psi4_HF_3_21G.py
+++ ase/test/calculator/psi4/test_psi4_HF_3_21G.py
@@ -9,11 +9,12 @@ from ase.calculators.psi4 import Psi4
 
 
 @pytest.mark.calculator_lite
-def test_main():
+@pytest.mark.calculator('psi4')
+def test_main(factory):
     atoms = molecule('H2O')
     atoms.rotate(30, 'x')
 
-    calc = Psi4(basis='3-21G')
+    calc = factory.calc(basis='3-21G')
     atoms.calc = calc
 
     # Calculate forces ahead of time, compare against finite difference after
@@ -23,7 +24,7 @@ def test_main():
                     rtol=1e-4, atol=1e-4)
 
     # Test the reader
-    calc2 = Psi4()
+    calc2 = factory.calc()
     calc2.read('psi4-calc')
     assert_allclose(calc2.results['energy'], atoms.get_potential_energy(),
                     rtol=1e-4, atol=1e-4)
@@ -33,8 +34,3 @@ def test_main():
     # Compare analytical vs numerical forces
     assert_allclose(atoms.get_forces(), calc.calculate_numerical_forces(atoms),
                     rtol=1e-4, atol=1e-4)
-
-    os.remove('psi4-calc.dat')
-    # Unfortunately, we can't currently remove timer.dat, because Psi4
-    # creates the file after this script exits. Not even atexit works.
-    # os.remove('timer.dat')
