042eca6fcd6593591d58c9801acfdf86314a084d
James Kermode <james.kermode@gmail.com>
2020-11-26 19:23:43 +0000

    WIP on pyamg failures

--- ase/test/precon/test_smallcell.py
+++ ase/test/precon/test_smallcell.py
@@ -1,7 +1,8 @@
 import pytest
 from ase.atoms import Atoms
+from ase.build import bulk
 from ase.optimize.precon import PreconLBFGS
-
+from ase.calculators.emt import EMT
 
 def test_precon_warn():
     with pytest.warns(UserWarning, match='The system is likely too small'):
@@ -12,8 +13,10 @@ def test_precon_warn():
 @pytest.mark.slow
 @pytest.mark.parametrize('N', [1, 3])
 def test_precon(N):
-    a0 = bulk('Cu', cubic=True)
-    a0 *= (N, N, N)
+    atoms = bulk('Cu', cubic=True)
+    atoms = (N, N, N)
+    atoms.calc = EMT()
+    opt = PreconLBFGS(atoms, precon="auto")
 
 def test_precon_nowarn():
     PreconLBFGS(Atoms('100H'))
