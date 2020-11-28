6b61a4f125c03b4cab5748b78a260602d6afcf17
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-11-28 16:15:46 +0100

    class and test for combined cache object

--- ase/test/test_filecache.py
+++ ase/test/test_filecache.py
@@ -1,10 +1,14 @@
 import pytest
-from ase.utils.filecache import MultiFileJSONCache
+from ase.utils.filecache import MultiFileJSONCache, CombinedJSONCache
 
 
 @pytest.fixture
 def cache():
     return MultiFileJSONCache('cache')
+
+
+def sample_dict():
+    return {'hello': [1, 2, 3], 'world': 'grumble'}
 
 
 def test_basic(cache):
@@ -22,8 +26,24 @@ def test_basic(cache):
 
 @pytest.mark.parametrize('dct', [
     {},
-    {'hello': [1, 2, 3], 'world': 'grumble'},
+    sample_dict(),
 ])
 def test_cache(dct, cache):
     cache.update(dct)
     assert dict(cache) == dct
+
+
+def test_split(cache):
+    dct = sample_dict()
+    cache.update(dct)
+    combined = cache.combine()
+    assert dict(combined) == dct
+
+
+def test_combine():
+    dct = sample_dict()
+    combined = CombinedJSONCache.dump_cache('cache', dct)
+    assert dict(combined) == dct
+    cache = combined.split()
+    assert dict(cache) == dct
+    assert len(combined) == 0
--- ase/utils/filecache.py
+++ ase/utils/filecache.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import json
-from collections.abc import MutableMapping
+from collections.abc import MutableMapping, Mapping
 from contextlib import contextmanager
-from ase.io.jsonio import read_json, encode #write_json
+from ase.io.jsonio import read_json, write_json, encode
 from ase.utils import opencew
 
 
@@ -80,3 +80,56 @@ class MultiFileJSONCache(MutableMapping):
             self._filename(key).unlink()
         except FileNotFoundError:
             missing(key)
+
+    def combine(self):
+        cache = CombinedJSONCache.dump_cache(self.directory, dict(self))
+        assert set(cache) == set(self)
+        self.clear()
+        assert len(self) == 0
+        return cache
+
+
+class CombinedJSONCache(Mapping):
+    def __init__(self, directory, dct):
+        self.directory = Path(directory)
+        self._dct = dict(dct)
+
+    @property
+    def _filename(self):
+        return self.directory / 'combined.json'
+
+    def _dump_json(self):
+        self.directory.mkdir(exist_ok=True, parents=True)
+        write_json(self._filename, self._dct)
+
+    def __len__(self):
+        return len(self._dct)
+
+    def __iter__(self):
+        return iter(self._dct)
+
+    def __getitem__(self, index):
+        return self._dct[index]
+
+    @classmethod
+    def dump_cache(cls, path, dct):
+        cache = cls(path, dct)
+        cache._dump_json()
+        return cache
+
+    @classmethod
+    def load(cls, path):
+        dct = read_json(path, always_array=False)
+        return cls(path, dct)
+
+    def clear(self):
+        self._filename.unlink()
+        self._dct.clear()
+
+    def split(self):
+        cache = MultiFileJSONCache(self.directory)
+        assert len(cache) == 0
+        cache.update(self)
+        assert set(cache) == set(self)
+        self.clear()
+        return cache
