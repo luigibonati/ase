fe3a6b6dd2f514db70f65566960d2eb70ec57de5
dskarls <dskpersistent@gmail.com>
2020-12-05 14:36:36 -0800

    TST: Use single var for relative tolerance in each lammpsdata test

--- ase/test/lammpsdata/test_lammpsdata_read.py
+++ ase/test/lammpsdata/test_lammpsdata_read.py
@@ -11,6 +11,9 @@ import ase.io
 
 from .parse_lammps_data_file import lammpsdata_file_extracted_sections
 
+# Relative tolerance for comparing floats with pytest.approx
+REL_TOL = 1e-2
+
 
 def test_lammpsdata_read(lammpsdata_file_path):
     atoms = ase.io.read(lammpsdata_file_path, format="lammps-data", units="metal")
@@ -22,23 +25,23 @@ def test_lammpsdata_read(lammpsdata_file_path):
     for cell_vec_read_in, cell_vec_expected in zip(
         cell_read_in, expected_values["cell"]
     ):
-        assert cell_vec_read_in == pytest.approx(cell_vec_expected, rel=1e-2)
+        assert cell_vec_read_in == pytest.approx(cell_vec_expected, rel=REL_TOL)
 
     # Check masses were read in correctly
     masses_read_in = atoms.get_masses()
     masses_expected = [expected_values["mass"]] * len(expected_values["positions"])
-    assert masses_read_in == pytest.approx(masses_expected, rel=1e-2)
+    assert masses_read_in == pytest.approx(masses_expected, rel=REL_TOL)
 
     # Check positions were read in correctly
     positions_read_in = atoms.get_positions()
     positions_expected = expected_values["positions"]
     for pos_read_in, pos_expected in zip(positions_read_in, positions_expected):
-        assert pos_read_in == pytest.approx(pos_expected, rel=1e-2)
+        assert pos_read_in == pytest.approx(pos_expected, rel=REL_TOL)
 
     # Check velocities were read in correctly
     velocities_read_in = atoms.get_velocities()
     velocities_expected = expected_values["velocities"]
     for vel_read_in, vel_expected in zip(velocities_read_in, velocities_expected):
-        assert vel_read_in == pytest.approx(vel_expected, rel=1e-2)
+        assert vel_read_in == pytest.approx(vel_expected, rel=REL_TOL)
 
     # TODO: Also check charges, travels, molecule id, bonds, and angles
--- ase/test/lammpsdata/test_lammpsdata_write.py
+++ ase/test/lammpsdata/test_lammpsdata_write.py
@@ -8,6 +8,9 @@ import ase.io
 
 from .parse_lammps_data_file import lammpsdata_file_extracted_sections
 
+# Relative tolerance for comparing floats with pytest.approx
+REL_TOL = 1e-2
+
 
 def test_lammpsdata_write(atoms):
     # Write atoms object to lammpsdata file-like object
@@ -24,16 +27,16 @@ def test_lammpsdata_write(atoms):
     cell_written = written_values["cell"]
     cell_expected = atoms.get_cell()
     for cell_vec_written, cell_vec_expected in zip(cell_written, cell_expected):
-        assert cell_vec_written == pytest.approx(cell_vec_expected, rel=1e-2)
+        assert cell_vec_written == pytest.approx(cell_vec_expected, rel=REL_TOL)
 
     # Check that positions were written correctly
     positions_written = written_values["positions"]
     positions_expected = atoms.get_positions(wrap=True)
     for pos_written, pos_expected in zip(positions_written, positions_expected):
-        assert pos_written == pytest.approx(pos_expected, rel=1e-2)
+        assert pos_written == pytest.approx(pos_expected, rel=REL_TOL)
 
     # Check velocities were read in correctly
     velocities_written = written_values["velocities"]
     velocities_expected = atoms.get_velocities()
     for vel_written, vel_expected in zip(velocities_written, velocities_expected):
-        assert vel_written == pytest.approx(vel_expected, rel=1e-2)
+        assert vel_written == pytest.approx(vel_expected, rel=REL_TOL)
