2cbc891d842673a078f1e57579184648e631e9c0
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-12-06 17:36:10 +0100

    split long test

--- ase/test/atoms/test_center.py
+++ ase/test/atoms/test_center.py
@@ -1,7 +1,13 @@
+import pytest
 import numpy as np
 from math import pi, sqrt, cos
 from ase import data
 from ase.lattice.cubic import FaceCenteredCubic
+
+
+symb = 'Cu'
+Z = data.atomic_numbers[symb]
+a0 = data.reference_states[Z]['a']
 
 
 def checkang(a, b, phi):
@@ -10,21 +16,20 @@ def checkang(a, b, phi):
     assert np.abs(cosphi - cos(phi)) < 1e-10
 
 
-def test_center():
-    symb = "Cu"
-    Z = data.atomic_numbers[symb]
-    a0 = data.reference_states[Z]['a']
-
+@pytest.fixture
+def atoms():
     # (100) oriented block
-    atoms = FaceCenteredCubic(size=(5,5,5), symbol="Cu", pbc=(1,1,0))
+    atoms = FaceCenteredCubic(size=(5,5,5), symbol=symb, pbc=(1,1,0))
     assert len(atoms) == 5*5*5*4
     c = atoms.get_cell()
     checkang(c[0], c[1], pi/2)
     checkang(c[0], c[2], pi/2)
     checkang(c[1], c[2], pi/2)
     assert np.abs(5 * a0 - c[2,2]) < 1e-10
+    return atoms
 
-    # Add vacuum in one direction
+
+def test_vacuum_one_direction(atoms):
     vac = 10.0
     atoms.center(axis=2, vacuum=vac)
     c = atoms.get_cell()
@@ -33,7 +38,8 @@ def test_center():
     checkang(c[1], c[2], pi/2)
     assert np.abs(4.5 * a0 + 2* vac - c[2,2]) < 1e-10
 
-    # Add vacuum in all directions
+
+def test_vacuum_all_directions(atoms):
     vac = 4.0
     atoms.center(vacuum=vac)
     c = atoms.get_cell()
@@ -44,9 +50,17 @@ def test_center():
     assert np.abs(4.5 * a0 + 2* vac - c[1,1]) < 1e-10
     assert np.abs(4.5 * a0 + 2* vac - c[2,2]) < 1e-10
 
-    # Now a general unit cell
-    atoms = FaceCenteredCubic(size=(5,5,5), directions=[[1,0,0], [0,1,0], [1,0,1]],
-                              symbol="Cu", pbc=(1,1,0))
+
+
+@pytest.fixture
+def atoms_guc():
+    return FaceCenteredCubic(size=(5,5,5),
+                             directions=[[1,0,0], [0,1,0], [1,0,1]],
+                             symbol="Cu", pbc=(1,1,0))
+
+
+def test_general_unit_cell(atoms_guc):
+    atoms = atoms_guc
     assert len(atoms) == 5*5*5*2
     c = atoms.get_cell()
     checkang(c[0], c[1], pi/2)
@@ -54,7 +68,9 @@ def test_center():
     checkang(c[1], c[2], pi/2)
     assert np.abs(2.5 * a0 - c[2,2]) < 1e-10
 
-    # Add vacuum in one direction
+
+def test_vacuum_one_direction_guc(atoms_guc):
+    atoms = atoms_guc
     vac = 10.0
     atoms.center(axis=2, vacuum=vac)
     c = atoms.get_cell()
