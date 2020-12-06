981fc6bf03de920671be057f5996b3b3cfb97b29
dskarls <dskpersistent@gmail.com>
2020-12-05 18:03:11 -0800

    TST: Remove test_lammpslib_interface from calculators-lite
    - This test already gets run under the 'main' CI job since it doesn't
    need a calculator etc, so we don't need to run it in 'calculators-lite'
    - Also move imports out of test function

--- ase/test/calculator/lammpslib/test_lammpslib_interface.py
+++ ase/test/calculator/lammpslib/test_lammpslib_interface.py
@@ -1,12 +1,9 @@
-import pytest
+import numpy as np
+from ase.calculators.lammpslib import is_upper_triangular
 
 
-@pytest.mark.calculator_lite
 def test_lammpslib_interface():
-    # test some functionality of the interace
-    import numpy as np
-    from ase.calculators.lammpslib import is_upper_triangular
-
+    """test some functionality of the interace"""
     m = np.ones((3, 3))
     assert not is_upper_triangular(m)
 
