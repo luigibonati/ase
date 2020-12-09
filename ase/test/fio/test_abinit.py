82f46ab1beb33cdf6798ac636083007a8ca65e54
Ask Hjorth Larsen <asklarsen@gmail.com>
2020-12-09 17:57:23 +0100

    correct unit

--- ase/test/fio/test_abinit.py
+++ ase/test/fio/test_abinit.py
@@ -74,7 +74,7 @@ def test_read_abinit_output():
         np.array([[5.0, 0.0, 0.1], [0.0, 6.0, 0.0], [0.0, 0.0, 7.0]]))
 
     ref_stress = pytest.approx([2.3, 2.4, 2.5, 3.1, 3.2, 3.3])
-    assert results.pop('stress') * (Hartree / Bohr**3) == ref_stress
+    assert results.pop('stress') / (Hartree / Bohr**3) == ref_stress
     assert results.pop('forces') == pytest.approx(
         np.array([[-0.1, -0.3, 0.4], [-0.2, -0.4, -0.5]]))
 
