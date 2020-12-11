266ece74adcd13b5541c544047954ac98800f043
James Kermode <james.kermode@gmail.com>
2020-12-11 19:00:15 +0000

    fix flakes - functions imported but not used after test simplificaton

--- ase/test/precon/test_smallcell.py
+++ ase/test/precon/test_smallcell.py
@@ -1,8 +1,7 @@
 import pytest
 from ase.atoms import Atoms
-from ase.build import bulk
 from ase.optimize.precon import PreconLBFGS
-from ase.calculators.emt import EMT
+
 
 def test_precon_warn():
     with pytest.warns(UserWarning, match='The system is likely too small'):
