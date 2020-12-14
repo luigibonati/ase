e19afd69a7fca558796e3656953cfc0211ec3342
Marcel Langer <me@sirmarcel.com>
2020-12-14 12:11:48 +0100

    ENH: Add smooth cutoff option to ase.calculators.lj
    Adds a jax-md / HOOMD-blue style smooth cutoff to the LJ calculator.
    
    Added a bit more documentation to the docstring so when someone touches
    the calculator next time, they can hopefully figure out the somewhat tricky
    changes to forces etc. that need to be done to get the smooth cutoff to work.
    
    Also added testing for this case.
    
    I also externally tested this against the jax-md implementation, and against
    finite differences to check that the pairwise forces are actually the correct
    derivative. The reference pressure test fails with the smooth cutoff, but not
    by much -- I think this is ok, given that the smooth cutoff does indeed modify
    the PES somewhat.

--- ase/test/calculator/test_lj.py
+++ ase/test/calculator/test_lj.py
@@ -21,22 +21,32 @@ def atoms():
     return atoms
 
 
-def test_minimum_energy(atoms):
+@pytest.fixture
+def atoms_smooth():
+    """two atoms at potential minimum, with 'smooth' LJ"""
+
+    atoms = Atoms('H2', positions=[[0, 0, 0], [0, 0, 2 ** (1.0 / 6.0)]])
+    calc = LennardJones(rc=1.0e5, smooth=True)
+    atoms.calc = calc
+
+    return atoms
+
+
+def test_minimum_energy(atoms, atoms_smooth):
     # testing at the minimum to see if anything is on fire
     # See https://en.wikipedia.org/wiki/Lennard-Jones_potential
     # Minimum is at r=2^(1/6)*sigma, and it's -1.
 
-    energy = atoms.get_potential_energy()
-    energies = atoms.get_potential_energies()
+    assert atoms.get_potential_energy() == reference_potential_energy
+    assert atoms_smooth.get_potential_energy() == reference_potential_energy
+    assert atoms.get_potential_energies().sum() == reference_potential_energy
+    assert atoms_smooth.get_potential_energies().sum() == reference_potential_energy
 
-    assert energy == reference_potential_energy
-    assert energies.sum() == reference_potential_energy
 
-
-def test_minimum_forces(atoms):
+def test_minimum_forces(atoms, atoms_smooth):
     # forces should be zero
-    forces = atoms.get_forces()
-    np.testing.assert_allclose(forces, 0, atol=1e-14)
+    np.testing.assert_allclose(atoms.get_forces(), 0, atol=1e-14)
+    np.testing.assert_allclose(atoms_smooth.get_forces(), 0, atol=1e-14)
 
 
 def test_system_changes(atoms):
@@ -47,6 +57,7 @@ def test_system_changes(atoms):
 
 
 # test bulk properties
+stretch = 1.5
 reference_force = pytest.approx(1.57190846e-05)
 reference_pressure = pytest.approx(1.473229212e-05)
 
@@ -54,9 +65,6 @@ reference_pressure = pytest.approx(1.473229212e-05)
 @pytest.fixture
 def atoms_bulk():
     """return a stretched Ar fcc cell"""
-
-    stretch = 1.5
-
     atoms = bulk("Ar", cubic=True)
     atoms.set_cell(atoms.cell * stretch, scale_atoms=True)
 
@@ -66,32 +74,52 @@ def atoms_bulk():
     return atoms
 
 
-def test_bulk_energies(atoms_bulk):
-    # check energies
-    energy = atoms_bulk.get_potential_energy()
-    energies = atoms_bulk.get_potential_energies()
+@pytest.fixture
+def atoms_smooth_bulk():
+    """return a stretched Ar fcc cell, with a 'smooth' LJ"""
+    atoms = bulk("Ar", cubic=True)
+    atoms.set_cell(atoms.cell * stretch, scale_atoms=True)
 
-    assert np.allclose(energy, energies.sum())
+    # somewhat hand-picked parameters, but ok for comparison
+    calc = LennardJones(rc=12, ro=10, smooth=True)
+    atoms.calc = calc
+
+    return atoms
+
+
+def test_bulk_energies(atoms_bulk, atoms_smooth_bulk):
+    # check energies
+
+    assert np.allclose(
+        atoms_bulk.get_potential_energy(), atoms_bulk.get_potential_energies().sum()
+    )
+    assert np.allclose(
+        atoms_smooth_bulk.get_potential_energy(),
+        atoms_smooth_bulk.get_potential_energies().sum(),
+    )
 
     # energies should be equal in this high-symmetry structure
-    assert energies.std() == pytest.approx(0.0)
+    assert atoms_bulk.get_potential_energies().std() == pytest.approx(0.0)
+    assert atoms_smooth_bulk.get_potential_energies().std() == pytest.approx(0.0)
 
 
-def test_bulk_forces(atoms_bulk):
+def test_bulk_forces(atoms_bulk, atoms_smooth_bulk):
     # displace atom for 0.03 \AA
     atoms_bulk.positions[0, 0] += 0.03
+    atoms_smooth_bulk.positions[0, 0] += 0.03
 
     # check forces sum to zero
-    forces = atoms_bulk.get_forces()
-
-    assert np.allclose(forces.sum(axis=0), 0)
+    assert np.allclose(atoms_bulk.get_forces().sum(axis=0), 0)
+    assert np.allclose(atoms_smooth_bulk.get_forces().sum(axis=0), 0)
 
     # check reference force
-    assert forces[0, 0] == reference_force
+    assert atoms_bulk.get_forces()[0, 0] == reference_force
+    assert atoms_smooth_bulk.get_forces()[0, 0] == reference_force
 
 
 def test_bulk_stress(atoms_bulk):
     # check stress computation for sanity and reference
+    # reference value computed for "non-smooth" LJ
     stress = atoms_bulk.get_stress()
     stresses = atoms_bulk.get_stresses()
 
