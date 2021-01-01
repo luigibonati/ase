f8aff3371ab65b67e780be3d7e6a33b427918238
Rashid Rafeek <rashidrafeek@gmail.com>
2021-01-01 11:15:47 +0530

    cli exec: added -e option and added completion

--- ase/cli/exec.py
+++ ase/cli/exec.py
@@ -1,19 +1,29 @@
 import os
 
-from ase.io import read, write
+from ase.io import read
 
 
 class CLICommand:
     """ Execute code on files.
 
-    The given python code is evaluated on the atoms object where 'atoms' is
-    used to denote the Atoms object.
+    The given python code is evaluated on the Atoms object read from
+    the input file for each frame of the file. `atoms` is used to 
+    denote the Atoms object. Either of -e or -E option should provided
+    for evaluating code given as a string or from a file, respectively.
     """
 
     @staticmethod
     def add_arguments(parser):
         add = parser.add_argument
         add('input', nargs='+', metavar='input-file')
+        add('-e', '--exec-code',
+            help='Python code to execute on each atoms. The Atoms'
+            ' object is available as `atoms`. '
+            'Example: For printing cell parameters from all the '
+            'frames, `print(atoms.cell.cellpar())`')
+        add('-E', '--exec-file',
+            help='Python source code file to execute on each '
+            'frame, usage is as for -e/--exec-code.')
         add('-i', '--input-format', metavar='FORMAT',
             help='Specify input FORMAT')
         add('-n', '--image-number',
@@ -23,25 +33,16 @@ class CLICommand:
             'the back) or a range: start:stop:step, where the '
             '":step" part can be left out - default values are '
             '0:nimages:1.')
-        add('-e', '--exec-code',
-            help='Python code to execute on each atoms before '
-            'writing it to output file. The Atoms object is '
-            'available as `atoms`. Set `atoms.info["_output"] = False` '
-            'to suppress output of this frame.')
-        add('-E', '--exec-file',
-            help='Python source code file to execute on each '
-            'frame, usage is as for -e/--exec-code.')
         add('--read-args', nargs='+', action='store',
             default={}, metavar="KEY=VALUE",
             help='Additional keyword arguments to pass to '
             '`ase.io.read()`.')
-        add('--write-args', nargs='+', action='store',
-            default={}, metavar="KEY=VALUE",
-            help='Additional keyword arguments to pass to '
-            '`ase.io.write()`.')
 
     @staticmethod
     def run(args, parser):
+        if not (args.exec_code or args.exec_file):
+            parser.error("Atleast one of '-e' or '-E' must be provided")
+
         if args.read_args:
             args.read_args = eval("dict({0})"
                                   .format(', '.join(args.read_args)))
