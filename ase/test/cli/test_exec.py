e37d5dfdad885ef4ee28525b50b0bdfc065df049
Rashid Rafeek <rashidrafeek@gmail.com>
2021-01-02 13:20:03 +0530

    Lint fix

--- ase/test/cli/test_exec.py
+++ ase/test/cli/test_exec.py
@@ -2,6 +2,7 @@ import pytest
 from ase.build import bulk
 from ase.io import write
 
+
 @pytest.fixture
 def fname():
     atoms = bulk('Au')
@@ -9,17 +10,21 @@ def fname():
     write(filename, atoms)
     return filename
 
+
 def test_exec_fail_withoutcode(cli, fname):
     cli.ase('exec', fname, expect_fail=True)
+
 
 def test_exec_atoms(cli, fname):
     atoms = bulk('Au')
     out = cli.ase('exec', fname, '-e', 'print(atoms.symbols)')
     assert str(atoms.symbols) in out
 
+
 def test_exec_index(cli, fname):
     out = cli.ase('exec', fname, '-e', 'print(index)')
     assert str(0) in out
+
 
 def test_exec_images(cli, fname):
     atoms = bulk('Au')
