c644eed399ae8cccb92921269abf2c8e02cabe02
Rashid Rafeek <rashidrafeek@gmail.com>
2021-01-02 12:31:08 +0530

    CLI exec command: added tests

---
+++ ase/test/cli/test_exec.py
@@ -0,0 +1,27 @@
+import pytest
+from ase.build import bulk
+from ase.io import write
+
+@pytest.fixture
+def fname():
+    atoms = bulk('Au')
+    filename = 'file.traj'
+    write(filename, atoms)
+    return filename
+
+def test_exec_fail_withoutcode(cli, fname):
+    cli.ase('exec', fname, expect_fail=True)
+
+def test_exec_sample_cell(cli, fname):
+    atoms = bulk('Au')
+    out = cli.ase('exec', fname, '-e', 'print(atoms.cell.cellpar()[1])')
+    assert repr(atoms.cell.cellpar()[1]) in out
+
+def test_exec_index(cli, fname):
+    out = cli.ase('exec', fname, '-e', 'print(index)')
+    assert str(0) in out
+
+def test_exec_images(cli, fname):
+    atoms = bulk('Au')
+    out = cli.ase('exec', fname, '-e', 'print(len(images[0]))')
+    assert str(len(atoms)) in out
