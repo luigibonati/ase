29f2bc298ae9d984a8e57d235899fe98c7f787d0
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-01-16 12:58:39 +0100

    fix bug with scaled coordinates of center of mass with cell rank<3

--- ase/test/atoms/test_atoms.py
+++ ase/test/atoms/test_atoms.py
@@ -34,3 +34,18 @@ def test_set_masses():
     m0 = atoms.get_masses()
     atoms.set_masses([1, None])
     assert atoms.get_masses() == pytest.approx([1, m0[1]])
+
+
+def test_com():
+    """Test that atoms.get_center_of_mass(scaled=True) works"""
+
+    d = 1.142
+    a = Atoms('CO', positions=[(2, 0, 0), (2, -d, 0)], pbc=True)
+    a.set_cell(np.array(((4, -4, 0), (0, 5.657, 0), (0, 0, 10))))
+
+    def array_almost_equal(a1, a2, tol=np.finfo(type(1.0)).eps):
+        return (np.abs(a1 - a2) < tol).all()
+
+    scaledref = np.array((0.5, 0.23823622, 0.))
+    assert array_almost_equal(a.get_center_of_mass(scaled=True),
+                              scaledref, tol=1e-8)
