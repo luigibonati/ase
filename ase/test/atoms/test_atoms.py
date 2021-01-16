8979b10b1c9e2cb2fb9e0d085c34793914717d30
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-01-16 13:04:15 +0100

    add test for cell.rank<3

--- ase/test/atoms/test_atoms.py
+++ ase/test/atoms/test_atoms.py
@@ -36,12 +36,13 @@ def test_set_masses():
     assert atoms.get_masses() == pytest.approx([1, m0[1]])
 
 
-def test_com():
+@pytest.mark.parametrize('zlength', [0, 10])
+def test_com(zlength):
     """Test that atoms.get_center_of_mass(scaled=True) works"""
 
     d = 1.142
     a = Atoms('CO', positions=[(2, 0, 0), (2, -d, 0)], pbc=True)
-    a.set_cell(np.array(((4, -4, 0), (0, 5.657, 0), (0, 0, 10))))
+    a.set_cell(np.array(((4, -4, 0), (0, 5.657, 0), (0, 0, zlength))))
 
     def array_almost_equal(a1, a2, tol=np.finfo(type(1.0)).eps):
         return (np.abs(a1 - a2) < tol).all()
