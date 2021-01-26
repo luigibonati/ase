3d758b8e95b74dfc0f6cbbc070c3a4ed576fafe1
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-01-27 00:21:18 +0100

    add get_vasp_version and test thereof

--- ase/calculators/vasp/vasp.py
+++ ase/calculators/vasp/vasp.py
@@ -38,6 +38,11 @@ from ase.calculators.singlepoint import SinglePointDFTCalculator
 from ase.calculators.vasp.create_input import GenerateVaspInput
 
 
+def get_vasp_version(string):
+    match = re.search(r'(vasp\.\S+)', string, re.M)
+    return match.group(1)
+
+
 class Vasp(GenerateVaspInput, Calculator):  # type: ignore
     """ASE interface for the Vienna Ab initio Simulation Package (VASP),
     with the Calculator interface.
