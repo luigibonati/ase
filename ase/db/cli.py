2910b74e4a7a14ed88b192bfb2217f0b6a29769f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2021-01-27 09:42:58 +0100

    LINT

--- ase/db/cli.py
+++ ase/db/cli.py
@@ -2,7 +2,6 @@ import json
 import sys
 from collections import defaultdict
 from contextlib import contextmanager
-from random import randint
 from typing import Iterable, Iterator
 
 import ase.io
