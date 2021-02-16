877fe586e1a159932a64c5280f608f2b43dce75f
dskarls <dskpersistent@gmail.com>
2021-02-15 17:51:38 -0800

    TST: Move dimer_params fixture to lammpslib common fixtures

--- ase/test/calculator/lammpslib/test_lammpslib_post_changebox_cmds.py
+++ ase/test/calculator/lammpslib/test_lammpslib_post_changebox_cmds.py
@@ -5,17 +5,6 @@ from ase import Atoms
 
 
 @pytest.fixture
-def dimer_params():
-    dimer_params = {}
-    a = 2.0
-    dimer_params["symbols"] = "Ni" * 2
-    dimer_params["positions"] = [(0, 0, 0), (a, 0, 0)]
-    dimer_params["cell"] = (1000 * a, 1000 * a, 1000 * a)
-    dimer_params["pbc"] = (False, False, False)
-    return dimer_params
-
-
-@pytest.fixture
 def lj_epsilons():
     # Set up original LJ epsilon energy parameter and another, modified,
     # epsilon value
