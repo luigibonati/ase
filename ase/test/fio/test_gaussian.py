6cea06b2849ae89bedcd5db007d5a0cb33c2ff11
Laura Murgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-02-19 11:44:03 +0000

    TST: Use pytest fixtures to return StringIO objects

--- ase/test/fio/test_gaussian.py
+++ ase/test/fio/test_gaussian.py
@@ -1,110 +1,123 @@
 
-# Tests of Gaussian I/O
+import copy
 from io import StringIO
 
 import numpy as np
+import pytest
 from ase.atoms import Atoms
 from ase.io.gaussian import read_gaussian_in
 
-import copy
 
-# make an example input string with cartesian coords:
-fd_cartesian = StringIO(u'''
-%chk=example.chk
-%Nprocshared=16
-# N B3LYP/6-31G(d',p') ! ASE formatted method and basis
-# Opt(Tight, MaxCyc=100)/Integral=Ultrafine, Freq
+@pytest.fixture
+def fd_cartesian():
+    # make an example input string with cartesian coords:
+    fd_cartesian = StringIO('''
+    %chk=example.chk
+    %Nprocshared=16
+    # N B3LYP/6-31G(d',p') ! ASE formatted method and basis
+    # Opt(Tight, MaxCyc=100)/Integral=Ultrafine, Freq
 
-Gaussian input prepared by ASE
+    Gaussian input prepared by ASE
 
-0 1
-O1,  -0.464,   0.177,   0.0
-H1(iso=0.1134289259, NMagM=-8.89, ZEff=-1), -0.464,   1.137,   0.0
-H2(iso=2, spin=1, QMom=1, RadNuclear=1, ZNuc=2),   0.441,  -0.143,   0.0
-TV        10.0000000000        0.0000000000        0.0000000000
-TV         0.0000000000       10.0000000000        0.0000000000
-TV         0.0000000000        0.0000000000       10.0000000000
+    0 1
+    8,  -0.464,   0.177,   0.0
+    1(iso=0.1134289259, NMagM=-8.89, ZEff=-1), -0.464,   1.137,   0.0
+    1(iso=2, spin=1, QMom=1, RadNuclear=1, ZNuc=2),   0.441,  -0.143,   0.0
+    TV        10.0000000000        0.0000000000        0.0000000000
+    TV         0.0000000000       10.0000000000        0.0000000000
+    TV         0.0000000000        0.0000000000       10.0000000000
 
-''')
-
-# make an example input string with cartesian coords and a basis set
-# definition:
-fd_cartesian_basis_set = StringIO(u'''
-%chk=example.chk
-%Nprocshared=16
-# N B3LYP/Gen ! ASE formatted method and basis
-# Opt(Tight MaxCyc=100) Integral=Ultrafine Freq
-
-Gaussian input prepared by ASE
-
-0 1
-O1  -0.464   0.177   0.0
-H1(iso=0.1134289259) -0.464   1.137   0.0
-H2(iso=2)   0.441  -0.143   0.0
-
-H     0
-S    2   1.00
-      0.5447178000D+01       0.1562849787D+00
-      0.8245472400D+00       0.9046908767D+00
-S    1   1.00
-      0.1831915800D+00       1.0000000
-****
-O     0
-S    6   1.00
-      0.5472270000D+04       0.1832168810D-02
-      0.8178060000D+03       0.1410469084D-01
-      0.1864460000D+03       0.6862615542D-01
-      0.5302300000D+02       0.2293758510D+00
-      0.1718000000D+02       0.4663986970D+00
-      0.5911960000D+01       0.3641727634D+00
-SP   2   1.00
-      0.7402940000D+01      -0.4044535832D+00       0.2445861070D+00
-      0.1576200000D+01       0.1221561761D+01       0.8539553735D+00
-SP   1   1.00
-      0.3736840000D+00       0.1000000000D+01       0.1000000000D+01
-****
-
-''')
-
-# make an example input string with a z-matrix:
-fd_zmatrix = StringIO(u'''
-%chk=example.chk
-%Nprocshared=16
-# T B3LYP/Gen
-# opt=(Tight, MaxCyc=100)
-# integral(Ultrafine)
-freq=ReadIso
-
-Gaussian input with Z matrix
-
-0 1
-B 0 0.00 0.00 0.00
-H 0 1.31 0.00 0.00
-H 1 r1 2 a1
-B 2 r1 1 a2 3 d1
-H 1 r2 4 a3 2 d2
-H 1 r2 4 a3 2 -d2
-H 4 r2 1 a3 2 d2
-H 4 r2 1 a3 2 -d2
-Variables:
-r1 1.31
-r2 1.19
-a1 97
-a2 83
-a3 120
-d1 0
-d2 90
-
-300 1.0
-
-0.1134289259 ! mass of second H
-
-@basis-set-filename.gbs
-
-''')
+    ''')
+    return fd_cartesian
 
 
-def test_readwrite_gaussian():
+@pytest.fixture
+def fd_cartesian_basis_set():
+    # make an example input string with cartesian coords and a basis set
+    # definition:
+    fd_cartesian_basis_set = StringIO('''
+    %chk=example.chk
+    %Nprocshared=16
+    # N B3LYP/Gen ! ASE formatted method and basis
+    # Opt(Tight MaxCyc=100) Integral=Ultrafine Freq
+
+    Gaussian input prepared by ASE
+
+    0 1
+    O1  -0.464   0.177   0.0
+    H1(iso=0.1134289259) -0.464   1.137   0.0
+    H2(iso=2)   0.441  -0.143   0.0
+
+    H     0
+    S    2   1.00
+        0.5447178000D+01       0.1562849787D+00
+        0.8245472400D+00       0.9046908767D+00
+    S    1   1.00
+        0.1831915800D+00       1.0000000
+    ****
+    O     0
+    S    6   1.00
+        0.5472270000D+04       0.1832168810D-02
+        0.8178060000D+03       0.1410469084D-01
+        0.1864460000D+03       0.6862615542D-01
+        0.5302300000D+02       0.2293758510D+00
+        0.1718000000D+02       0.4663986970D+00
+        0.5911960000D+01       0.3641727634D+00
+    SP   2   1.00
+        0.7402940000D+01      -0.4044535832D+00       0.2445861070D+00
+        0.1576200000D+01       0.1221561761D+01       0.8539553735D+00
+    SP   1   1.00
+        0.3736840000D+00       0.1000000000D+01       0.1000000000D+01
+    ****
+
+    ''')
+
+    return fd_cartesian_basis_set
+
+
+@pytest.fixture
+def fd_zmatrix():
+    # make an example input string with a z-matrix:
+    fd_zmatrix = StringIO('''
+    %chk=example.chk
+    %Nprocshared=16
+    # T B3LYP/Gen
+    # opt=(Tight, MaxCyc=100)
+    # integral(Ultrafine)
+    freq=ReadIso
+
+    Gaussian input with Z matrix
+
+    0 1
+    B 0 0.00 0.00 0.00
+    H 0 1.31 0.00 0.00
+    H 1 r1 2 a1
+    B 2 r1 1 a2 3 d1
+    H 1 r2 4 a3 2 d2
+    H 1 r2 4 a3 2 -d2
+    H 4 r2 1 a3 2 d2
+    H 4 r2 1 a3 2 -d2
+    Variables:
+    r1 1.31
+    r2 1.19
+    a1 97
+    a2 83
+    a3 120
+    d1 0
+    d2 90
+
+    300 1.0
+
+    0.1134289259 ! mass of second H
+
+    @basis-set-filename.gbs
+
+    ''')
+
+    return fd_zmatrix
+
+
+def test_readwrite_gaussian(fd_cartesian, fd_cartesian_basis_set, fd_zmatrix):
     '''Tests the read_gaussian_in and write_gaussian_in methods'''
 
     def check_atom_properties(atoms, atoms_new, params):
