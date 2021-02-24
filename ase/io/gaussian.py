8134054bd0add8c8e4cdee0f3f3c0f54b79398e5
Laura Murgatroyd <laura.murgatroyd@stfc.ac.uk>
2021-02-24 15:46:17 +0000

    DOC: Add comments about ReadIso section

--- ase/io/gaussian.py
+++ ase/io/gaussian.py
@@ -616,9 +616,22 @@ def _delete_readiso_param(parameters):
 
 
 def _update_readiso_params(parameters, symbols):
+    ''' Deletes the ReadIso option from the route section as we
+    write out the masses in the nuclear properties section
+    instead of the ReadIso section.
+    Ensures the masses array is the same length as the
+    symbols array. This is necessary due to the way the
+    ReadIso section is defined:
+    The mass of each atom is listed on a separate line, in
+    order of appearance in the molecule spec. A blank line
+    indicates not to modify the mass for that atom.
+    But you do not have to leave blank lines equal to the
+    remaining atoms after you finsihed setting masses.
+    E.g. if you had 10 masses and only want to set the mass
+    for the first atom, you don't have to leave 9 blank lines
+    after it.
+    '''
     parameters = _delete_readiso_param(parameters)
-    # Ensures the masses array is the same length as the
-    # symbols array:
     if parameters.get('isolist') is not None:
         if len(parameters['isolist']) < len(symbols):
             for i in range(0, len(symbols) - len(parameters['isolist'])):
