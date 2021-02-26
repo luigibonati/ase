7c9b7fe066162e0e8b98a3f2230a5bd6dac0f967
Michael Walter <mcoywalter@gmail.com>
2021-02-26 07:46:19 +0100

    types to arrays to automatically copy

--- ase/io/opls.py
+++ ase/io/opls.py
@@ -183,7 +183,8 @@ minimize        1.0e-14 1.0e-5 100000 100000
         # header
         fileobj.write(fileobj.name + ' (by ' + str(self.__class__) + ')\n\n')
         fileobj.write(str(len(atoms)) + ' atoms\n')
-        fileobj.write(str(len(atoms.types)) + ' atom types\n')
+        fileobj.write(str(len(atoms.arrays['types']))
+                      + ' atom types\n')
         blist = connectivities['bonds']
         if len(blist):
             btypes = connectivities['bond types']
@@ -219,14 +220,14 @@ minimize        1.0e-14 1.0e-5 100000 100000
             molid = [1] * len(atoms)
         for i, r in enumerate(
                 p.vector_to_lammps(atoms.get_positions())):
-            atype = atoms.types[tag[i]]
+            atype = atoms.arrays['types'][tag[i]]
             if len(atype) < 2:
                 atype = atype + ' '
             q = self.data['one'][atype][2]
             fileobj.write('%6d %3d %3d %s %s %s %s' % ((i + 1, molid[i],
                                                         tag[i] + 1,
                                                         q) + tuple(r)))
-            fileobj.write(' # ' + atoms.types[tag[i]] + '\n')
+            fileobj.write(' # ' + atoms.arrays['types'][tag[i]] + '\n')
 
         # velocities
         velocities = atoms.get_velocities()
@@ -239,7 +240,7 @@ minimize        1.0e-14 1.0e-5 100000 100000
 
         # masses
         fileobj.write('\nMasses\n\n')
-        for i, typ in enumerate(atoms.types):
+        for i, typ in enumerate(atoms.arrays['types']):
             cs = atoms.split_symbol(typ)[0]
             fileobj.write('%6d %g # %s -> %s\n' %
                           (i + 1,
@@ -494,7 +495,7 @@ minimize        1.0e-14 1.0e-5 100000 100000
                       ' # consider changing these parameters\n')
         fileobj.write('special_bonds lj/coul 0.0 0.0 0.5\n')
         data = self.data['one']
-        for ia, atype in enumerate(atoms.types):
+        for ia, atype in enumerate(atoms.arrays['types']):
             if len(atype) < 2:
                 atype = atype + ' '
             fileobj.write('pair_coeff ' + str(ia + 1) + ' ' + str(ia + 1))
@@ -505,7 +506,7 @@ minimize        1.0e-14 1.0e-5 100000 100000
 
         # Charges
         fileobj.write('\n# charges\n')
-        for ia, atype in enumerate(atoms.types):
+        for ia, atype in enumerate(atoms.arrays['types']):
             if len(atype) < 2:
                 atype = atype + ' '
             fileobj.write('set type ' + str(ia + 1))
@@ -528,11 +529,11 @@ class OPLSStructure(Atoms):
         if filename:
             self.read_extended_xyz(filename)
         else:
-            self.types = []
+            self.arrays['types'] = []
             for atom in self:
-                if atom.symbol not in self.types:
-                    self.types.append(atom.symbol)
-                atom.tag = self.types.index(atom.symbol)
+                if atom.symbol not in self.arrays['types']:
+                    self.arrays['types'].append(atom.symbol)
+                atom.tag = self.arrays['types'].index(atom.symbol)
 
     def append(self, atom):
         """Append atom to end."""
@@ -552,7 +553,7 @@ class OPLSStructure(Atoms):
                 types.append(type)
             atom.tag = types_map[type]
             self.append(atom)
-        self.types = types
+        self.arrays['types'] = types
 
         # copy extra array info
         for name, array in atoms.arrays.items():
@@ -568,13 +569,13 @@ class OPLSStructure(Atoms):
         return string[0], string[1]
 
     def get_types(self):
-        return self.types
+        return self.arrays['types']
 
     def colored(self, elements):
         res = Atoms()
         res.set_cell(self.get_cell())
         for atom in self:
-            elem = self.types[atom.tag]
+            elem = self.arrays['types'][atom.tag]
             if elem in elements:
                 elem = elements[elem]
             res.append(Atom(elem, atom.position))
@@ -660,7 +661,7 @@ class OPLSStructure(Atoms):
             key = next_key()
 
         if key == 'Masses':
-            ntypes = len(self.types)
+            ntypes = len(self.arrays['types'])
             masses = np.empty((ntypes))
             for i in range(ntypes):
                 w = lines.pop(0).split()
@@ -690,13 +691,14 @@ class OPLSStructure(Atoms):
                 ams[np.isnan(ams)] = 0
                 for i, mass in enumerate(masses):
                     m2 = (ams - mass)**2
-                    symbolmap[self.types[i]] = chemical_symbols[m2.argmin()]
-                    typemap[self.types[i]] = newtype(
+                    symbolmap[self.arrays['types'][i]] = (
+                        chemical_symbols[m2.argmin()])
+                    typemap[self.arrays['types'][i]] = newtype(
                         chemical_symbols[m2.argmin()], types)
-                    types.append(typemap[self.types[i]])
+                    types.append(typemap[self.arrays['types'][i]])
                 for atom in self:
                     atom.symbol = symbolmap[atom.symbol]
-                self.types = types
+                self.arrays['types'] = types
 
             key = next_key()
 
