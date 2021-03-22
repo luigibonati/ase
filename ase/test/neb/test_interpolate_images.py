f8929fddd7287829ede856c32f6c0416c9fe66f4
Petr Grigorev <grigorievpit@gmail.com>
2021-03-22 16:32:55 +0100

    added test for images with FixAtoms constraint

--- ase/test/neb/test_interpolate_images.py
+++ ase/test/neb/test_interpolate_images.py
@@ -1,5 +1,6 @@
 from ase import Atoms
 from ase.neb import interpolate
+from ase.constraints import FixAtoms
 import numpy as np
 import pytest
 
@@ -41,6 +42,19 @@ def test_interpolate_images_default(images, initial, average_pos):
     assert_interpolated([image.positions for image in images])
     assert np.allclose(images[1].cell, initial.cell)
 
+def test_interpolate_images_fixed(images, initial, average_pos):
+
+    for image in images:
+        image.set_constraint(FixAtoms([0]))
+
+    interpolate(images)
+    assert images[1].positions == pytest.approx(images[0].positions)
+    assert np.allclose(images[1].cell, initial.cell)
+
+    interpolate(images, apply_constraint=False)
+    assert images[1].positions == pytest.approx(average_pos)
+    assert_interpolated([image.positions for image in images])
+    assert np.allclose(images[1].cell, initial.cell)
 
 def test_interpolate_images_scaled_coord(images, initial):
     interpolate(images, use_scaled_coord=True)
