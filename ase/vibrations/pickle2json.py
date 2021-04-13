876f91a6146b8f60b11b61731db5e9e6b164e524
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-04-13 21:59:13 +0200

    remember to actually commit the conversion tool

---
+++ ase/vibrations/pickle2json.py
@@ -0,0 +1,53 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import pickle
+
+import numpy as np
+
+from ase.utils import workdir
+from ase.utils.filecache import MultiFileJSONCache
+
+description = """
+Convert legacy pickle files from ASE vibrations calculations to JSON.
+
+Pickles are no longer supported for file storage.
+
+WARNING: Only run this command on trusted pickles since unpickling
+a maliciously crafted pickle allows arbitrary code execution.
+Indeed that is why pickles are no longer used.
+"""
+
+
+def port(picklefile):
+    picklefile = Path(picklefile)
+
+    name = picklefile.name
+
+    vibname, key, pckl = name.rsplit('.', 3)
+    assert pckl == 'pckl'
+
+    cache = MultiFileJSONCache(picklefile.parent / vibname)
+
+    obj = pickle.loads(picklefile.read_bytes())
+    assert isinstance(obj, np.ndarray), f'not supported: {type(obj)}'
+    dct = {'forces': obj}
+    outfilename = cache._filename(key)
+
+    if key in cache:
+        del cache[key]
+
+    cache[key] = dct
+    print(f'wrote {picklefile} ==> {outfilename}')
+
+
+def main(argv=None):
+    parser = ArgumentParser(description=description)
+    parser.add_argument('picklefile', nargs='+')
+    args = parser.parse_args(argv)
+
+    for fname in args.picklefile:
+        port(fname)
+
+
+if __name__ == '__main__':
+    main()
