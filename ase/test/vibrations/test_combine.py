d7732a56ab8494ece40363eeab506a382b3ed3e2
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-04-13 20:15:33 +0200

    re-enable read-from-outside-directory check

--- ase/test/vibrations/test_combine.py
+++ ase/test/vibrations/test_combine.py
@@ -1,53 +1,53 @@
 from numpy.random import RandomState
 
 from ase.build import molecule
-from ase.vibrations import Vibrations
-from ase.vibrations import Infrared
+from ase.vibrations import Vibrations, Infrared
+from ase.utils import workdir
 
 
-def test_combine():
-    class RandomCalculator():
-        """Fake Calculator class.
+class RandomCalculator:
+    """Fake Calculator class."""
+    def __init__(self):
+        self.rng = RandomState(42)
 
-        """
-        def __init__(self):
-            self.rng = RandomState(42)
+    def get_forces(self, atoms):
+        return self.rng.rand(len(atoms), 3)
 
-        def get_forces(self, atoms):
-            return self.rng.rand(len(atoms), 3)
-
-        def get_dipole_moment(self, atoms):
-            return self.rng.rand(3)
+    def get_dipole_moment(self, atoms):
+        return self.rng.rand(3)
 
 
-    atoms = molecule('C2H6')
-    ir = Infrared(atoms)
-    ir.calc = RandomCalculator()
-    ir.run()
-    freqs = ir.get_frequencies()
-    ints = ir.intensities
-    assert ir.combine() == 49
+def test_combine(testdir):
+    dirname = 'subdir'
+    vibname = 'ir'
+    with workdir(dirname, mkdir=True):
+        atoms = molecule('C2H6')
+        ir = Infrared(atoms)
+        assert ir.name == vibname
+        ir.calc = RandomCalculator()
+        ir.run()
+        freqs = ir.get_frequencies()
+        ints = ir.intensities
+        assert ir.combine() == 49
 
-    ir = Infrared(atoms)
-    assert (freqs == ir.get_frequencies()).all()
-    assert (ints == ir.intensities).all()
+        ir = Infrared(atoms)
+        assert (freqs == ir.get_frequencies()).all()
+        assert (ints == ir.intensities).all()
 
-    vib = Vibrations(atoms, name='ir')
-    assert (freqs == vib.get_frequencies()).all()
+        vib = Vibrations(atoms, name=vibname)
+        assert (freqs == vib.get_frequencies()).all()
 
-    # Read the data from other working directory
-    # dirname = os.path.basename(os.getcwd())
-    # os.chdir('..')  # Change working directory
-    # ir = Infrared(atoms, name=os.path.join(dirname, 'ir'))
-    # assert (freqs == ir.get_frequencies()).all()
-    # os.chdir(dirname)
+        # Read the data from other working directory
+        with workdir('..'):
+            ir = Infrared(atoms, name=f'{dirname}/{vibname}')
+            assert (freqs == ir.get_frequencies()).all()
 
-    ir = Infrared(atoms)
-    assert ir.split() == 1
-    assert (freqs == ir.get_frequencies()).all()
-    assert (ints == ir.intensities).all()
+        ir = Infrared(atoms)
+        assert ir.split() == 1
+        assert (freqs == ir.get_frequencies()).all()
+        assert (ints == ir.intensities).all()
 
-    vib = Vibrations(atoms, name='ir')
-    assert (freqs == vib.get_frequencies()).all()
+        vib = Vibrations(atoms, name=vibname)
+        assert (freqs == vib.get_frequencies()).all()
 
-    assert ir.clean() == 49
+        assert ir.clean() == 49
