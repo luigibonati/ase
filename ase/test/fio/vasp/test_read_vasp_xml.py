bdf83b623132b6eea01ce9b10bd80e571d107eea
Petr Grigorev <grigorievpit@gmail.com>
2021-04-14 15:36:49 +0200

    TST changed test_atoms() to test atoms read from vasprun.xml

--- ase/test/fio/vasp/test_read_vasp_xml.py
+++ ase/test/fio/vasp/test_read_vasp_xml.py
@@ -9,14 +9,29 @@ from ase.io import read
 def vasprun(datadir):
     return datadir / 'vasp' / 'vasprun.xml'
 
-def test_positions(vasprun, tmp_path):
+
+def test_atoms(vasprun, tmp_path):
     copyfile(vasprun, tmp_path / 'vasprun.xml')
     expected_scaled_positions = np.array([[0.0, 0.0, 0.0],
                                          [0.5,  0.5, 0.5]])
 
     atoms = read(tmp_path / 'vasprun.xml', index=-1)
+
+    # chek number of atoms
+    assert len(atoms)==2
+
+    # check scaled_positions
     np.testing.assert_allclose(atoms.get_scaled_positions(),
                                expected_scaled_positions)
+
+    expected_cell = np.array([[3.16, 0.0, 0.0],
+                              [0.0, 3.16, 0.0],
+                              [0.0, 0.0, 3.16]])
+
+    # check cell
+    np.testing.assert_allclose(atoms.cell, expected_cell)
+
+    # check real positions
     np.testing.assert_allclose(atoms.positions,
                                expected_scaled_positions @
                                atoms.cell.complete())
