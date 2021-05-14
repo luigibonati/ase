b5fc515016ede9ed8d1813c6dce40b77eb3af10f
Calvin <calvinku96@gmail.com>
2021-05-15 01:10:35 +0800

    BUG: skip over write_ns in ase.io.espresso.read_espresso_out

--- ase/io/espresso.py
+++ ase/io/espresso.py
@@ -319,7 +319,12 @@ def read_espresso_out(fileobj, index=-1, results_required=True):
 
         for bands_index in indexes[_PW_BANDS] + indexes[_PW_BANDSTRUCTURE]:
             if image_index < bands_index < next_index:
-                bands_index += 2
+                bands_index += 1
+                # skip over the lines with DFT+U occupation matrices
+                if 'enter write_ns' in pwo_lines[bands_index]:
+                    while 'exit write_ns' not in pwo_lines[bands_index]:
+                        bands_index += 1
+                bands_index += 1
 
                 if pwo_lines[bands_index].strip() == kpoints_warning:
                     continue
