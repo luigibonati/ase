648437799ee373de712c8df3ac98f37c6b30bc62
William <brojorgensen@protonmail.com>
2021-05-18 14:55:04 +0200

    add test for __repr__

--- ase/test/constraints/test_fixed_line.py
+++ ase/test/constraints/test_fixed_line.py
@@ -20,3 +20,10 @@ def test_valid_inputs(indices):
 def test_invalid_inputs(indices):
     with pytest.raises(ValueError) as e_info:
         c = FixedLine(indices, [1, 0, 0])
+
+@pytest.mark.parametrize('indices', [0, [0], [0, 1]])
+def test_repr(indices):
+    c = FixedLine(indices, [1, 0, 0])
+    repr(FixedLine(indices, [1, 0, 0])) == (
+        "<FixedLine: {'indices': " + str(indices) + ", 'direction': [1. 0. 0.]}>"
+    )
