c03851066dbfc30caec9a935ede44c7b4fae469c
William <brojorgensen@protonmail.com>
2021-05-18 14:54:50 +0200

    add test for invalid inputs

--- ase/test/constraints/test_fixed_line.py
+++ ase/test/constraints/test_fixed_line.py
@@ -10,3 +10,13 @@ import numpy as np
 @pytest.mark.parametrize('indices', [0, [0], [0, 1]])
 def test_valid_inputs(indices):
     c = FixedLine(indices, [1, 0, 0])
+
+@pytest.mark.parametrize(
+    'indices', [
+        [0, 1, 1],
+        [[0, 1], [0, 1]],
+    ]
+)
+def test_invalid_inputs(indices):
+    with pytest.raises(ValueError) as e_info:
+        c = FixedLine(indices, [1, 0, 0])
