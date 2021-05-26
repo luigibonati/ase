0d28a2ba38d85aaf736952943cd11ab14e693d4f
Adam J. Jackson <a.j.jackson@physics.org>
2021-05-26 18:02:32 +0100

    Vibrations: replace slab test using ForceConstantCalculator
    Instead of checking for expected frequencies/modes, just make sure
    that
    - the right Hessian makes it into VibrationsData
    - Animated modes handle the frozen atoms correctly
    
    Everything else should be covered adequately by other tests, and this
    greatly reduces the amount of data that has to be baked in.

--- ase/test/vibrations/test_vib.py
+++ ase/test/vibrations/test_vib.py
@@ -6,7 +6,6 @@ from numpy.testing import assert_array_almost_equal
 
 from ase import units, Atoms
 import ase.io
-from ase.calculators.emt import EMT
 from ase.calculators.qmmm import ForceConstantCalculator
 from ase.vibrations import Vibrations, VibrationsData
 from ase.thermochemistry import IdealGasThermo
@@ -487,32 +486,34 @@ class TestVibrationsData:
 
 
 class TestSlab:
-    "Adsorption of N2 on Ag slab - vibration with frozen molecules"
-    def setup(self):
-        # To reproduce n2_on_ag_data:
-        # from ase.build import fcc111, add_adsorbate
-        # ag_slab = fcc111('Ag', (4, 4, 2), a=2.031776)
-        # ag_slab.calc = EMT()
-        # n2 = Atoms('N2',
-        #            positions=[(0, 0, 0), (0, 0, 1.1)],
-        #            calculator=EMT())
-        # QuasiNewton(n2).run(fmax=0.01)
-        # add_adsorbate(ag_slab, n2, height=0.1, position='fcc')
-        # QuasiNewton(ag_slab).run(fmax=1e-3)
+    "N2 above Ag slab - vibration with frozen molecules"
+    def test_vibration_on_surface(self, testdir):
+        from ase.build import fcc111, add_adsorbate
+        ag_slab = fcc111('Ag', (4, 4, 2), a=2)
+        n2 = Atoms('N2', positions=[[0., 0., 0.],
+                                    [0., np.sqrt(2), np.sqrt(2)]])
+        add_adsorbate(ag_slab, n2, height=1, position='fcc')
 
-        self.n2_on_ag = Atoms(**n2_on_ag_data)
+        # Add an interaction between the N atoms
+        hessian_bottom_corner = np.zeros((2, 3, 2, 3))
+        hessian_bottom_corner[-1, :, -2] = [1, 1, 1]
+        hessian_bottom_corner[-2, :, -1] = [1, 1, 1]
 
-    def test_vibrations_on_surface(self, testdir):
-        atoms = self.n2_on_ag.copy()
-        atoms.calc = EMT()
-        vibs = Vibrations(atoms, indices=[-2, -1])
+        hessian = np.zeros((34, 3, 34, 3))
+        hessian[32:, :, 32:, :] = hessian_bottom_corner
+
+        ag_slab.calc = ForceConstantCalculator(hessian.reshape((34 * 3,
+                                                                34 * 3)),
+                                               ref=ag_slab.copy(),
+                                               f0=np.zeros((34, 3)))
+
+        # Check that Vibrations with restricted indices returns correct Hessian
+        vibs = Vibrations(ag_slab, indices=[-2, -1])
         vibs.run()
+        vibs.read()
 
-        freqs = vibs.get_frequencies()
-        assert len(freqs) == 6
-
-        vib_data = vibs.get_vibrations()
-        assert_array_almost_equal(freqs, vib_data.get_frequencies())
+        assert_array_almost_equal(vibs.get_vibrations().get_hessian(),
+                                  hessian_bottom_corner)
 
         # These should blow up if the vectors don't match number of atoms
         vibs.summary()
@@ -522,51 +523,5 @@ class TestSlab:
             # Frozen atoms should have zero displacement
             assert_array_almost_equal(vibs.get_mode(i)[0], [0., 0., 0.])
 
-            # At least one of the N atoms should have finite displacement
-            # (It's a strange test system, the N aren't interacting.)
-
-            assert np.any(vibs.get_mode(i)[-2:, :])
-
-
-n2_on_ag_data = {"positions": np.array([
-    0.11522782450469321, 1.1144065461066939, -7.372462459539312,
-    2.988453472155979, 1.1101021162088538, -7.348097091369633,
-    2.9889081250223644, 1.1097319309858253, -2.6771726550093766,
-    4.42748679033195, 0.27953500864036246, -5.023424263568356,
-    2.988647487319677, 2.7672422535369448, -9.643893082987166,
-    1.5522857100833238, 3.5986073870805897, -2.6783496788096266,
-    2.9888605955178464, 2.771359213321457, -5.023112326741649,
-    4.426002266203988, 3.5977377263928743, -2.676511065984475,
-    1.5434899579941024, 1.9689693193513231, 4.359003102544383,
-    2.9884153514662484, 4.426580053510861, -0.33110100322886604,
-    2.9889669670192083, 6.086834077920163, -2.677099904430725,
-    3.024247641227268, 4.433177847668567, 4.2898771182878415,
-    1.5502364630326282, 5.258366760370252, -9.643885588772582,
-    4.428995536176684, 3.5964348929876357, -7.372516399428504,
-    4.426684159759102, 5.258550258416217, -9.643980112734312,
-    7.295816104506667, 5.258864438883064, 2.019516665372352,
-    -1.3252650813821554, -1.3804744498930257, -7.3724702111318585,
-    1.5500159875082098, 0.27916914728271, -5.022740848670942,
-    2.9715785808019946, -0.5543782652837727, 4.358845144185516,
-    5.7954830502790236, -2.0529908028768005, 6.678266958798145,
-    0.1153238670863118, 2.7689332793398296, -5.025638884052345,
-    1.5600467349762661, 0.289360698297394, 1.99591074689944,
-    4.426218846810004, 1.937761330337365, -0.32974010002269494,
-    5.852146736614804, 2.7651313433743065, 2.0295010676684493,
-    1.5284008009881223, 0.3517577750219494, 6.6584232454693435,
-    1.5507901579723018, 1.9426262048750098, -0.3324184072062275,
-    5.863789878343788, 4.425475594763032, -0.33120515517967963,
-    4.420403704816752, 1.916168307407643, 4.444908498098164,
-    4.122554698411387, 0.7417811921529287, 7.875168164626224,
-    2.9952638361634243, 2.7603477361272026, 2.015529715672994,
-    5.816292331229005, 2.8333022021373018, 6.783605490492659,
-    7.438679230360008, 1.8826240456931018, 8.876555284894582,
-    -1.2449602230175023, -0.135081713547721, 5.9937992070297925,
-    0.7183412937260268, 0.4147345393028897, 44.45984325166285]).reshape(34, 3),
-    "numbers": [47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47,
-                47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47, 47,
-                47, 47, 47, 47, 47, 47, 47, 47, 7, 7],
-    "cell": [[5.746730349808315, 0.0, 0.0],
-             [2.8733651749041575, 4.976814471633033, 0.0],
-             [0.0, 0.0, 0.0]],
-    "pbc": [True, True, False]}
+            # The N atoms should have finite displacement
+            assert np.all(vibs.get_mode(i)[-2:, :])
