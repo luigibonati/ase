705a445c2aa689d18ec91d4177a241c88d5bc060
daniel speckhard <speckharddt@gmail.com>
2021-05-31 15:19:57 +0200

    making changes to exciting calc class.

--- ase/calculators/exciting.py
+++ ase/calculators/exciting.py
@@ -8,83 +8,152 @@ from ase.calculators.calculator import PropertyNotImplementedError
 from xml.dom import minidom
 
 
-class Exciting:
-    def __init__(self, dir='calc', paramdict=None,
-                 speciespath=None,
-                 bin='excitingser', kpts=(1, 1, 1),
-                 autormt=False, tshift=True, **kwargs):
-        """Exciting calculator object constructor
+ """Construct exciting-calculator object.
 
-        dir: string
-            directory in which to execute exciting
-        paramdict: dict
-            Dictionary containing XML parameters. String values are
-            translated to attributes, nested dictionaries are translated
-            to sub elements. A list of dictionaries is translated to a
-            list of sub elements named after the key of which the list
-            is the value.  Default: None
-        speciespath: string
-            Directory or URL to look up species files
-        bin: string
-            Path or executable name of exciting.  Default: ``excitingser``
-        kpts: integer list length 3
-            Number of k-points
-        autormt: bool
-            Bla bla?
-        kwargs: dictionary like
-            list of key value pairs to be converted into groundstate attributes
-
+        Parameters
+        ==========
+        dir (string): directory in which to execute exciting.
+        paramdict (dict): Dictionary containing XML parameters. String
+            values are translated to attributes, nested dictionaries are
+            translated to sub elements. A list of dictionaries is
+            translated to a  list of sub elements named after the key
+            of which the list is the value. Default: None
+        species_path: string
+            Directory or URL to look up species files folder.
+        exciting_binary (string): Path to executable of exciting code.
+            Default: 'excitingser'
+        kpts (list of ints): Number of k-points. List len should be 3.
+        autormt (bool): Assign the autormt boolean. If true the muffin tin
+            radius is set automatically by rmtapm.
+        kwargs (dict like): list of key value pairs to be
+            converted into groundstate attributes.
         """
+        # Assign member variables using contructor arguments.
         self.dir = dir
         self.energy = None
-
         self.paramdict = paramdict
-        if speciespath is None:
-            speciespath = os.environ['EXCITINGROOT'] + '/species'
-        self.speciespath = speciespath
+        # If the speciespath is not gives, try to locate it.
+        print(species_path)
+        # if species_path is None:
+        #     try: # TODO: check whether this dir exists.
+        #         species_path = os.environ['EXCITINGROOT'] + '/species'
+        #     except KeyError:
+        #         print('No EXCITINGROOT local variable found and no species path given.')
+        #         sys.exit('No species path given and no EXCITINGROOT local var found')
+        # try:
+        #     assert os.isdir(species_path)
+        # except:
+        #     print('hmm')
+        self.species_path = species_path
+        # We initialize our _calc.s+caconverged flag indicating
+        # whether the calculation is finished to False.
         self.converged = False
-        self.excitingbinary = bin
+        self.exciting_binary = exciting_binary
+        # TODO: find out what this does? Add comment.
         self.autormt = autormt
+        # TODO: find out what this does, add comment. 
+        # this is not a param in the constructor?.
         self.tshift = tshift
+        # Instead of defining paramdict you can also define
+        # kwargs seperately._calc.ss+ca
         self.groundstate_attributes = kwargs
-        if ('ngridk' not in kwargs.keys() and (not (self.paramdict))):
-            self.groundstate_attributes['ngridk'] = ' '.join(map(str, kpts))
+        # If we can't find ngrik in kwargs and paramdict=None
+        if ('ngridk' not in kwargs.keys() and (
+                not (self.paramdict))):
+            # Set the groundstate attributes ngridk value
+            # using the kpts constructure param. The join and map
+            # convert [2, 2, 2] into '2 2 2'.
+            self.groundstate_attributes[
+                'ngridk'] = ' '.join(map(str, kpts))
 
     def update(self, atoms):
-        if (not self.converged or
-            len(self.numbers) != len(atoms) or
-            (self.numbers != atoms.get_atomic_numbers()).any()):
+        """Initialize calc if needed then run exciting calc.
+        
+        Parameters
+        ====================================================
+        atoms (ASE Atoms Object): atom numbers, positions and
+            periodic boundary conditions.
+        """
+        # If the calculation is not over or the numbers
+        # member variable hasn't been initalized we initalize
+        # the atoms input.
+        if (
+                not self.converged or
+                len(self.numbers) != len(atoms) or
+                (self.numbers != atoms.get_atomic_numbers()).any()):
             self.initialize(atoms)
             self.calculate(atoms)
+        # Otherwise run the calculations with this ASE atoms object.
         elif ((self.positions != atoms.get_positions()).any() or
               (self.pbc != atoms.get_pbc()).any() or
               (self.cell != atoms.get_cell()).any()):
             self.calculate(atoms)
 
     def initialize(self, atoms):
+        """Initialize atomic information by writing input file.
+        
+        Parameters:
+        atoms (ASE object): ASE atoms object holding
+            geometry, atomic information about calculation.
+        """
+        # Get a list of the atomic numbers (a.m.u) save
+        # it to the member variable called self.numbers.
         self.numbers = atoms.get_atomic_numbers().copy()
+        # Write ASE atoms information into input.
         self.write(atoms)
 
     def get_potential_energy(self, atoms):
+        """Get potential energy.
+
+        Parameters:
+        ===========
+        atoms (ASE Atoms Object): positions, cell and pbc of input.
+
+        Returns:
+        ==========
+        forces (list of floats): total forces on the structure.
         """
-        returns potential Energy
-        """
+        # Update the atoms.
         self.update(atoms)
+        # Return the total energy of the calculation.
         return self.energy
 
     def get_forces(self, atoms):
+        """Run exciting calc and get forces.
+
+        Parameters:
+        ===========
+        atoms (ASE Atoms Object): positions, cell and pbc of input.
+
+        Returns:
+        ==========
+        forces (list of floats): total forces on the structure.
+        """
+        # Run exciting calculation and read output.
         self.update(atoms)
+        # return forces.
         return self.forces.copy()
 
     def get_stress(self, atoms):
         raise PropertyNotImplementedError
 
     def calculate(self, atoms):
+        """Run exciting calculation.
+        
+        Parameters:
+        ===========================
+        atoms (ASE Atoms Object): contains atomic
+            information.
+        """
+        # Get positions of the ASE Atoms object atom positions.
         self.positions = atoms.get_positions().copy()
+        # Get the ASE Atoms object cell vectors.
         self.cell = atoms.get_cell().copy()
+        # Get the periodic boundary conditions.
         self.pbc = atoms.get_pbc().copy()
-
+        # Write input file.
         self.initialize(atoms)
+
         from pathlib import Path
         xmlfile = Path(self.dir) / 'input.xml'
         assert xmlfile.is_file()
@@ -96,50 +165,78 @@ class Exciting:
         assert (Path(self.dir) / 'INFO.OUT').is_file()
         assert (Path(self.dir) / 'info.xml').exists()
 
-        #syscall = ('cd %(dir)s; %(bin)s;' %
-        #           {'dir': self.dir, 'bin': self.excitingbinary})
-        #print(syscall)
-        #assert os.system(syscall) == 0
+        # Command line call to calculate exciting.
+        # syscall = 'cd ' + self.dir + 's;' + self.exciting_binary + 's;'
+        # print(syscall)
+        # Ensure that the syscall doesn't return an error.
+        # assert os.system(syscall) == 0
+        # Read the results of the calculation.
         self.read()
 
     def write(self, atoms):
+        """Write atomic info inputs to an xml."""
+        # Check if the directory where we want to save our file exists.
+        # If not, create the directory.
         if not os.path.isdir(self.dir):
             os.mkdir(self.dir)
+        # Create an XML Document Object Model (DOM) where we can
+        # then assign different attributes of the DOM.
         root = atoms2etree(atoms)
-        root.find('structure').attrib['speciespath'] = self.speciespath
-        root.find('structure').attrib['autormt'] = str(self.autormt).lower()
-        root.find('structure').attrib['tshift'] = str(self.tshift).lower()
+        # Assign the species path to the XML DOM.
+        root.find('structure').attrib['speciespath'] = self.species_path
+        # Assign the autormt boolean.
+        root.find('structure').attrib['autormt'] = str(
+            self.autormt).lower()
+        # Assign the tshift bool. If true crystal is shifted so
+        # closest atom to origin is now at origin.
+        root.find('structure').attrib['tshift'] = str(
+            self.tshift).lower()
 
         def prettify(elem):
+            "Make the element prettier to read."
             rough_string = ET.tostring(elem, 'utf-8')
             reparsed = minidom.parseString(rough_string)
             return reparsed.toprettyxml(indent="\t")
 
-        if(self.paramdict):
+        # Check if paramdict is not None.
+        if self.paramdict:
+            # Assign dict key values to XML dom object root.
             self.dicttoxml(self.paramdict, root)
-            fd = open('%s/input.xml' % self.dir, 'w')
-            fd.write(prettify(root))
-            fd.close()
-        else:
+            with open(self.dir + '/input.xml', 'w') as fd:
+                fd.write(prettify(root))
+        else:  # For undefined paramdict.
             groundstate = ET.SubElement(root, 'groundstate', tforce='true')
             for key, value in self.groundstate_attributes.items():
                 if key == 'title':
                     root.findall('title')[0].text = value
                 else:
                     groundstate.attrib[key] = str(value)
-            fd = open('%s/input.xml' % self.dir, 'w')
-            fd.write(prettify(root))
-            fd.close()
+            with open(self.dir + '/input.xml', 'w') as fd:
+                fd.write(prettify(root))
+                fd.close()
 
     def dicttoxml(self, pdict, element):
+        """Write dictionary k,v paris to XML DOM object.
+
+        Parameters:
+        ================================
+        pdict (dict): dict with k,v pairs that go into
+            the xml like file.
+        element (XML DOM object): the XML object
+            that we want to modify using dict k,v pairs.
+        """
         for key, value in pdict.items():
             if (isinstance(value, str) and key == 'text()'):
                 element.text = value
             elif (isinstance(value, str)):
                 element.attrib[key] = value
             elif (isinstance(value, list)):
+                # if the value is a list, recursively call this
+                # method to add each member of the list with the
+                # same key for all of them.
                 for item in value:
                     self.dicttoxml(item, ET.SubElement(element, key))
+            # TODO: understand what this does.
             elif (isinstance(value, dict)):
                 if(element.findall(key) == []):
                     self.dicttoxml(value, ET.SubElement(element, key))
@@ -149,26 +246,39 @@ class Exciting:
                 print('cannot deal with', key, '=', value)
 
     def read(self):
+        """ Read total energy and forces from info.xml output.
+        
+        Reads info.xml output file into an object.
         """
-        reads Total energy and forces from info.xml
-        """
-        INFO_file = '%s/info.xml' % self.dir
-
+        # Define where to find output file which is called
+        # info.xml in exciing.
+        output_file = self.dir + '/info.xml'
+        # Try to open the output file.
         try:
-            fd = open(INFO_file)
+            with open(output_file, 'r') as outfile:
+                # Parse the XML output.
+                parsed_output = ET.parse(outfile)
         except IOError:
-            raise RuntimeError("output doesn't exist")
-        info = ET.parse(fd)
-        self.energy = float(info.findall(
-            'groundstate/scl/iter/energies')[-1].attrib['totalEnergy']) * Hartree
+            raise RuntimeError(
+                "Output file %s doesn't exist" % output_fle)
+        # Find the last istance of 'totalEnergy'.
+        self.energy = float(parsed_output.findall(
+            'groundstate/scl/iter/energies')[-1].attrib[
+                'totalEnergy']) * Hartree
+        # Initialize forces list.
         forces = []
+        # final all instances of 'totalforce'.
         forcesnodes = info.findall(
-            'groundstate/scl/structure')[-1].findall('species/atom/forces/totalforce')
+                'groundstate/scl/structure')[-1].findall(
+                    'species/atom/forces/totalforce')
+        # Go through each force in the found instances of 'total force'.
         for force in forcesnodes:
+            # Apend the total force to the forces list.
             forces.append(np.array(list(force.attrib.values())).astype(float))
+        # Reshape forces so we get three columns (x,y,z) and scale units.
         self.forces = np.reshape(forces, (-1, 3)) * Hartree / Bohr
-
+        # Check if the calculation converged.
         if str(info.find('groundstate').attrib['status']) == 'finished':
             self.converged = True
         else:
-            raise RuntimeError('calculation did not finish correctly')
+            raise RuntimeError('Calculation did not converge.')
