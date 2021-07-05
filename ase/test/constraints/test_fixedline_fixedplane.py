ecd6decd34c71db273614544557ab0d00c6f4534
William <brojorgensen@protonmail.com>
2021-07-05 11:04:43 +0200

    factor out duplicate code in tests

--- ase/test/constraints/test_fixedline_fixedplane.py
+++ ase/test/constraints/test_fixedline_fixedplane.py
@@ -1,9 +1,15 @@
 from ase.build import molecule
 from ase.calculators.emt import EMT
 from ase.constraints import FixedLine
+from ase.constraints import FixedPlane
 from ase.optimize import BFGS
 import numpy as np
 import pytest
+
+
+@pytest.fixture(params=[FixedLine, FixedPlane])
+def fixture_test_class(request):
+    return request.param
 
 
 @pytest.mark.parametrize(
@@ -14,8 +20,8 @@ import pytest
         np.array([0, 1], dtype=np.int64),
     ]
 )
-def test_valid_inputs_indices(indices):
-    _ = FixedLine(indices, [1, 0, 0])
+def test_valid_inputs_indices(fixture_test_class, indices):
+    _ = fixture_test_class(indices, [1, 0, 0])
 
 
 @pytest.mark.parametrize(
@@ -24,61 +30,29 @@ def test_valid_inputs_indices(indices):
         [[0, 1], [0, 1]],
     ]
 )
-def test_invalid_inputs_indices(indices):
+def test_invalid_inputs_indices(fixture_test_class, indices):
     with pytest.raises(ValueError) as _:
-        _ = FixedLine(indices, [1, 0, 0])
+        _ = fixture_test_class(indices, [1, 0, 0])
 
 
 @pytest.mark.parametrize('direction', [[0, 0, 1], (0, 0, 1)])
-def test_valid_inputs_direction(direction):
-    _ = FixedLine(0, direction)
+def test_valid_inputs_direction(fixture_test_class, direction):
+    _ = fixture_test_class(0, direction)
 
 
 @pytest.mark.parametrize('direction', [[0, 1], None, "42"])
-def test_invalid_inputs_direction(direction):
+def test_invalid_inputs_direction(fixture_test_class, direction):
     with pytest.raises(Exception) as _:
         _ = FixedLine(0, direction)
 
 
-@pytest.mark.parametrize('indices', [0, [0], [0, 1]])
-def test_repr(indices):
-    repr(FixedLine(indices, [1, 0, 0])) == (
-        "<FixedLine: "
-        "{'indices': " + str(indices) + ", 'direction': [1. 0. 0.]}>"
-    )
-
-
-def test_constrained_optimization_single():
-    c = FixedLine(0, [1, 0, 0])
-
+def _check_simple_constraints(constraints, indices):
     mol = molecule("butadiene")
-    mol.set_constraint(c)
+    mol.set_constraint(constraints)
 
     assert len(mol.constraints) == 1
-    assert isinstance(c.dir, np.ndarray)
-    assert (np.asarray([1, 0, 0]) == c.dir).all()
-
-    mol.calc = EMT()
-
-    cold_positions = mol[0].position.copy()
-    opt = BFGS(mol)
-    opt.run(steps=5)
-    cnew_positions = mol[0].position.copy()
-
-    assert np.max(np.abs(cnew_positions[1:] - cold_positions[1:])) < 1e-8
-    assert np.max(np.abs(cnew_positions[0] - cold_positions[0])) > 1e-8
-
-
-def test_constrained_optimization_multiple():
-    indices = [0, 1]
-    c = FixedLine(indices, [1, 0, 0])
-
-    mol = molecule("butadiene")
-    mol.set_constraint(c)
-
-    assert len(mol.constraints) == 1
-    assert isinstance(c.dir, np.ndarray)
-    assert (np.asarray([1, 0, 0]) == c.dir).all()
+    assert isinstance(constraints.dir, np.ndarray)
+    assert (np.asarray([1, 0, 0]) == constraints.dir).all()
 
     mol.calc = EMT()
 
@@ -87,5 +61,75 @@ def test_constrained_optimization_multiple():
     opt.run(steps=5)
     cnew_positions = mol[indices].positions.copy()
 
+    return cold_positions, cnew_positions
+
+
+@pytest.mark.parametrize('indices', [0, [0], [0, 1]])
+def test_repr_fixedline(fixture_test_class, indices):
+    repr(FixedLine(indices, [1, 0, 0])) == (
+        "<FixedLine: "
+        "{'indices': " + str(indices) + ", 'direction': [1. 0. 0.]}>"
+    )
+
+
+@pytest.mark.parametrize(
+    'indices,expected', [
+        (0, 2),
+        ([0], 2),
+        ([0, 1], 4),
+    ]
+)
+def test_removed_dof_fixedline(indices, expected):
+    constraints = FixedLine(indices, direction=[1, 0, 0])
+    print(constraints.get_removed_dof)
+    assert constraints.get_removed_dof == expected
+    
+
+@pytest.mark.parametrize('indices', [[0], [0, 1]])
+def test_constrained_optimization_fixedline(indices):
+    """
+    A single int is not tested as that changes the call from Atoms.positions
+    to Atom.position
+    """
+    constraints = FixedLine(indices, [1, 0, 0])
+
+    cold_positions, cnew_positions = _check_simple_constraints(
+        constraints, indices
+    )
     assert np.max(np.abs(cnew_positions[:, 1:] - cold_positions[:, 1:])) < 1e-8
     assert np.max(np.abs(cnew_positions[:, 0] - cold_positions[:, 0])) > 1e-8
+
+
+@pytest.mark.parametrize('indices', [0, [0], [0, 1]])
+def test_repr_fixedplane(fixture_test_class, indices):
+    repr(FixedPlane(indices, [1, 0, 0])) == (
+        "<FixedPlane: "
+        "{'indices': " + str(indices) + ", 'direction': [1. 0. 0.]}>"
+    )
+
+
+@pytest.mark.parametrize(
+    'indices,expected', [
+        (0, 1),
+        ([0], 1),
+        ([0, 1], 2),
+    ]
+)
+def test_removed_dof_fixedplane(indices, expected):
+    constraints = FixedPlane(indices, direction=[1, 0, 0])
+    assert constraints.get_removed_dof == expected
+
+
+@pytest.mark.parametrize('indices', [[0], [0, 1]])
+def test_constrained_optimization_fixedplane(indices):
+    """
+    A single int is not tested as that changes the call from Atoms.positions
+    to Atom.position
+    """
+    constraints = FixedPlane(indices, [1, 0, 0])
+
+    cold_positions, cnew_positions = _check_simple_constraints(
+        constraints, indices
+    )
+    assert np.max(np.abs(cnew_positions[:, 1:] - cold_positions[:, 1:])) > 1e-8
+    assert np.max(np.abs(cnew_positions[:, 0] - cold_positions[:, 0])) < 1e-8
