52ebdb660cd3b833c052c99be51fb1e45569706f
Michael Walter <mcoywalter@gmail.com>
2021-07-12 18:25:42 +0200

    Use ABC also in Siesta interface

--- ase/calculators/polarizability.py
+++ ase/calculators/polarizability.py
@@ -2,5 +2,8 @@ from abc import ABC
 
 
 class StaticPolarizabilityCalculator(ABC):
+    def __call__(self, atoms):
+        return self.calculate(atoms)
+
     def calculate(self, atoms):
         pass
