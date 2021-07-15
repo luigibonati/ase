3536d805b7282af2b4eca362efa7cc319cad88cb
Ivan Kondov <ivan.kondov@kit.edu>
2021-07-15 18:43:52 +0200

    ignore type and cleanup

--- ase/test/calculator/turbomole/test_turbomole_tm.py
+++ ase/test/calculator/turbomole/test_turbomole_tm.py
@@ -1,4 +1,3 @@
-"""unit tests for turbomole calculator that need no turbomole installed"""
 # type: ignore
 import pytest
 import numpy as np
@@ -7,10 +6,6 @@ from ase.calculators.turbomole import Turbomole
 @pytest.fixture
 def default_params():
     return {'multiplicity': 1}
-
-#@pytest.fixture
-#def calc(params):
-#    return Turbomole(**params)
 
 def test_turbomole_empty():
     with pytest.raises(AssertionError) as err:
@@ -24,6 +19,3 @@ def test_turbomole_default(default_params):
     assert calc['directory'] == '.'
     assert not calc['restart']
     assert calc['atoms'] is None
-#    assert calc['define_str'] is None
-#
-#    assert not calc['restart']
