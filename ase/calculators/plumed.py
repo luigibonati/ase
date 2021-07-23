9483ad1c2a9dd6e2883e3f73946df4c8a5ec53c5
Sucerquia <danielsucerquia@hotmail.com>
2021-07-23 15:02:57 -0500

    ENH: assert line in the method 'read' of plumed calculator

--- ase/calculators/plumed.py
+++ ase/calculators/plumed.py
@@ -147,6 +147,8 @@ class Plumed(Calculator):
                 read_files['COLVAR'] = np.loadtxt('COLVAR', unpack=True)
             if exists('HILLS'):
                 read_files['HILLS'] = np.loadtxt('HILLS', unpack=True)
+        
+        assert not len(read_files) == 0, "There are not files for reading"
         return read_files
 
     def __enter__(self):
