9eb37db2ae7b70a0bb5379a9ba6783c47b0336f5
Michael Walter <mcoywalter@gmail.com>
2021-07-27 09:27:38 +0200

    removed __call__() and added docstring. Calculate as abstractmethod

--- ase/calculators/polarizability.py
+++ ase/calculators/polarizability.py
@@ -1,9 +1,15 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class StaticPolarizabilityCalculator(ABC):
-    def __call__(self, atoms):
-        return self.calculate(atoms)
-
+    @abstractmethod
     def calculate(self, atoms):
+        """Calculate the polarizability tensor
+
+        atoms: Atoms object
+        
+        Returns:
+          Polarizabilty tensor (3x3 matrix) in units (e^2 Angstrom^2 / eV)
+          Can be multiplied by Bohr * Ha to get (Angstrom^3)
+        """
         pass
