3ed6f79570760646867e4da3a9fa99e4f702a9aa
Martin Schlipf <martin.schlipf@gmail.com>
2021-11-17 18:06:56 +0100

    Add test for dictionary arguments

--- ase/io/vasp_parsers/incar_writer.py
+++ ase/io/vasp_parsers/incar_writer.py
@@ -1,16 +1,26 @@
 from collections.abc import Iterable
 
+
 def write_incar(parameters):
     incar_lines = []
-    for key, value in parameters.items():
-        if isinstance(value,str):
-            if value.find("\n") != -1:
-                value = '"' + value + '"'
-            incar_lines.append(f"{key} = {value}")
-        elif isinstance(value,Iterable):
-            incar_lines.append(f"{key} = {' '.join(str(x) for x in value)}")
-        else:
-            incar_lines.append(f"{key} = {value}")
-
+    for item in parameters.items():
+        incar_lines += list(generate_line(*item))
     with open("INCAR", "w") as incar:
         incar.write("\n".join(incar_lines))
+
+
+def generate_line(key, value, num_spaces=0):
+    indent = " " * num_spaces
+    if isinstance(value, str):
+        if value.find("\n") != -1:
+            value = '"' + value + '"'
+        yield indent + f"{key} = {value}"
+    elif isinstance(value, dict):
+        yield indent + f"{key} {{"
+        for item in value.items():
+            yield from generate_line(*item, num_spaces + 4)
+        yield indent + "}"
+    elif isinstance(value, Iterable):
+        yield indent + f"{key} = {' '.join(str(x) for x in value)}"
+    else:
+        yield indent + f"{key} = {value}"
