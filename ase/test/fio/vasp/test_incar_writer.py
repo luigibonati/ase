2985e8311c714e49563bf57012c741932a36db71
Martin Schlipf <martin.schlipf@gmail.com>
2021-11-17 16:51:33 +0100

    Refactor common part of test into function

--- ase/test/fio/vasp/test_incar_writer.py
+++ ase/test/fio/vasp/test_incar_writer.py
@@ -3,35 +3,30 @@ from unittest.mock import mock_open, patch
 
 
 def test_write_string_to_incar():
+    parameters = {"INCAR_TAG": "string"}
+    expected_output = "INCAR_TAG = string"
+    check_write_incar_file(parameters, expected_output)
+
+
+def check_write_incar_file(parameters, expected_output):
     mock = mock_open()
     with patch("ase.io.vasp_parsers.incar_writer.open", mock):
-        write_incar({"INCAR_TAG": "string"})
+        write_incar(parameters)
         mock.assert_called_once_with("INCAR", "w")
         incar = mock()
-        incar.write.assert_called_once_with("INCAR_TAG = string")
+        incar.write.assert_called_once_with(expected_output)
 
 
 def test_write_integer_to_incar():
-    mock = mock_open()
-    with patch("ase.io.vasp_parsers.incar_writer.open", mock):
-        write_incar({"INCAR_TAG": 5})
-        mock.assert_called_once_with("INCAR", "w")
-        incar = mock()
-        incar.write.assert_called_once_with("INCAR_TAG = 5") 
-                                                                                                   
+    parameters = {"INCAR_TAG": 5}
+    expected_output = "INCAR_TAG = 5"
+    check_write_incar_file(parameters, expected_output)
 
-def test_write_True_to_incar():
-    mock = mock_open()
-    with patch("ase.io.vasp_parsers.incar_writer.open", mock):
-        write_incar({"INCAR_TAG": True})
-        mock.assert_called_once_with("INCAR", "w")
-        incar = mock()
-        incar.write.assert_called_once_with("INCAR_TAG = True")                                                                                                   
 
-def test_write_False_to_incar():
-    mock = mock_open()
-    with patch("ase.io.vasp_parsers.incar_writer.open", mock):
-        write_incar({"INCAR_TAG": False})
-        mock.assert_called_once_with("INCAR", "w")
-        incar = mock()
-        incar.write.assert_called_once_with("INCAR_TAG = False")                                                                                                    
+def test_write_bool_to_incar():
+    parameters = {"INCAR_TAG": True}
+    expected_output = "INCAR_TAG = True"
+    check_write_incar_file(parameters, expected_output)
+    parameters = {"INCAR_TAG": False}
+    expected_output = "INCAR_TAG = False"
+    check_write_incar_file(parameters, expected_output)
