74985f04126ccccf297a8331d979745ee99ecf7b
MichelangeloDomina <domina.michelangelo@gmail.com>
2021-11-17 16:26:02 +0000

    Multiple Text in the incar tested

--- ase/io/vasp_parsers/incar_writer.py
+++ ase/io/vasp_parsers/incar_writer.py
@@ -1,10 +1,12 @@
 from collections.abc import Iterable
 
-
 def write_incar(parameters):
+    incar_lines = []
+    for key, value in parameters.items():
+        if isinstance(value,Iterable) and not isinstance(value,str):
+            incar_lines.append(f"{key} = {' '.join(str(x) for x in value)}")
+        else:
+            incar_lines.append(f"{key} = {value}")
+
     with open("INCAR", "w") as incar:
-        for key, value in parameters.items():
-            if isinstance(value, Iterable) and not isinstance(value, str):
-                incar.write(f"{key} = {' '.join(str(x) for x in value)}")
-            else:
-                incar.write(f"{key} = {value}")
+        incar.write("\n".join(incar_lines))
