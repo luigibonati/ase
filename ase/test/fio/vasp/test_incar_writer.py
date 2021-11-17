61513641c779e12163737a465481b91aca011f92
Michelangelo Domina <dominam@parsons01.cluster>
2021-11-17 15:42:06 +0000

    EDITED NEW TEST_INCAR (NUMBER,TRUE,FALSE)

--- ase/test/fio/vasp/test_incar_writer.py
+++ ase/test/fio/vasp/test_incar_writer.py
@@ -9,3 +9,29 @@ def test_write_string_to_incar():
         mock.assert_called_once_with("INCAR", "w")
         incar = mock()
         incar.write.assert_called_once_with("INCAR_TAG = string")
+
+
+def test_write_integer_to_incar():
+    mock = mock_open()
+    with patch("ase.io.vasp_parsers.incar_writer.open", mock):
+        write_incar({"INCAR_TAG": 5})
+        mock.assert_called_once_with("INCAR", "w")
+        incar = mock()
+        incar.write.assert_called_once_with("INCAR_TAG = 5") 
+                                                                                                   
+
+def test_write_True_to_incar():
+    mock = mock_open()
+    with patch("ase.io.vasp_parsers.incar_writer.open", mock):
+        write_incar({"INCAR_TAG": True})
+        mock.assert_called_once_with("INCAR", "w")
+        incar = mock()
+        incar.write.assert_called_once_with("INCAR_TAG = True")                                                                                                   
+
+def test_write_False_to_incar():
+    mock = mock_open()
+    with patch("ase.io.vasp_parsers.incar_writer.open", mock):
+        write_incar({"INCAR_TAG": False})
+        mock.assert_called_once_with("INCAR", "w")
+        incar = mock()
+        incar.write.assert_called_once_with("INCAR_TAG = False")                                                                                                    
