73de15c616c425050b95a4f9ca07f667e8c689b2
Martin Schlipf <martin.schlipf@gmail.com>
2021-11-17 17:09:34 +0100

    Fix writing lists to INCAR

--- ase/io/vasp_parsers/incar_writer.py
+++ ase/io/vasp_parsers/incar_writer.py
@@ -1,4 +1,10 @@
+from collections.abc import Iterable
+
+
 def write_incar(parameters):
     with open("INCAR", "w") as incar:
         for key, value in parameters.items():
-            incar.write(f"{key} = {value}")
+            if isinstance(value, Iterable) and not isinstance(value, str):
+                incar.write(f"{key} = {' '.join(str(x) for x in value)}")
+            else:
+                incar.write(f"{key} = {value}")
