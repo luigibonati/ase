9ea61db33cfbde471398733bf0d02b5d097233e2
Martin Schlipf <martin.schlipf@gmail.com>
2021-11-18 12:19:52 +0100

    Implement KPOINTS for Monkhorst mode

--- ase/io/vasp_parsers/kpoints_writer.py
+++ ase/io/vasp_parsers/kpoints_writer.py
@@ -1,12 +1,23 @@
 def write_kpoints(parameters):
     with open("KPOINTS", "w") as kpoints:
         if "Auto" in parameters:
-            kpoints.write(f"""KPOINTS created by Atomic Simulation Environment
+            kpoints.write(
+                f"""KPOINTS created by Atomic Simulation Environment
 0
 Auto
-{parameters['Auto']}""")
+{parameters['Auto']}"""
+            )
         elif "Gamma" in parameters:
-            kpoints.write(f"""KPOINTS created by Atomic Simulation Environment
+            kpoints.write(
+                f"""KPOINTS created by Atomic Simulation Environment
 0
 Gamma
-{" ".join(str(x) for x in parameters['Gamma'])}""")
+{" ".join(str(x) for x in parameters['Gamma'])}"""
+            )
+        elif "Monkhorst" in parameters:
+            kpoints.write(
+                f"""KPOINTS created by Atomic Simulation Environment
+0
+Monkhorst
+{" ".join(str(x) for x in parameters['Monkhorst'])}"""
+            )
--- ase/test/fio/vasp/test_kpoints_writer.py
+++ ase/test/fio/vasp/test_kpoints_writer.py
@@ -1,27 +1,38 @@
 from ase.io.vasp_parsers.kpoints_writer import write_kpoints
 from unittest.mock import mock_open, patch
 
-def check_write_kpoints_file(parameters,expected_output):
+
+def check_write_kpoints_file(parameters, expected_output):
     mock = mock_open()
     with patch("ase.io.vasp_parsers.kpoints_writer.open", mock):
         write_kpoints(parameters)
         mock.assert_called_once_with("KPOINTS", "w")
         kpoints = mock()
         kpoints.write.assert_called_once_with(expected_output)
- 
+
+
 def test_kpoints_fully_automatic_mode():
-    parameters = {"Auto" : 10}
+    parameters = {"Auto": 10}
     expected_output = """KPOINTS created by Atomic Simulation Environment
 0
 Auto
 10"""
-    check_write_kpoints_file(parameters,expected_output) 
+    check_write_kpoints_file(parameters, expected_output)
 
 
 def test_kpoints_fully_automatic_mode():
-    parameters = {"Gamma" : [4,4,4]}
+    parameters = {"Gamma": [4, 4, 4]}
     expected_output = """KPOINTS created by Atomic Simulation Environment
 0
 Gamma
 4 4 4"""
-    check_write_kpoints_file(parameters,expected_output) 
+    check_write_kpoints_file(parameters, expected_output)
+
+
+def test_kpoints_fully_automatic_mode():
+    parameters = {"Monkhorst": [4, 4, 4]}
+    expected_output = """KPOINTS created by Atomic Simulation Environment
+0
+Monkhorst
+4 4 4"""
+    check_write_kpoints_file(parameters, expected_output)
