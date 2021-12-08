c2d06ecea227f39b89dbcea4fc2b68de6f928fc5
Ask Hjorth Larsen <asklarsen@gmail.com>
2021-12-08 13:43:26 +0100

    claim more ports

--- ase/test/test_socket_port0.py
+++ ase/test/test_socket_port0.py
@@ -2,10 +2,10 @@ import socket
 
 
 def test_socket_port0():
-    sock = socket.socket()
-    x = sock.bind(('', 0))
-    assert x is None
-    ip, port = sock.getsockname()
-    print('ip', ip)
-    print('port', port)
+    for i in range(40):
+        sock = socket.socket()
+        x = sock.bind(('', 0))
+        assert x is None
+        ip, port = sock.getsockname()
+        print('ip', ip, 'port', port)
     assert 0
