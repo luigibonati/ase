a37d369b9b2f85e5e9c746119fd170d1a966f7a7
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-01-17 23:10:28 +0100

    move numeric forces and stress to ase.calculator.test as functions.  Keep stubs on Calculator for now, but they should go

--- ase/calculators/calculator.py
+++ ase/calculators/calculator.py
@@ -827,50 +827,13 @@ class Calculator(BaseCalculator):
         """Calculate numerical forces using finite difference.
 
         All atoms will be displaced by +d and -d in all directions."""
-
-        from ase.calculators.test import numeric_force
-        return np.array([[numeric_force(atoms, a, i, d)
-                          for i in range(3)] for a in range(len(atoms))])
+        from ase.calculators.test import numeric_forces
+        return numeric_forces(atoms, d=d)
 
     def calculate_numerical_stress(self, atoms, d=1e-6, voigt=True):
         """Calculate numerical stress using finite difference."""
-
-        stress = np.zeros((3, 3), dtype=float)
-
-        cell = atoms.cell.copy()
-        V = atoms.get_volume()
-        for i in range(3):
-            x = np.eye(3)
-            x[i, i] += d
-            atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-            eplus = atoms.get_potential_energy(force_consistent=True)
-
-            x[i, i] -= 2 * d
-            atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-            eminus = atoms.get_potential_energy(force_consistent=True)
-
-            stress[i, i] = (eplus - eminus) / (2 * d * V)
-            x[i, i] += d
-
-            j = i - 2
-            x[i, j] = d
-            x[j, i] = d
-            atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-            eplus = atoms.get_potential_energy(force_consistent=True)
-
-            x[i, j] = -d
-            x[j, i] = -d
-            atoms.set_cell(np.dot(cell, x), scale_atoms=True)
-            eminus = atoms.get_potential_energy(force_consistent=True)
-
-            stress[i, j] = (eplus - eminus) / (4 * d * V)
-            stress[j, i] = stress[i, j]
-        atoms.set_cell(cell, scale_atoms=True)
-
-        if voigt:
-            return stress.flat[[0, 4, 8, 5, 2, 1]]
-        else:
-            return stress
+        from ase.calculators.test import numeric_stress
+        return numeric_stress(atoms, d=d, voigt=voigt)
 
     def _deprecated_get_spin_polarized(self):
         msg = ('This calculator does not implement get_spin_polarized().  '
