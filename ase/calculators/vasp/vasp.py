c219d1ef64e1012076a2efcb7443d0edd5e7d485
Patrick Melix <patrick.melix@uni-leipzig.de>
2022-01-27 14:37:52 +0100

    fix missing lines

--- ase/calculators/vasp/vasp.py
+++ ase/calculators/vasp/vasp.py
@@ -1153,12 +1153,14 @@ class Vasp(GenerateVaspInput, Calculator):  # type: ignore
 
     def read_vib_freq(self, lines=None):
         """Read vibrational frequencies.
-        Returns list of real and list of imaginary frequencies."""
 
+        Returns list of real and list of imaginary frequencies."""
         freq = []
         i_freq = []
+
         if not lines:
             lines = self.load_file('OUTCAR')
+
         for line in lines:
             data = line.split()
             if 'THz' in data:
