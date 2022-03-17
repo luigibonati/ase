38efb7f0c695c10fce385ed8f5d9d8960ae50754
Peter Koval <koval.peter@gmail.com>
2022-03-17 14:28:17 +0100

    added a test on reading of the siesta.EIG file

--- ase/test/calculator/siesta/test_siesta_read_eigenvalues.py
+++ ase/test/calculator/siesta/test_siesta_read_eigenvalues.py
@@ -1,9 +1,18 @@
 import ase.build
+from ase.calculators.siesta import Siesta
+
+
+def test_siesta_read_eigenvalues_soc(datadir):
+    """ In this test, we read a stored siesta.EIG file."""
+    calc = Siesta()
+    assert calc.read_eigenvalues() == 1
+    calc.directory = datadir / 'siesta'
+    assert calc.read_eigenvalues() == 0
+    assert calc.results['eigenvalues'].shape == (1, 1, 30)
 
 
 def test_siesta_read_eigenvalues(siesta_factory):
-    # Test real calculation which produces a
-    # gapped .EIG file
+    # Test real calculation which produces a gapped .EIG file
     atoms = ase.build.bulk('Si', cubic=True)
     calc = siesta_factory.calc(kpts=[2, 1, 1])
     atoms.calc = calc
@@ -12,3 +21,5 @@ def test_siesta_read_eigenvalues(siesta_factory):
     assert calc.results['eigenvalues'].shape[:2] == (1, 2)  # spins x bands
     assert calc.get_k_point_weights().shape == (2,)
     assert calc.get_ibz_k_points().shape == (2, 3)
+
+
