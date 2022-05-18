0f2db2c7138b9a24a3fa5c3ae690854ecda759ff
Patrick Melix <patrick.melix@uni-leipzig.de>
2022-05-18 12:16:25 +0200

    orca autoclass doc, doc enhanced, added SP test, enhanced tests

--- ase/test/calculator/test_orca.py
+++ ase/test/calculator/test_orca.py
@@ -4,27 +4,32 @@ import numpy as np
 from ase.calculators.orca import ORCA
 from ase.atoms import Atoms
 from ase.units import Hartree
+from ase.optimize import BFGS
 
 @pytest.fixture
 def water():
     return Atoms('OHH', positions=[(0, 0, 0), (1, 0, 0), (0, 1, 0)])
 
-def test_orca(water):
-    from ase.optimize import BFGS
 
-    water.calc = ORCA(label='water',
-                      orcasimpleinput='BLYP def2-SVP')
+def test_orca(water):
+    water.calc = ORCA(label='water', orcasimpleinput='BLYP def2-SVP Engrad')
 
     with BFGS(water) as opt:
         opt.run(fmax=0.05)
 
     final_energy = water.get_potential_energy()
-    print(final_energy)
+    np.testing.assert_almost_equal(final_energy, -2077.24420, decimal=0)
 
-    assert abs(final_energy + 2077.24420) < 1.0
+
+def test_orca_sp(water):
+    water.calc = ORCA(label='water', orcasimpleinput='BLYP def2-SVP', task="SP")
+
+    final_energy = water.get_potential_energy()
+    np.testing.assert_almost_equal(final_energy, -2077.24420, decimal=0)
+
 
 def test_orca_use_last_energy(water):
     water.calc = ORCA(label='water', orcasimpleinput='PBE def2-SVP Opt TightOpt')
     energy = water.get_potential_energy() / Hartree
 
-    assert np.testing.assert_almost_equal(energy, -76.272686944630, decimal=5)
+    np.testing.assert_almost_equal(energy, -76.272686944630, decimal=6)
