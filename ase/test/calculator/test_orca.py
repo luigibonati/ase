36a3beabace8e0f6c3c802d3fb4108eef7caee6d
Patrick Melix <patrick.melix@uni-leipzig.de>
2022-05-18 10:33:04 +0200

    make orca test use fixture atoms

--- ase/test/calculator/test_orca.py
+++ ase/test/calculator/test_orca.py
@@ -1,18 +1,23 @@
-def test_orca():
+import pytest
+
+from ase.calculators.orca import ORCA
+from ase.atoms import Atoms
+
+@pytest.fixture
+def water():
+    return Atoms('OHH', positions=[(0, 0, 0), (1, 0, 0), (0, 1, 0)])
+
+def test_orca(water):
     from ase.optimize import BFGS
-    from ase.atoms import Atoms
-    from ase.calculators.orca import ORCA
 
-    atoms = Atoms('OHH',
-                  positions=[(0, 0, 0), (1, 0, 0), (0, 1, 0)])
-
-    atoms.calc = ORCA(label='water',
+    water.calc = ORCA(label='water',
                       orcasimpleinput='BLYP def2-SVP')
 
-    with BFGS(atoms) as opt:
+    with BFGS(water) as opt:
         opt.run(fmax=0.05)
 
-    final_energy = atoms.get_potential_energy()
+    final_energy = water.get_potential_energy()
     print(final_energy)
 
     assert abs(final_energy + 2077.24420) < 1.0
+
