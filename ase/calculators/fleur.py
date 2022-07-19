d7ac47fa7e8a355b0172a30f460549914134bf54
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-07-19 20:52:55 +0200

    shorten message and include link

--- ase/calculators/fleur.py
+++ ase/calculators/fleur.py
@@ -1,5 +1,5 @@
 def FLEUR(*args, **kwargs):
     raise RuntimeError(
-        'The FLEUR calculator has been removed due to lack of maintenance.  '
-        'For ASE support for modern FLEUR versions, '
-        'please refer to the python package ase-fleur')
+        'Please refer to the python package ase-fleur for '
+        'ASE support for modern FLEUR versions.  See: '
+        'https://github.com/JuDFTteam/ase-fleur.')
