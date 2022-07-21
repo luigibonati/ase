9884c51ef7207581b6d02d65143061ab6aec3ba6
Tuomas Rossi <tuomas.rossi@alumni.aalto.fi>
2022-07-21 13:32:27 +0300

    Increase maximum module count

--- ase/test/cli/test_imports.py
+++ ase/test/cli/test_imports.py
@@ -9,6 +9,7 @@ def test_imports():
         'ase.io.formats',  # possibly slow external formats
         'ase.calculators.(?!names).*',  # any calculator
     ]
+    max_module_count = 333  # this depends on the environment
     check_imports("from ase.cli.main import main; main(args=[])",
                   forbidden_modules=forbidden_modules,
-                  max_module_count=270)
+                  max_module_count=max_module_count)
