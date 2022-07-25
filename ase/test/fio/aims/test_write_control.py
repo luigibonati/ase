8ff7bc6a8a963d024c7cd5aaad6c1f38b537694a
dts <speckhard@fhi.mpg.de>
2022-07-25 17:06:38 +0200

    Linting.

--- ase/test/fio/aims/test_write_control.py
+++ ase/test/fio/aims/test_write_control.py
@@ -4,16 +4,19 @@ Control.in file contains calculation parameters such as the functional and
 k grid as well as basis set size parameters. We write this file to a string
 and assert we find expected values.
 """
+# Standard imports.
+import io
+import pathlib
 import re
-from pathlib import Path
-from io import StringIO
+# Third party imports.
 import pytest
-from ase.io.aims import write_control, write_species, translate_tier, format_tiers, get_tier_list
-from ase.build import bulk
-from ase.calculators.aims import AimsCube
+
+import ase.io.aims
+import ase.build
+import ase.calculators.aims
 
 
-parent = Path(__file__).parent
+parent = pathlib.Path(__file__).parent
 
 
 _PARAMETERS_DICT = {
@@ -37,7 +40,7 @@ def contains(pattern, txt):
 @pytest.fixture(name="create_au_bulk_atoms_obj")
 def fixture_create_au_bulk_atoms_obj():
     """Create an ASE.Atoms bulk object of Gold."""
-    return bulk("Au")
+    return ase.build.bulk("Au")
 
 
 def test_control(create_au_bulk_atoms_obj):
@@ -47,13 +50,17 @@ def test_control(create_au_bulk_atoms_obj):
     the aims IO functionality for writing the conrol.in file, such as adding an
     AimsCube to the system.
     """
-    # Copy the global parameters dicitonary to avoid rewriting common parameters.
+    # Copy the global parameters dicitonary to avoid rewriting common
+    # parameters.
     parameters = _PARAMETERS_DICT.copy()
-     # Add AimsCube to the parameter dictionary.
-    parameters["cubes"] = AimsCube(plots=("delta_density",))
-    # Write control.in file to a string which we can directly access for testing.
-    string_output = StringIO()
-    write_control(string_output, create_au_bulk_atoms_obj, parameters)
+    # Add AimsCube to the parameter dictionary.
+    parameters["cubes"] = ase.calculators.aims.AimsCube(
+        plots=("delta_density",))
+    # Write control.in file to a string which we can directly access for
+    # testing.
+    string_output = io.StringIO()
+    ase.io.aims.write_control(
+        string_output, create_au_bulk_atoms_obj, parameters)
     txt = string_output.getvalue()
 
     assert contains(r"k_grid\s+2 2 2", txt)
@@ -76,17 +83,18 @@ def test_control(create_au_bulk_atoms_obj):
 @pytest.mark.parametrize(
     "functional,expected_functional_expression",
     [("PBE", r"xc\s+PBE"), ("LDA", r"xc\s+pw-lda"),
-    pytest.param("PBE_06_Fake", None, marks=pytest.mark.xfail)])
+     pytest.param("PBE_06_Fake", None, marks=pytest.mark.xfail)])
 def test_control_functional(
         create_au_bulk_atoms_obj, functional: str,
         expected_functional_expression: str):
-    """Test that the functional that is set is written to the control.in file."""
+    """Test that the functional written to the control.in file."""
     # Copy the global parameters dicitonary to avoid rewriting common
     # parameters. Then assign functional to parameter dictionary.
     parameters = _PARAMETERS_DICT.copy()
     parameters["xc"] = functional
-    string_output = StringIO()
-    write_control(string_output, create_au_bulk_atoms_obj, parameters)
+    string_output = io.StringIO()
+    ase.io.aims.write_control(
+        string_output, create_au_bulk_atoms_obj, parameters)
     txt = string_output.getvalue()
     assert contains(expected_functional_expression, txt)
 
@@ -100,13 +108,14 @@ def test_control_wrong_tier(create_au_bulk_atoms_obj):
     parameters = _PARAMETERS_DICT.copy()
     parameters["tier"] = "1"
     with pytest.raises(ValueError, match='Given basis tier:'):
-        write_control(StringIO(), create_au_bulk_atoms_obj, parameters)
+        ase.io.aims.write_control(
+            io.StringIO(), create_au_bulk_atoms_obj, parameters)
+
 
 @pytest.mark.parametrize(
-    "output_level,tier,expected_basis_set_re",
-    [
+    "output_level,tier,expected_basis_set_re", [
         ("tight", 0, "#     hydro 4 f 7.4"),
-        ("tight", 1,"ionic 6 p auto\n     hydro 4 f 7.4")])
+        ("tight", 1, "ionic 6 p auto\n     hydro 4 f 7.4")])
 def test_control_tier(
         create_au_bulk_atoms_obj,
         output_level: str, tier: int,
@@ -122,30 +131,32 @@ def test_control_tier(
         create_au_bulk_atoms_obj: PyTest fixture to create a test Au bulk ase
             Atoms object that we can use to write out the control.in file.
         output_level: The numerical settings (e.g. light, tight, really_tight).
-        tier: The basis set size (either None for standard, 0 for minimal, 1 for tier1,
-            etc...)
+        tier: The basis set size (either None for standard, 0 for minimal, 1
+            for tier1, etc...)
         expected_basis_set_re: Expression we expect to find in the control.in.
-            We expect lines to be either commented or uncommented which indicate
-            whether a basis set function is included or not in the calcuation.
+            We expect lines to be either commented or uncommented which
+            indicate whether a basis set function is included or not in the
+            calcuation.
     """
     parameters = _PARAMETERS_DICT.copy()
     parameters["output_level"] = output_level
     parameters['tier'] = tier
-    string_output = StringIO()
-    write_control(string_output, create_au_bulk_atoms_obj, parameters)
+    string_output = io.StringIO()
+    ase.io.aims.write_control(
+        string_output, create_au_bulk_atoms_obj, parameters)
     txt = string_output.getvalue()
     assert contains(expected_basis_set_re, txt)
 
+
 @pytest.mark.parametrize(
-    "tier_as_str,expected_tier_as_int",
-    [
+    "tier_as_str,expected_tier_as_int", [
         ("first", 1), ("second", 2), ("third", 3),
         ("fourth", 4), ("Gibberish", -1)])
 def test_translate_tier(
         tier_as_str,
         expected_tier_as_int):
     """Test translate_tier from string to integer."""
-    assert translate_tier(tier_as_str) == expected_tier_as_int
+    assert ase.io.aims.translate_tier(tier_as_str) == expected_tier_as_int
 
 
 def test_format_tiers(
@@ -154,8 +165,11 @@ def test_format_tiers(
     target_tier = "second"
     found_target = True
     do_uncomment = True
-    output_line, found_target_returned, do_uncomment_returned = format_tiers(
-        line, target_tier, found_target, do_uncomment)
+    (
+        output_line,
+        found_target_returned,
+        do_uncomment_returned) = ase.io.aims.format_tiers(
+            line, target_tier, found_target, do_uncomment)
     assert output_line == "     hydro 4 f 7.4"
     # These two state variables should not have been altered by the function.
     assert found_target_returned == found_target
@@ -170,7 +184,7 @@ def test_format_tiers(
         pytest.param("s", None, None, marks=pytest.mark.xfail)])
 def test_get_tier_list(tier, species, expected_tier_list):
     """Test getting a list of tiers using the get_tier_list method."""
-    tier_list = get_tier_list(tier, species)
+    tier_list = ase.io.aims.get_tier_list(tier, species)
 
     if expected_tier_list is None:
         assert tier_list == expected_tier_list
@@ -181,7 +195,7 @@ def test_get_tier_list(tier, species, expected_tier_list):
 def test_write_species(create_au_bulk_atoms_obj):
     """Test writing species file."""
     parameters = {"tier": 0, "species_dir": f"{parent}/species_dir/"}
-    file_handle = StringIO()
-    write_species(
+    file_handle = io.StringIO()
+    ase.io.aims.write_species(
         file_handle, create_au_bulk_atoms_obj, parameters)
     assert contains("#     ionic 6 p auto", file_handle.getvalue())
