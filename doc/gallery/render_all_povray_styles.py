4c738a9d322384a9ec67fab5057ef0f814669f6f
Michael Waters <michael.j.waters@northwestern.edu>
2022-10-21 18:23:31 -0500

    Code style clean up.

--- doc/gallery/render_all_povray_styles.py
+++ doc/gallery/render_all_povray_styles.py
@@ -6,31 +6,30 @@ import os
 
 atoms = ptable(spacing=3)
 
-
 ######
 atoms.write('ptable.true_colors.png')
 io.write('ptable.xyz',atoms)
 #########
 from ase.io.pov import POVRAY
-styles = list( POVRAY.material_styles_dict.keys() )
+styles = list( POVRAY.material_styles_dict)
 
 for style in styles:
-    pov_name = 'ptable.%s.pov'%style
+    pov_name = 'ptable.{}.pov'.format(style)
     ini_name = pov_name.replace('pov','ini')
 
     kwargs = { # For povray files only
-        'textures': len(atoms)*[style],
-        'transparent'  : True, # Transparent background
-        'canvas_width' : 1000,  # Width of canvas in pixels
-        'camera_type': 'orthographic angle 65', 
-        }
+             'textures': len(atoms) * [style],
+             'transparent'  : True, # Transparent background
+             'canvas_width' : 1000,  # Width of canvas in pixels
+             'camera_type': 'orthographic angle 65', 
+             }
 
-    generic_projection_settings = {
-        }
+    generic_projection_settings = {}
 
-    io.write(pov_name, atoms, 
+    pov_object = io.write(pov_name, atoms, 
         **generic_projection_settings,
         povray_settings=kwargs)
-
+        
     if run_povray:
-        os.system('povray %s'%ini_name)
+        pov_object.render()
+
