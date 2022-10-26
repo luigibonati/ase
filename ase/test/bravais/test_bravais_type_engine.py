3089f2c64fb17f111a7670a2ee3cfaab2d770f4c
Ask Hjorth Larsen <asklarsen@gmail.com>
2022-10-26 18:05:59 +0200

    prune Niggli op table, including only operations that at least once produce an optimal (i.e. lowest orthogonality defect) lattice

--- ase/test/bravais/test_bravais_type_engine.py
+++ ase/test/bravais/test_bravais_type_engine.py
@@ -1,49 +1,30 @@
 import pytest
 import numpy as np
-from ase.geometry.bravais_type_engine import generate_niggli_op_table
-
-
-ref_info = {
-    'LINE': 1,
-    'SQR': 1,
-    'RECT': 1,
-    'CRECT': 4,
-    'HEX2D': 1,
-    'OBL': 5,
-    'FCC': 1,
-    'BCC': 1,
-    'CUB': 1,
-    'TET': 2,
-    'BCT': 5,
-    'HEX': 2,
-    'ORC': 1,
-    'ORCC': 5,
-    'ORCF': 2,
-    'ORCI': 4,
-    'RHL': 3,
-    # 'MCL': 15,
-    # 'MCLC': 27,
-    # 'TRI': 19,
-}
+from ase.geometry.bravais_type_engine import (
+    generate_niggli_op_table, niggli_op_table)
 
 
 # We disable the three lattices that have infinite reductions.
 # Maybe we can test those, but not today.
-n3d_lattices = 14 - 3
-n2d_lattices = 5
-n1d_lattices = 1
-assert len(ref_info) == n3d_lattices + n2d_lattices + n1d_lattices
+#n3d_lattices = 14 - 3
+#n2d_lattices = 5
+#n1d_lattices = 1
+#assert len(ref_info) == n3d_lattices + n2d_lattices + n1d_lattices
+
+ignore_lattices = {'MCL', 'MCLC', 'TRI'}
 
 
 def ref_info_iter():
-    for key, val in ref_info.items():
-        yield key, val
+    for latname, ops in niggli_op_table.items():
+        if latname in ignore_lattices:
+            continue
+        yield latname, ops
 
 
-@pytest.mark.parametrize('lattice_name,ref_nops', ref_info_iter())
-def test_generate_niggli_table(lattice_name, ref_nops):
-    length_grid = np.logspace(-1, 1, 30)
-    angle_grid = np.linspace(30, 170, 50)
+@pytest.mark.parametrize('lattice_name,ref_ops', ref_info_iter())
+def test_generate_niggli_table(lattice_name, ref_ops):
+    length_grid = np.logspace(-1, 1, 41)
+    angle_grid = np.linspace(30, 170, 51)
     table = generate_niggli_op_table(lattices=[lattice_name],
                                      angle_grid=angle_grid,
                                      length_grid=length_grid)
@@ -51,4 +32,4 @@ def test_generate_niggli_table(lattice_name, ref_nops):
         print('{}: {}'.format(key, len(table[key])))
 
     mappings = table[lattice_name]
-    assert len(mappings) == ref_nops
+    assert set(mappings) == set(ref_ops)
